e087eb85a4c2e51af467a1a33662152136b57beb
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2023-12-27 13:01:46 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -0,0 +1,12 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin", "utf8")
+  .trim()
+  .split("\n")
+  .map((line) => line.replace(/\r/, ""));
+
+const solution = (input) => {
+  const nums = input[0].split(" ").map((v) => Number(v));
+  console.log(nums[0] + nums[1]);
+};
+
+solution(input);
