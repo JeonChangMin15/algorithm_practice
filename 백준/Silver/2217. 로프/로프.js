9961caa11dddc1720b6252991f3909f36a2789fa
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2024-01-02 20:46:44 +0900

    Update 로프.js

--- 백준/Silver/2217. 로프/로프.js
+++ 백준/Silver/2217. 로프/로프.js
@@ -1,23 +1,29 @@
-
-
 const input = require("fs").readFileSync("/dev/stdin", "utf8").trim().split("\n");
 
+
+// k개의 로프를 사용해서 w인 물체를 들어올린다
+// 로프를 사용해서 최대의 무게를 구해라
+// 먼저 무게를 오름차순으로 정렬
+// 해당무게에다가 갯수의 -1씩 감소시켜서 곱한 값들의 최대값을 출력하면된다
 const solution = (input) => {
   const n = Number(input[0]);
-  const weight = [];
+  const weights = [];
   for (let i = 1; i < input.length; i++) {
-    const w = Number(input[i]);
-    weight.push(w);
-  }
-  const total = [];
-  weight.sort((a, b) => a - b);
-  for (let i = 0; i < weight.length; i++) {
-    const value = (n - i) * weight[i];
-    total.push(value);
+    weights.push(Number(input[i]));
   }
 
-  const max = Math.max(...total);
-  console.log(max);
+  weights.sort((a, b) => a - b);
+  const limitN = [];
+  for (let i = n; i >= 1; i--) {
+    limitN.push(i);
+  }
+
+  const total = [];
+  for (let i = 0; i < weights.length; i++) {
+    total.push(weights[i] * limitN[i]);
+  }
+
+  console.log(Math.max(...total));
 };
 
 solution(input);
