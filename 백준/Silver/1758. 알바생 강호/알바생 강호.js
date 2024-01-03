28c9c9d5732f6828588915ad57ba52455bd9ef18
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2024-01-03 14:39:57 +0900

    Update 알바생 강호.js

--- 백준/Silver/1758. 알바생 강호/알바생 강호.js
+++ 백준/Silver/1758. 알바생 강호/알바생 강호.js
@@ -1,28 +1,28 @@
-// 첫번째 줄에는 인원수고 그다음줄부터는 주려고하는 팁 액수이다
-// 손님의 순서를 임의로 바꿔서 팁의 액수를 최대로 받을 수 있는 액수를 구해야된다
-// 금액을 내림차순으로 바꿔서 해당 인덱스의 값 - (i+1 -1)
-// 0보다 크면 더해주며된다
+const input = require("fs")
+  .readFileSync("/dev/stdin", "utf8")
+  .trim()
+  .split("\n")
+  .map((line) => line.replace(/\r/, ""));
 
-const input = require("fs").readFileSync("/dev/stdin", "utf8").trim().split("\n");
-
-
+// 첫번째줄에는 사람의 수
+// 두번째 줄에는 사람이 주려는 팁의 액수
+// 팁의 액수를 배열에넣어서 내림차순으로 정렬하고
+// 각 금액의 순서 - 1 만큼의 액수를 차감한 후 팁을 지급
+// 팁이 양수일때만 주고 음수면 0원
 const solution = (input) => {
   const n = Number(input[0]);
-  const tip = [];
+  const tips = [];
+  let answer = 0;
   for (let i = 1; i < input.length; i++) {
-    const val = Number(input[i]);
-    tip.push(val);
+    tips.push(Number(input[i]));
+  }
+  tips.sort((a, b) => b - a);
+  for (let i = 0; i < tips.length; i++) {
+    const money = tips[i] - i;
+    if (money > 0) answer += money;
   }
 
-  tip.sort((a, b) => b - a);
-
-  let total = 0;
-  for (let i = 0; i < tip.length; i++) {
-    const currentTip = tip[i] - i;
-    if (currentTip > 0) total += currentTip;
-  }
-
-  console.log(total);
+  console.log(answer);
 };
 
 solution(input);
