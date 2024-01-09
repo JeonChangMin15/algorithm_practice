ffcb943c3d0440f7228351fc46b671bea4af4735
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2024-01-09 10:01:50 +0900

    Update 에너지 드링크.js

--- 백준/Silver/20115. 에너지 드링크/에너지 드링크.js
+++ 백준/Silver/20115. 에너지 드링크/에너지 드링크.js
@@ -1,21 +1,23 @@
-// 첫번째줄에는 드링크 갯수가 주어지고
-// 두번째줄에는 드링크 용량이 주어진다
-// 오름차순으로 sort하고나서
-// 마지막 하나만 제외하고 전부다 반토막내서 마지막으로 큰것에대가 넣으면 된다
+const input = require("fs")
+  .readFileSync("/dev/stdin", "utf8")
+  .trim()
+  .split("\n")
+  .map((line) => line.replace(/\r/, ""));
 
-const input = require("fs").readFileSync("/dev/stdin", "utf8").trim().split("\n");
-
+// 1. 임의의 서로 다른 두 에너지 드링크를 고른다
+// 2. 한쪽에너지 드링크를 다른쪽 에너지 드링크에 붙는데 절반만 넣는다
+// 3. 이렇게해서 하나의 에너지 드링크만 남을때가지 반복해서 드링크양을 최대로 만든다
+// 첫째줄에 드링크수 둘째줄에 드링크 용량이 주어진다
+// 오름차순으로 만들고 가장큰 용량을 제외한 나머지의 합 절반과 가장큰 용량을 더하면된다
 const solution = (input) => {
   const n = Number(input[0]);
   const size = input[1].split(" ").map((v) => Number(v));
   size.sort((a, b) => a - b);
-  let answer = 0;
-  answer += size[n - 1];
-  for (let i = 0; i < n - 1; i++) {
-    answer += size[i] / 2;
-  }
 
-  console.log(answer);
+  const max = size.pop();
+  const res = size.reduce((prev, cur) => prev + cur, 0) / 2;
+
+  console.log(max + res);
 };
 
 solution(input);
