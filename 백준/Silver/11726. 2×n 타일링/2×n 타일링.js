ca8da3d1c9d69b95f0fac0d1951798c16e9889f4
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2024-01-17 09:57:46 +0900

    [Silver III] Title: 2×n 타일링, Time: 200 ms, Memory: 9640 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.js
@@ -0,0 +1,23 @@
+
+
+const input = require("fs")
+  .readFileSync("/dev/stdin", "utf8")
+  .trim()
+  .split("\n")
+  .map((line) => line.replace(/\r/, ""));
+
+const solution = (input) => {
+  const n = Number(input[0]);
+  const dp = Array(n + 1).fill(0);
+
+  dp[1] = 1;
+  dp[2] = 2;
+
+  for (let i = 3; i <= n; i++) {
+    dp[i] = (dp[i - 1] + dp[i - 2])%10007;
+  }
+
+  console.log(dp[n]);
+};
+
+solution(input);
