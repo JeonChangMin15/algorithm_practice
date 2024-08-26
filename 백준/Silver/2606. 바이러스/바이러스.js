30c1f05fd83ac0a16db24a8bcaf4b40d5ac50f8b
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2024-08-26 19:35:49 +0900

    [Silver III] Title: 바이러스, Time: 92 ms, Memory: 9448 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.js
+++ 백준/Silver/2606. 바이러스/바이러스.js
@@ -1,44 +1,41 @@
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
-  .split("\n")
-  .map((line) => line.replace(/\r/, ""));
+  .split("\n");
 
-// 1번노드부터 깊이 우선 탐색을 시작해서 몇개의 노드가 감염되는지 구하는지 문제
-// 그래프 객체를 하나만들어서 방문한 노드인지 체크
-// 배열에다가 방문한 노드를 담아서 해당 길이의 -1을 리턴하면된다.
-// 첫번째 줄은 컴퓨터의 수, 두번째수는 컴퓨터쌍의 수
-
+// 1번 노드가 감염됐을때 감염된 컴퓨터 수를 구해라
+// 첫번째줄에 컴퓨터수 두번째줄에 연결 수
+// dfs로 visited로 체킹하면서 돌면 된다
+// dfs(1) 그래프 만들어서 풀면됨
 const solution = (input) => {
   const computerN = Number(input[0]);
-  const nodeN = Number(input[1]);
-  const grpah = {};
+  const lineN = Number(input[1]);
+  const graph = {};
 
-  for (let i = 1; i <= computerN; i++) grpah[i] = [];
-
-  for (let i = 2; i <= nodeN + 1; i++) {
-    const [n, m] = input[i].split(" ").map((v) => Number(v));
-    grpah[n].push(m);
-    grpah[m].push(n);
+  for (let i = 1; i <= computerN; i++) {
+    graph[i] = [];
   }
 
-  const visited = [];
+  for (let i = 2; i < 2 + lineN; i++) {
+    const [n1, n2] = input[i].split(" ").map((v) => Number(v));
+    graph[n1].push(n2);
+    graph[n2].push(n1);
+  }
 
-  // node를 들어왔으면 먼저 visited에 있는지 확인
-  // 그리고 visited에 해당 노드 푸쉬
-  // 해당 노드에 있는 그래프 노드를 반복문 돌면서 확인
+  const visited = Array(computerN + 1).fill(false);
+
   const dfs = (node) => {
-    if (visited.includes(node)) return;
-    visited.push(node);
+    visited[node] = true;
 
-    for (let i = 0; i < grpah[node].length; i++) {
-      dfs(grpah[node][i]);
+    for (const nextNode of graph[node]) {
+      if (visited[nextNode]) continue;
+      dfs(nextNode);
     }
   };
 
   dfs(1);
 
-  console.log(visited.length - 1);
+  console.log(visited.filter((v) => v === true).length - 1);
 };
 
 solution(input);
