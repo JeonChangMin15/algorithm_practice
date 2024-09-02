2d440b24bcdfdf24c1ce43307785038f61123c3c
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2024-09-02 19:59:23 +0900

    [Silver III] Title: N과 M (1), Time: 1664 ms, Memory: 13596 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.js
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -1,16 +1,17 @@
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
-  .split("\n")
-  .map((line) => line.replace(/\r/, ""));
+  .split("\n");
 
-// 자연수 N,M이 주어지고 1부터 N까지 숫자에서 M개의 중복수열
-// dfs로 풀이하면된다
+// 첫번째줄에 n과 m이 주어진다
+// 1부터 N까지 M개로 조합되는 수열을 출력
+// 문제는 중복순열은 하지 않는다
+// 백트래킹을 풀어야한다
 const solution = (input) => {
-  const [n, m] = input[0].split(" ").map((v) => Number(v));
+  const [n, sizeN] = input[0].split(" ").map((v) => Number(v));
 
   const dfs = (arr) => {
-    if (arr.length === m) {
+    if (arr.length === sizeN) {
       console.log(arr.join(" "));
       return;
     }
@@ -18,7 +19,7 @@ const solution = (input) => {
     for (let i = 1; i <= n; i++) {
       if (arr.includes(i)) continue;
       arr.push(i);
-      dfs(arr);
+      dfs(arr, i + 1);
       arr.pop();
     }
   };
