277484525718c272c266b97c486adbd9a6f825fe
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2024-09-25 13:34:47 +0900

    [Silver II] Title: 그대, 그머가 되어, Time: 144 ms, Memory: 113980 KB -BaekjoonHub

--- 백준/Silver/14496. 그대， 그머가 되어/그대， 그머가 되어.py
+++ 백준/Silver/14496. 그대， 그머가 되어/그대， 그머가 되어.py
@@ -1,36 +1,32 @@
 from collections import deque
 
-startNode, endNode = list(map(int, input().split()))
-nodeN, lineN = list(map(int, input().split()))
-graph = {}
+start, end = list(map(int, input().split()))
+n, lineN = list(map(int, input().split()))
 
-for i in range(1, nodeN+1):
-  graph[i] = []
+graph = [[] for _ in range(n+1)]
 
 for i in range(lineN):
   n1, n2 = list(map(int, input().split()))
   graph[n1].append(n2)
   graph[n2].append(n1)
 
-queue = deque()
-queue.append([startNode, 0])
-visited = [False]*(nodeN + 1)
-visited[startNode] = True
+visited = [False]*(n+1)
+visited[start] = True
 
+queue = deque()
+queue.append([start, 0])
 answer = -1
 
 while len(queue):
-  curNode, cnt = queue.popleft()
-
-  if curNode == endNode:
+  node, cnt = queue.popleft()
+  if node == end:
     answer = cnt
     break
 
-  for nextNode in graph[curNode]:
+  for nextNode in graph[node]:
     if visited[nextNode]:
       continue
-
-    queue.append([nextNode, cnt + 1])
+    queue.append([nextNode, cnt+1])
     visited[nextNode] = True
 
 print(answer)
