d3546f92cbaaacd5f3b59f392cc627ce044a39da
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2024-09-30 08:46:25 +0900

    [Silver II] Title: 점프 점프, Time: 192 ms, Memory: 21368 KB -BaekjoonHub

--- 백준/Silver/14248. 점프 점프/점프 점프.js
+++ 백준/Silver/14248. 점프 점프/점프 점프.js
@@ -1,37 +1,41 @@
-// const input = require("fs")
-//   .readFileSync("example.txt", "utf8")
-//   .trim()
-//   .split("\n")
-//   .map((line) => line.replace(/\r/, ""));
-
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// 첫번째줄에 돌의 개수
-// 둘째줄에는 각 위치에서 점프할 수 있는거리
-//해당 거리만큼 오른쪽, 왼쪽으로 이동 가능, 돌밖으로는 안됨
-// 셋째줄에는 처음 위치 1부터 시작해서 -1한 위치로 인덱스
-// 방문 가능한 돌들의 갯수를 출력
 const solution = (input) => {
   const n = Number(input[0]);
-  const step = input[1].split(" ").map((v) => Number(v));
-  const start = Number(input[2]) - 1;
+  const arr = input[1].split(" ").map((v) => Number(v));
+  const start = Number(input[2]);
 
-  const visited = Array(n).fill(false);
-  const dfs = (position) => {
-    if (position < 0 || position >= n) return;
-    visited[position] = true;
+  const queue = [];
+  queue.push(start - 1);
+  const visted = Array(n).fill(false);
+  visted[start - 1] = true;
 
-    dfs(position + step[position]);
-    dfs(position - step[position]);
-  };
+  while (queue.length) {
+    const curNode = queue.shift();
+    const step = arr[curNode];
 
-  dfs(start);
+    if (curNode - step >= 0 && !visted[curNode - step]) {
+      queue.push(curNode - step);
+      visted[curNode - step] = true;
+    }
 
-  console.log(visited.filter((v) => v === true).length);
+    if (curNode + step < n && !visted[curNode + step]) {
+      queue.push(curNode + step);
+      visted[curNode + step] = true;
+    }
+  }
+
+  let answer = 0;
+
+  for (const val of visted) {
+    if (val) answer += 1;
+  }
+
+  console.log(answer);
 };
 
 solution(input);
