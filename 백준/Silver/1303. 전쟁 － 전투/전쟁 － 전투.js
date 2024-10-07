4e0e125761e501da5cb4fce29f8ab6056418641c
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2024-10-07 20:17:31 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 152 ms, Memory: 10024 KB -BaekjoonHub

--- 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.js
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.js
@@ -1,19 +1,16 @@
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
-  .split("\n")
-  .map((line) => line.replace(/\r/, ""));
+  .split("\n");
 
-// 첫째줄에 가로크기, 세로크기가 주어지고
-// 다음줄부터 띄어쓰기 없이 주어지고 B-> 파란색, W는 흰색이다
-// n명이 뭉쳣을때 n제곱이다. w영역의 제곱들의합, b영역의 제곱들의 합을 한줄에 출력하면된다
-// visited와 dfs로 row,col,team으로 비교하면서 영역을 구해서 더하면된다
 const solution = (input) => {
   const [colN, rowN] = input[0].split(" ").map((v) => Number(v));
   const grid = [];
-  for (let i = 1; i < input.length; i++) {
+
+  for (let i = 1; i <= rowN; i++) {
     grid.push(input[i].split(""));
   }
+
   const visited = Array(rowN)
     .fill(0)
     .map((v) => Array(colN).fill(false));
@@ -26,11 +23,11 @@ const solution = (input) => {
       y >= colN ||
       team !== grid[x][y] ||
       visited[x][y]
-    )
+    ) {
       return 0;
+    }
 
     visited[x][y] = true;
-
     const up = dfs(x - 1, y, team);
     const down = dfs(x + 1, y, team);
     const left = dfs(x, y - 1, team);
@@ -39,23 +36,23 @@ const solution = (input) => {
     return 1 + up + down + left + right;
   };
 
-  let blueTeam = 0;
-  let whiteTeam = 0;
+  let white = 0;
+  let blue = 0;
 
   for (let i = 0; i < rowN; i++) {
     for (let j = 0; j < colN; j++) {
-      if (visited[i][j]) continue;
-      const area = dfs(i, j, grid[i][j]);
-
-      if (grid[i][j] === "W") {
-        whiteTeam += area * area;
-      } else {
-        blueTeam += area * area;
+      if (!visited[i][j]) {
+        const area = dfs(i, j, grid[i][j]);
+        if (grid[i][j] == "W") {
+          white += area * area;
+        } else {
+          blue += area * area;
+        }
       }
     }
   }
 
-  console.log(whiteTeam, blueTeam);
+  console.log(white, blue);
 };
 
 solution(input);
