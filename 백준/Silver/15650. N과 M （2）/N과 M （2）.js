86e3d169ad5ef206a6ed88a5e1a4cb2f105378a4
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2024-10-21 12:36:31 +0900

    [Silver III] Title: N과 M (2), Time: 96 ms, Memory: 9460 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.js
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.js
@@ -4,19 +4,17 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// 자연수 N,M이 주어지고 1부터 N까지 숫자에서 M개의 중복수열
-// 종복되는 수열을 출력하면 안된다. 오름차순으로 출력
-// dfs로 풀이하면된다
 const solution = (input) => {
-  const [n, m] = input[0].split(" ").map((v) => Number(v));
+  const [n, sizeN] = input[0].split(" ").map((v) => Number(v));
 
   const dfs = (arr, start) => {
-    if (arr.length === m) {
+    if (arr.length === sizeN) {
       console.log(arr.join(" "));
       return;
     }
 
     for (let i = start; i <= n; i++) {
+      if (arr.includes(i)) continue;
       arr.push(i);
       dfs(arr, i + 1);
       arr.pop();
