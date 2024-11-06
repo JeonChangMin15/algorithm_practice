e2c60db395bf3c9e7dba27fab1ef9a86b85ee671
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2024-11-06 13:37:49 +0900

    [Gold V] Title: 입국심사, Time: 268 ms, Memory: 114916 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.py
+++ 백준/Gold/3079. 입국심사/입국심사.py
@@ -1,20 +1,24 @@
+n, peopleN = list(map(int, input().split()))
+arr = []
 
-import sys
+for i in range(n):
+  arr.append(int(input()))
 
-n, m = map(int, sys.stdin.readline().split())
-t = [int(sys.stdin.readline()) for _ in range(n)]
 
-left = min(t)
-answer = right = max(t) * m
+left = min(arr)
+right = max(arr)*peopleN
+answer = float('inf')
 
 while left <= right:
-    total = 0
-    mid = (left + right) // 2
-    for i in range(n):
-        total += mid // t[i]
-    if total >= m:
-        right = mid - 1
-        answer = min(answer, mid)
-    else:
-        left = mid + 1
+  mid = (left + right) // 2
+  curPeopleN = 0
+  for t in arr:
+    curPeopleN += mid // t
+
+  if curPeopleN >= peopleN:
+    answer = min(answer, mid)
+    right = mid - 1
+  else:
+    left = mid + 1
+
 print(answer)
