2f7a83b6b7660ca2cd704f364414431107748d76
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2024-11-29 13:24:11 +0900

    [Silver I] Title: 부등호, Time: 188 ms, Memory: 12840 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.js
+++ 백준/Silver/2529. 부등호/부등호.js
@@ -4,61 +4,46 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// 첫째줄에 부등호의 갯수가 주어지고 둘째줄에 부등호들이 주어진다
-// 숫자조합은 0부터 9까지 하나씩만 사용해서 부등호 갯수 +1의 숫자 조합을 만들어서
-// 부등호 관계를 다 만족하는 최대, 최소 정수를 출력해야된다 021 이런숫자도
-// 일단 조합으로 해당 숫자의 길이에 해당되는걸 다 넣고 하나씩 부등호 조합을 비교하면될거같다
+// 0-9 까지 숫자가 단 하나만 들어갈 수 있다
+// 해당 부등호를 만족하는 숫자조합을 최대 최소를 각각 출력해야된다
+// step으로 바로 직전값과 해당 부등호를 보고 판단해서 넣으면된다.
 const solution = (input) => {
   const n = Number(input[0]);
-  const relations = input[1].split(" ");
+  const method = input[1].split(" ");
+  const answer = [];
 
-  let maxNum = -Infinity;
-  let minNum = Infinity;
-
-  let maxAnswer = "";
-  let minAnswer = "";
-
-  const dfs = (arr) => {
-    if (arr.length === n + 1) {
-      const val = arr.join("");
-      let isValid = true;
-
-      for (let i = 0; i < relations.length; i++) {
-        if (relations[i] === "<" && arr[i] > arr[i + 1]) {
-          isValid = false;
-          break;
-        }
-
-        if (relations[i] === ">" && arr[i] < arr[i + 1]) {
-          isValid = false;
-          break;
-        }
-      }
-
-      if (isValid && Number(val) > maxNum) {
-        maxNum = Number(val);
-        maxAnswer = val;
-      }
-
-      if (isValid && Number(val) < minNum) {
-        minNum = Number(val);
-        minAnswer = val;
-      }
+  const dfs = (arr, step) => {
+    if (step === n) {
+      answer.push(arr.join(""));
       return;
     }
 
     for (let i = 0; i <= 9; i++) {
       if (arr.includes(i)) continue;
-      arr.push(i);
-      dfs(arr);
-      arr.pop();
+      if (step === -1) {
+        arr.push(i);
+        dfs(arr, step + 1);
+        arr.pop();
+      } else {
+        if (method[step] === "<" && arr[step] < i) {
+          arr.push(i);
+          dfs(arr, step + 1);
+          arr.pop();
+        }
+
+        if (method[step] === ">" && arr[step] > i) {
+          arr.push(i);
+          dfs(arr, step + 1);
+          arr.pop();
+        }
+      }
     }
   };
 
-  dfs([]);
+  dfs([], -1);
 
-  console.log(maxAnswer);
-  console.log(minAnswer);
+  console.log(answer[answer.length - 1]);
+  console.log(answer[0]);
 };
 
 solution(input);
