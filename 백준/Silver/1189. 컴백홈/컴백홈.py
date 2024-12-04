be06e20939a7b2c0abe09caba924014887e568d1
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2024-12-04 13:20:42 +0900

    [Silver I] Title: 컴백홈, Time: 308 ms, Memory: 117468 KB -BaekjoonHub

--- 백준/Silver/1189. 컴백홈/컴백홈.py
+++ 백준/Silver/1189. 컴백홈/컴백홈.py
@@ -4,29 +4,25 @@ grid = []
 for i in range(rowN):
   grid.append(list(input()))
 
+visited = [[False]*colN for _ in range(rowN)]
 answer = 0
-visited = [[False]*(colN) for i in range(rowN)]
 
-def dfs(row, col, depth):
+def dfs(x, y, curLen):
   global answer
-  if (row < 0 or 
-      row >= rowN or 
-      col < 0 or 
-      col >= colN or 
-      visited[row][col] or 
-      grid[row][col] == 'T'):
+
+  if x < 0 or x>=rowN or y < 0 or y>=colN or grid[x][y]=='T' or visited[x][y]:
     return
-  
-  if row == 0 and col == colN-1 and depth == dist:
+
+  if x == 0 and y == colN -1 and curLen == dist:
     answer += 1
     return
 
-  visited[row][col] = True
-  dfs(row+1, col, depth+1)
-  dfs(row-1, col, depth+1)
-  dfs(row, col+1, depth+1)
-  dfs(row, col-1, depth+1)
-  visited[row][col] = False
+  visited[x][y] = True
+  dfs(x-1, y, curLen+1)
+  dfs(x+1, y, curLen+1)
+  dfs(x, y-1, curLen+1)
+  dfs(x, y+1, curLen+1)
+  visited[x][y] = False
 
 dfs(rowN-1, 0, 1)
 
