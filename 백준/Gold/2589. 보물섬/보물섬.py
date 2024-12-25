c21e3815fc760ef6381674116d16f1d0385a8f73
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2024-12-25 13:33:54 +0900

    [Gold V] Title: 보물섬, Time: 708 ms, Memory: 119936 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.py
+++ 백준/Gold/2589. 보물섬/보물섬.py
@@ -2,43 +2,39 @@ from collections import deque
 
 rowN, colN = list(map(int, input().split()))
 grid = []
+
 for i in range(rowN):
   grid.append(list(input()))
 
+dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
 def bfs(startX, startY):
-  visited = [[False]*colN for _ in range(rowN)]
   queue = deque()
   queue.append([startX, startY, 0])
-  visited[startX][startY] = True
+  visited = [[False]*colN for _ in range(rowN)]
   maxDist = 0
-  
-  dirs =[[-1,0],[1,0],[0,-1],[0,1]]
+  visited[startX][startY] = True
 
   while len(queue):
     x, y, dist = queue.popleft()
-    maxDist = max(maxDist, dist)
+    maxDist = max(dist, maxDist)
 
     for dx, dy in dirs:
       nextX = x + dx
       nextY = y + dy
-      isValid = (0<= nextX < rowN and 
-                 0<=nextY<colN and 
-                 not visited[nextX][nextY] and 
-                 grid[nextX][nextY] == "L")
-
+      isValid = 0<=nextX<rowN and 0<=nextY<colN and grid[nextX][nextY]=='L' and not visited[nextX][nextY]
       if isValid:
-        visited[nextX][nextY] = True
         queue.append([nextX, nextY, dist + 1])
-  
+        visited[nextX][nextY] = True
+
   return maxDist
 
 answer = 0
 
 for i in range(rowN):
   for j in range(colN):
-    if grid[i][j] == 'W':
-      continue
-    result = bfs(i, j)
-    answer = max(answer, result)
+    if grid[i][j] == 'L':
+      dist = bfs(i, j)
+      answer = max(dist, answer)
 
 print(answer)
