a9895946303426faa085728f723a021461b03854
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2024-12-30 13:57:21 +0900

    [Silver II] Title: 차이를 최대로, Time: 144 ms, Memory: 111480 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
@@ -1,17 +1,17 @@
 n = int(input())
-nums = list(map(int, input().split()))
+nums = list(map(int , input().split()))
+
 answer = 0
 
 def dfs(arr):
   global answer
-  if len(arr) == n:
-    total = 0
-    for i in range(n-1):
-      idx = arr[i]
-      nextIdx = arr[i+1]
-      total += abs(nums[idx] - nums[nextIdx])
 
-    answer = max(answer, total)
+  if len(arr) == n:
+    curSum = 0
+    for i in range(n-1):
+      curSum += abs(nums[arr[i]] - nums[arr[i+1]])
+    
+    answer = max(answer, curSum)
     return
 
   for i in range(n):
