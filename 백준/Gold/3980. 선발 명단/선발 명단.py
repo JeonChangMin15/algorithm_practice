2c09e125653bd3e674fa2661939505e7df60fb0a
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-01-01 10:33:31 +0900

    [Gold V] Title: 선발 명단, Time: 156 ms, Memory: 111416 KB -BaekjoonHub

--- 백준/Gold/3980. 선발 명단/선발 명단.py
+++ 백준/Gold/3980. 선발 명단/선발 명단.py
@@ -1,27 +1,30 @@
-n = int(input())
+caseN = int(input())
+answer = 0
 
-for i in range(n):
-  grid = []
-  visited = [False]*11
+def backtracking(arr, grid):
+  global answer
+  if len(arr) == 11:
+    point = 0
+    for i in range(11):
+      person = arr[i]
+      point += grid[person][i]
+
+    answer = max(point, answer)
+    return
 
   for i in range(11):
+    position = len(arr)
+
+    if grid[i][position] > 0 and i not in arr:
+      arr.append(i)
+      backtracking(arr, grid)
+      arr.pop()
+
+for i in range(caseN):
+  grid = []
+  answer = 0
+  for _ in range(11):
     grid.append(list(map(int, input().split())))
 
-  max_val = 0
-
-  def dfs(order, total):
-    global max_val
-
-    if all(v for v in visited) and order == 11:
-      max_val = max(max_val, total)
-      return
-
-    for i in range(11):
-      if grid[order][i] > 0 and not visited[i]:
-        visited[i] = True
-        dfs(order + 1, total + grid[order][i])
-        visited[i] = False
-
-  dfs(0, 0)
-
-  print(max_val)
+  backtracking([], grid)
+  print(answer)
