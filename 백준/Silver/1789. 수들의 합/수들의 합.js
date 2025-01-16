f62d1d8c71df363ddc979fe4d9250dfde647fd3b
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-01-17 08:46:38 +0900

    [Silver V] Title: 수들의 합, Time: 156 ms, Memory: 10188 KB -BaekjoonHub

--- 백준/Silver/1789. 수들의 합/수들의 합.js
+++ 백준/Silver/1789. 수들의 합/수들의 합.js
@@ -4,21 +4,28 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
+// 서로 다른 값들을 더해서 해당 숫자가 되는 조합의 최대수를 구한다
+// 1부터 더하면서 해당 값보다 커지면 더한 조합의-1을 해주면 된다
 const solution = (input) => {
   const target = Number(input[0]);
-  let cnt = 0;
   let sum = 0;
+  let cnt = 0;
+  let val = 1;
 
-  for (let i = 1; i <= target; i++) {
-    sum += i;
+  while (true) {
+    sum += val;
     cnt += 1;
+
     if (sum > target) {
-      cnt -= 1;
+      console.log(cnt - 1);
+      break;
+    } else if (sum === target) {
+      console.log(cnt);
       break;
     }
-  }
 
-  console.log(cnt);
+    val += 1;
+  }
 };
 
 solution(input);
