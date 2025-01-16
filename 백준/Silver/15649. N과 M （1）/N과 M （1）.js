578b07f24e943ece2677a78f6673c7e2cb8a8d8a
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-01-16 09:54:32 +0900

    [Silver III] Title: N과 M (1), Time: 1660 ms, Memory: 13684 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.js
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -1,25 +1,21 @@
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
-  .split("\n");
+  .split("\n")
+  .map((line) => line.replace(/\r/, ""));
 
-// 첫번째줄에 n과 m이 주어진다
-// 1부터 N까지 M개로 조합되는 수열을 출력
-// 문제는 중복순열은 하지 않는다
-// 백트래킹을 풀어야한다
 const solution = (input) => {
   const [n, sizeN] = input[0].split(" ").map((v) => Number(v));
 
   const dfs = (arr) => {
     if (arr.length === sizeN) {
       console.log(arr.join(" "));
-      return;
     }
 
     for (let i = 1; i <= n; i++) {
       if (arr.includes(i)) continue;
       arr.push(i);
-      dfs(arr, i + 1);
+      dfs(arr);
       arr.pop();
     }
   };
