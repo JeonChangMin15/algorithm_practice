127306f5f087488cdc098fe57eab3473af9098c0
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-01-19 12:44:07 +0900

    [Silver V] Title: 숫자 카드, Time: 856 ms, Memory: 146472 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.js
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.js
@@ -1,46 +1,42 @@
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
-  .split("\n")
-  .map((line) => line.replace(/\r/, ""));
+  .split("\n");
 
 const solution = (input) => {
-  const cardN = Number(input[0]);
-
-  const cards = input[1]
+  const n = Number(input[0]);
+  const nums = input[1]
     .split(" ")
     .map((v) => Number(v))
     .sort((a, b) => a - b);
-
   const testN = Number(input[2]);
+  const cases = input[3].split(" ").map((v) => Number(v));
 
-  const testCase = input[3]
-    .split(" ")
-    .map((v, index) => [Number(v), index])
-    .sort((a, b) => a[0] - b[0]);
-
-  const answer = Array(testN).fill(0);
-
-  for (let i = 0; i < testCase.length; i++) {
+  const searchInclude = (target) => {
     let lt = 0;
-    let rt = cardN - 1;
-    const target = testCase[i][0];
-    const targetIndex = testCase[i][1];
+    let rt = n - 1;
+    let isInclude = false;
 
     while (lt <= rt) {
-      let mid = Math.floor((lt + rt) / 2);
-
-      if (target === cards[mid]) {
-        answer[targetIndex] = 1;
+      const mid = Math.floor((lt + rt) / 2);
+      if (nums[mid] === target) {
+        isInclude = true;
         break;
       }
-      if (target < cards[mid]) {
+
+      if (nums[mid] < target) {
+        lt = mid + 1;
+      } else {
         rt = mid - 1;
       }
-      if (target > cards[mid]) {
-        lt = mid + 1;
-      }
     }
+
+    return isInclude ? 1 : 0;
+  };
+
+  const answer = [];
+  for (const val of cases) {
+    answer.push(searchInclude(val));
   }
 
   console.log(answer.join(" "));
