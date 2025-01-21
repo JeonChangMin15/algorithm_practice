7733e710620a7da8785f6bbbf90eda24cf8fcbac
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-01-21 13:00:46 +0900

    [Silver II] Title: DFS와 BFS, Time: 176 ms, Memory: 15448 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.js
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.js
@@ -1,66 +1,59 @@
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
-  .split("\n");
+  .split("\n")
+  .map((line) => line.replace(/\r/, ""));
 
-// 첫째줄에 정점의 개수 간선의 갯수 시작점
-// 그다음 줄부터 간선의 조합 정점 번호가 작은것부터 방문
-// 첫째줄에 dfs 두번째줄에 bfs 출력
-// 먼저 그래프로 넣고 sort한다 그다음부터 탐색하면서 배열에다가 넣으면 된다
 const solution = (input) => {
-  const [nodeN, lineN, startN] = input[0].split(" ").map((v) => Number(v));
-  const graph = {};
-  for (let i = 1; i <= nodeN; i++) {
-    graph[i] = [];
-  }
+  const [n, lineN, start] = input[0].split(" ").map((v) => Number(v));
+  const grid = Array(n + 1)
+    .fill(0)
+    .map((v) => []);
+
   for (let i = 1; i <= lineN; i++) {
     const [n1, n2] = input[i].split(" ").map((v) => Number(v));
-    graph[n1].push(n2);
-    graph[n2].push(n1);
+    grid[n1].push(n2);
+    grid[n2].push(n1);
   }
-  for (let i = 1; i <= nodeN; i++) {
-    graph[i].sort((a, b) => a - b);
+
+  for (let i = 0; i <= n; i++) {
+    grid[i].sort((a, b) => a - b);
   }
 
   const dfsArr = [];
-  const dfsVisited = Array(nodeN + 1).fill(false);
+  const dfsVisit = Array(n + 1).fill(false);
 
-  const dfs = (curNode) => {
-    dfsVisited[curNode] = true;
-    dfsArr.push(curNode);
+  const dfs = (node) => {
+    dfsVisit[node] = true;
+    dfsArr.push(node);
 
-    for (const nextNode of graph[curNode]) {
-      if (dfsVisited[nextNode]) continue;
+    for (const nextNode of grid[node]) {
+      if (dfsVisit[nextNode]) continue;
       dfs(nextNode);
     }
   };
 
-  dfs(startN);
-  console.log(dfsArr.join(" "));
+  dfs(start);
 
-  const bfs = (startNode) => {
-    const bfsArr = [];
-    const bfsVisited = Array(nodeN + 1).fill(false);
+  const bfsVisit = Array(n + 1).fill(false);
+  const bfsArr = [];
+  const queue = [start];
+  bfsVisit[start] = true;
+  bfsArr.push(start);
 
-    const queue = [startNode];
-    bfsVisited[startNode] = true;
-    bfsArr.push(startNode);
+  while (queue.length) {
+    const node = queue.shift();
 
-    while (queue.length) {
-      const curNode = queue.shift();
-
-      for (const nextNode of graph[curNode]) {
-        if (bfsVisited[nextNode]) continue;
-        bfsArr.push(nextNode);
-        queue.push(nextNode);
-        bfsVisited[nextNode] = true;
-      }
+    for (const nextNode of grid[node]) {
+      if (bfsVisit[nextNode]) continue;
+      bfsVisit[nextNode] = true;
+      bfsArr.push(nextNode);
+      queue.push(nextNode);
     }
+  }
 
-    console.log(bfsArr.join(" "));
-  };
-
-  bfs(startN);
+  console.log(dfsArr.join(" "));
+  console.log(bfsArr.join(" "));
 };
 
 solution(input);
