0b9b858b382c867a26397894bd2f2bd1c92f0ec7
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-01-23 08:26:14 +0900

    [Silver III] Title: N과 M (3), Time: 720 ms, Memory: 149492 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.js
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.js
@@ -4,15 +4,14 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// 자연수 N,M이 주어지고 1부터 N까지 숫자에서 M개의 중복수열
-// 같은 수를 여러번 골라도 된다
-// dfs로 풀이하면된다
 const solution = (input) => {
-  const [n, m] = input[0].split(" ").map((v) => Number(v));
-  const combination = [];
+  const [n, maxLen] = input[0].split(" ").map((v) => Number(v));
+
+  const answer = [];
+
   const dfs = (arr) => {
-    if (arr.length === m) {
-      combination.push(arr.join(" "));
+    if (arr.length === maxLen) {
+      answer.push(arr.join(" "));
       return;
     }
 
@@ -25,12 +24,7 @@ const solution = (input) => {
 
   dfs([]);
 
-  let answer = combination[0];
-  for (let i = 1; i < combination.length; i++) {
-    answer += "\n" + combination[i];
-  }
-
-  console.log(answer);
+  console.log(answer.join("\n"));
 };
 
 solution(input);
