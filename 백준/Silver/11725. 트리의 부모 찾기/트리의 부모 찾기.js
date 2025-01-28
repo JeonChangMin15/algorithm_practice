26614f5e514d02c6022e544308dd50270af5898b
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-01-28 10:13:00 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 584 ms, Memory: 85864 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.js
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.js
@@ -1,55 +1,47 @@
-
+// const input = require("fs")
+//   .readFileSync("example.txt", "utf8")
+//   .trim()
+//   .split("\n");
 
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
-  .split("\n")
-  .map((line) => line.replace(/\r/, ""));
+  .split("\n");
 
-// 첫번째 줄에 노드의 개수
-// 두번째 줄부터는 두 정점들이 주어짐
-// graph를 만들고 1부터 깊이 우선 탐색을 한다
-// dfs로 이전노드와 현재노드를 파라미터로 가져가서 
-// 현재 노드의 인덱스에 이전노드를 넣어주면된다
-// 시간초과 문제는 console.log가 느려서 일일이 출력하면 시간초과가 나올 수 있다.
-
+// grid를 만들고 bfs로 [curNode, parent]로 넣어서 돌리면된다
+// visited로 방문한곳 마킹하면된다
+// 첫째줄에 노드 수 두번째줄부터 정점이 주어진다
+// 루트노드느 1로 고정된다
+// 각 노드들의 부모를 출력하면된다
 const solution = (input) => {
   const n = Number(input[0]);
-  const graph = {};
+  const grid = Array(n + 1)
+    .fill(0)
+    .map((v) => []);
 
-  for (let i = 1; i <= n; i++) {
-    graph[i] = [];
-  }
-
-  for (let i = 1; i < input.length; i++) {
+  for (let i = 1; i < n; i++) {
     const [n1, n2] = input[i].split(" ").map((v) => Number(v));
-    graph[n1].push(n2);
-    graph[n2].push(n1);
+    grid[n1].push(n2);
+    grid[n2].push(n1);
   }
 
-  const parentArr = Array(n + 1).fill(0);
   const visited = Array(n + 1).fill(false);
+  const queue = [1];
+  const answer = Array(n + 1).fill(0);
 
-  const dfs = (node, prev) => {
-    if (visited[node]) return;
-    visited[node] = true;
-    parentArr[node] = prev;
+  while (queue.length) {
+    const curNode = queue.shift();
 
-    for (let i = 0; i < graph[node].length; i++) {
-      const next = graph[node][i];
-      dfs(next, node);
+    for (const nextNode of grid[curNode]) {
+      if (!visited[nextNode]) {
+        visited[nextNode] = true;
+        answer[nextNode] = curNode;
+        queue.push(nextNode);
+      }
     }
-  };
-
-  dfs(1, -1);
-
-  let answer = "";
-
-  for (let i = 2; i < parentArr.length; i++) {
-    answer += parentArr[i] + "\n";
   }
 
-  console.log(answer);
+  console.log(answer.slice(2).join("\n"));
 };
 
 solution(input);
