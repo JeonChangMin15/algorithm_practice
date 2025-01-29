738e491f2374b05f0f51488271ed15194cfbf6b1
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-01-29 13:47:49 +0900

    [Silver III] Title: N과 M (5), Time: 148 ms, Memory: 118248 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -2,16 +2,20 @@ n, sizeN = list(map(int, input().split()))
 nums = list(map(int, input().split()))
 nums.sort()
 
+answer = []
+
 def dfs(arr):
   if len(arr) == sizeN:
-    print(" ".join(list(map(str, arr))))
+    answer.append(" ".join(list(map(str, arr))))
     return
-
-  for num in nums: 
-    if num in arr:
+  
+  for i in range(n):
+    if nums[i] in arr:
       continue
-    arr.append(num)
+    arr.append(nums[i])
     dfs(arr)
     arr.pop()
 
 dfs([])
+
+print("\n".join(answer))
