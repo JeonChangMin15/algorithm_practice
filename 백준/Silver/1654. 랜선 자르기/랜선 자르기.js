86032c37c7a71a980afc8a4efa4ecc5489105658
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-02-06 13:06:26 +0900

    [Silver II] Title: 랜선 자르기, Time: 168 ms, Memory: 12372 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.js
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.js
@@ -4,35 +4,36 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// 첫째줄에 랜선의 개수, 필요한 랜선의 개수가 주어진다
-// 그다음줄부터 길이가 하나씩 주어진다
-// lt = 1 rt = 랜선중 가장 짧은거
-// 이분탐색으로 mid로 나눈 몫을 쭉 더해서 해당 목표보다 작으면 rt = mid -1
-// 해당 목표다 크거나 같으면 lt = mid +1로 구하면될거같다
+// 첫째줄에 선의 갯수와 필요한 선의 개수
+// 가장 길게 자를 수 있는 선분의 길이를 구해라
+// left = 1, right = max()
+// 만약 길이로 나눈 몫들의 합이 타겟보다 크면 선분의 길이를 left = mid +1, 값 갱신
+// 작으면 right = mid - 1
 const solution = (input) => {
-  const [n, target] = input[0].split(" ").map((v) => Number(v));
+  const [n, targetN] = input[0].split(" ").map((v) => Number(v));
   const arr = [];
-  for (let i = 1; i < input.length; i++) {
+
+  for (let i = 1; i <= n; i++) {
     arr.push(Number(input[i]));
   }
-  arr.sort((a, b) => a - b);
 
-  let lt = 1;
-  let rt = arr[n - 1];
+  let left = 1;
+  let right = Math.max(...arr);
   let answer = 1;
 
-  while (lt <= rt) {
-    let mid = Math.floor((lt + rt) / 2);
-    let sum = 0;
+  while (left <= right) {
+    let cnt = 0;
+    let mid = Math.floor((left + right) / 2);
+
     arr.forEach((v) => {
-      sum += Math.floor(v / mid);
+      cnt += Math.floor(v / mid);
     });
 
-    if (sum >= target) {
-      lt = mid + 1;
-      answer = mid;
+    if (cnt >= targetN) {
+      answer = Math.max(answer, mid);
+      left = mid + 1;
     } else {
-      rt = mid - 1;
+      right = mid - 1;
     }
   }
 
