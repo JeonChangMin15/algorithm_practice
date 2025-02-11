5ba3bce3d521a2e8a0a128c33851a02a253cf254
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-02-11 12:27:41 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1332 ms, Memory: 170868 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,34 +1,29 @@
-import heapq
+from collections import deque
 
-cityN, lineN, minDist, startN = list(map(int, input().split()))
-graph = [[] for _ in range(cityN+1)]
+cityN, lineN, targetDist, startCity = list(map(int,input().split()))
+graph = [[] for _ in range(cityN + 1)]
 
 for i in range(lineN):
-  n1, n2 = list(map(int, input().split()))
+  n1, n2 = map(int,input().split())
   graph[n1].append(n2)
 
-cityDist = [float('inf')] * (cityN+1)
-cityDist[startN] = 0
-
-queue = []
-heapq.heappush(queue, [0, startN])
+dist = [float('inf')]*(cityN+1)
+dist[startCity] = 0
+queue = deque()
+queue.append([startCity, 0])
 
 while len(queue):
-  curDist, curCity = heapq.heappop(queue)
-  if curDist > cityDist[curCity]:
-    continue
+  curCity, curDist = queue.popleft()
 
   for nextCity in graph[curCity]:
-    nextDist = curDist + 1
-    if nextDist >= cityDist[nextCity]:
-      continue
-    heapq.heappush(queue, [nextDist, nextCity])
-    cityDist[nextCity] = nextDist
+    if curDist + 1 < dist[nextCity]:
+      dist[nextCity] = curDist + 1
+      queue.append([nextCity, curDist + 1])
 
 answer = []
 
-for i in range(cityN+1):
-  if cityDist[i] == minDist:
+for i in range(1, cityN+1):
+  if dist[i] == targetDist:
     answer.append(i)
 
 print("\n".join(list(map(str, answer))) if len(answer) else -1)
