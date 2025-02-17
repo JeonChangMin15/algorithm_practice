542dedb90bd1062cdffa1b48150921ee7cd6d8d4
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-02-17 12:55:53 +0900

    [Silver III] Title: N과 M (7), Time: 824 ms, Memory: 212300 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.js
@@ -0,0 +1,34 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin", "utf8")
+  .trim()
+  .split("\n")
+  .map((line) => line.replace(/\r/, ""));
+
+const solution = (input) => {
+  const [n, combN] = input[0].split(" ").map((v) => Number(v));
+  const nums = input[1]
+    .split(" ")
+    .map((v) => Number(v))
+    .sort((a, b) => a - b);
+
+  const answer = [];
+
+  const dfs = (arr) => {
+    if (arr.length === combN) {
+      answer.push(arr.join(" "));
+      return;
+    }
+
+    for (let i = 0; i < n; i++) {
+      arr.push(nums[i]);
+      dfs(arr);
+      arr.pop();
+    }
+  };
+
+  dfs([]);
+
+  console.log(answer.join("\n"));
+};
+
+solution(input);
