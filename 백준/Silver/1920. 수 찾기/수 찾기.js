6debf223c55f1ca86dc8d7b2b6cbcc5b58d82e86
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-02-25 19:02:21 +0900

    [Silver IV] Title: 수 찾기, Time: 324 ms, Memory: 42388 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.js
+++ 백준/Silver/1920. 수 찾기/수 찾기.js
@@ -1,50 +1,46 @@
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
-  .split("\n")
-  .map((line) => line.replace(/\r/, ""));
+  .split("\n");
 
-// 첫번째줄에 숫자의 개수 둘째줄에는 숫자 리스트
-// 세번째줄에 숫자 M,네번째줄에 M개의 숫자 리스트가 주어지고 네번째줄에 있는 숫자들이
-// 두번째 줄에 있는 리스트에 포함되는지 판단 존재하면 1 없으면 0
+// 첫번째줄에 N, 두번째줄에 숫자들
+// 세번째줄에 M, 네번째줄에 있는지 확인해야하는 숫자들 리슽
+// 이분탐색으로 두번째 숫자들을 오름차순으로 정렬한 후 네번째줄에 있는 숫자들을 하나씩
+// 탐색한다.
 const solution = (input) => {
   const n = Number(input[0]);
-  const target = input[1]
+  const nums = input[1]
     .split(" ")
     .map((v) => Number(v))
     .sort((a, b) => a - b);
-
   const m = Number(input[2]);
-  const nums = input[3].split(" ").map((v) => Number(v));
+  const testCase = input[3].split(" ").map((v) => Number(v));
 
   const answer = [];
 
-  for (let num of nums) {
-    let isSame = false;
+  for (const target of testCase) {
     let lt = 0;
     let rt = n - 1;
+    let cur = 0;
 
     while (lt <= rt) {
-      let mid = Math.floor((lt + rt) / 2);
+      const mid = Math.floor((lt + rt) / 2);
+      const value = nums[mid];
 
-      if (target[mid] === num) {
-        isSame = true;
+      if (value === target) {
+        cur = 1;
         break;
-      }
-
-      if (num > target[mid]) {
-        lt = mid + 1;
-      } else {
+      } else if (value > target) {
         rt = mid - 1;
+      } else {
+        lt = mid + 1;
       }
     }
 
-    if (isSame) answer.push(1);
-    else answer.push(0);
+    answer.push(cur);
   }
 
-  const str = answer.join("\n");
-  console.log(str);
+  console.log(answer.join("\n"));
 };
 
 solution(input);
