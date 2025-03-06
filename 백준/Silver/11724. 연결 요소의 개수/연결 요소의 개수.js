7f16f332b41cfe52c3cf3265c834ca07fa934b2a
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-03-06 13:19:09 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 852 ms, Memory: 105760 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.js
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.js
@@ -1,38 +1,34 @@
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
-  .split("\n");
+  .split("\n")
+  .map((line) => line.replace(/\r/, ""));
 
-// 첫째줄에 정점의 개수 간선의 갯수가 주어진다
-// 1부터 N까지 dfs로 탐색하면된다
 const solution = (input) => {
-  const [n, m] = input[0].split(" ").map((v) => Number(v));
+  const [nodeN, lineN] = input[0].split(" ").map((v) => Number(v));
+  const graph = Array(nodeN + 1)
+    .fill(0)
+    .map((v) => []);
 
-  const graph = {};
-
-  for (let i = 1; i <= n; i++) {
-    graph[i] = [];
-  }
-
-  for (let i = 1; i < input.length; i++) {
+  for (let i = 1; i <= lineN; i++) {
     const [n1, n2] = input[i].split(" ").map((v) => Number(v));
     graph[n1].push(n2);
     graph[n2].push(n1);
   }
-  const visited = Array(n + 1).fill(false);
+
   let answer = 0;
+  const visited = Array(nodeN + 1).fill(false);
 
   const dfs = (node) => {
     if (visited[node]) return;
     visited[node] = true;
 
-    for (let i = 0; i < graph[node].length; i++) {
-      const nextNode = graph[node][i];
+    for (const nextNode of graph[node]) {
       dfs(nextNode);
     }
   };
 
-  for (let i = 1; i <= n; i++) {
+  for (let i = 1; i <= nodeN; i++) {
     if (visited[i]) continue;
     dfs(i);
     answer += 1;
