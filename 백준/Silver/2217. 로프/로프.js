a71b85426196c5821cd3c3152861d46c32717a0f
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-03-07 08:23:06 +0900

    [Silver IV] Title: 로프, Time: 240 ms, Memory: 26940 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.js
+++ 백준/Silver/2217. 로프/로프.js
@@ -1,29 +1,28 @@
-const input = require("fs").readFileSync("/dev/stdin", "utf8").trim().split("\n");
+const input = require("fs")
+  .readFileSync("/dev/stdin", "utf8")
+  .trim()
+  .split("\n")
+  .map((line) => line.replace(/\r/, ""));
 
-
-// k개의 로프를 사용해서 w인 물체를 들어올린다
-// 로프를 사용해서 최대의 무게를 구해라
-// 먼저 무게를 오름차순으로 정렬
-// 해당무게에다가 갯수의 -1씩 감소시켜서 곱한 값들의 최대값을 출력하면된다
+// k개의 로프를 사용하여 중량이 w인 물체를 올리면 고르게 w/k만큼 중량이 걸린다
+// 각 로프의 정보가 주어질때 들어올릴 수 있는 최대 중량을 구해내라
+// 모든 로프를 사용할 필요는 없고 임의로 몇개의 로프를 골라서 사용해도 된다
+// 첫번째줄에 N이 주어지고 N개의 줄에는 각 로프가 버틸 수 있는 최대 중량이 주어진다
+// 로프는 최대 100000
+// 결국 무게를 오름차순으로 한 후에 해당 무게*(가방갯수 - 인덱스)
 const solution = (input) => {
   const n = Number(input[0]);
-  const weights = [];
-  for (let i = 1; i < input.length; i++) {
-    weights.push(Number(input[i]));
+  const arr = [];
+  for (let i = 1; i <= n; i++) {
+    arr.push(Number(input[i]));
   }
+  arr.sort((a, b) => a - b);
 
-  weights.sort((a, b) => a - b);
-  const limitN = [];
-  for (let i = n; i >= 1; i--) {
-    limitN.push(i);
+  let answer = 0;
+  for (let i = 0; i < n; i++) {
+    answer = Math.max(answer, arr[i] * (n - i));
   }
-
-  const total = [];
-  for (let i = 0; i < weights.length; i++) {
-    total.push(weights[i] * limitN[i]);
-  }
-
-  console.log(Math.max(...total));
+  console.log(answer);
 };
 
 solution(input);
