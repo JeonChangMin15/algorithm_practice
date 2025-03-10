5c47daacfb100fdd48eabcc626ca9b6ee31da67b
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-03-10 09:07:24 +0900

    [Silver II] Title: 부분수열의 합, Time: 424 ms, Memory: 12568 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.js
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.js
@@ -1,38 +1,39 @@
-// const input = require("fs")
-//   .readFileSync("example.txt", "utf8")
-//   .trim()
-//   .split("\n")
-//   .map((line) => line.replace(/\r/, ""));
-
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// N개의 정수로 이루어진 수열에서 다 더한값이 S가되는 경우의 수를 구한다
-// 일단 부분수열을 인덱스로 담아서 판단한다. arr, start가 있는 dfs로 탐색을한다
-// 첫째줄에 숫자들의 개수와 target값이 주어진다
+// 첫번째 줄에 숫자의 수와 목표값이 주어진다
+// 두번째줄에 숫자들이 주어진다
+// 부분수열의 원소값을 다 더한게 목표값이 되는 경우의 수를 구해라
+// 백트레킹으로 인덱스 값들을 중복안되게 넣어서 모든 경우의 수를 체크하면된다
 const solution = (input) => {
-  const [n, target] = input[0].split(" ").map((v) => Number(v));
+  const [n, sumVal] = input[0].split(" ").map((v) => Number(v));
   const nums = input[1].split(" ").map((v) => Number(v));
   let answer = 0;
 
-  const dfs = (arr, start) => {
-    if (arr.length) {
-      const val = arr.map((v) => nums[v]).reduce((prev, cur) => prev + cur, 0);
-      if (val === target) answer += 1;
+  const backTrack = (arr, lastIndex) => {
+    if (lastIndex >= 0) {
+      const total = arr
+        .map((v) => nums[v])
+        .reduce((prev, cur) => prev + cur, 0);
+
+      if (total === sumVal) {
+        answer += 1;
+      }
     }
 
-    for (let i = start; i < n; i++) {
-      arr.push(i);
-      dfs(arr, i + 1);
-      arr.pop();
+    for (let i = 0; i < n; i++) {
+      if (i > lastIndex) {
+        arr.push(i);
+        backTrack(arr, i);
+        arr.pop();
+      }
     }
   };
 
-  dfs([], 0);
-
+  backTrack([], -1);
   console.log(answer);
 };
 
