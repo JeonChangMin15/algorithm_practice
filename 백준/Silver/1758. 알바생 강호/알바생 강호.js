8d0f72ef94e4edceaedce1c0a4635b2077f0f0f6
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-03-12 08:19:14 +0900

    [Silver IV] Title: 알바생 강호, Time: 200 ms, Memory: 27268 KB -BaekjoonHub

--- 백준/Silver/1758. 알바생 강호/알바생 강호.js
+++ 백준/Silver/1758. 알바생 강호/알바생 강호.js
@@ -4,25 +4,21 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// 첫번째줄에는 사람의 수
-// 두번째 줄에는 사람이 주려는 팁의 액수
-// 팁의 액수를 배열에넣어서 내림차순으로 정렬하고
-// 각 금액의 순서 - 1 만큼의 액수를 차감한 후 팁을 지급
-// 팁이 양수일때만 주고 음수면 0원
 const solution = (input) => {
   const n = Number(input[0]);
-  const tips = [];
-  let answer = 0;
-  for (let i = 1; i < input.length; i++) {
-    tips.push(Number(input[i]));
-  }
-  tips.sort((a, b) => b - a);
-  for (let i = 0; i < tips.length; i++) {
-    const money = tips[i] - i;
-    if (money > 0) answer += money;
-  }
+  const arr = [];
 
-  console.log(answer);
+  for (let i = 1; i <= n; i++) {
+    arr.push(Number(input[i]));
+  }
+  arr.sort((a, b) => b - a);
+  let total = 0;
+
+  for (let i = 0; i < n; i++) {
+    const tip = arr[i] - i;
+    if (tip > 0) total += tip;
+  }
+  console.log(total);
 };
 
 solution(input);
