b3dd2cc91430b821c73a33f5089d98104c1639ed
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-03-16 12:27:38 +0900

    [Silver II] Title: 그대, 그머가 되어, Time: 148 ms, Memory: 112592 KB -BaekjoonHub

--- 백준/Silver/14496. 그대， 그머가 되어/그대， 그머가 되어.py
+++ 백준/Silver/14496. 그대， 그머가 되어/그대， 그머가 되어.py
@@ -1,20 +1,19 @@
 from collections import deque
 
 start, end = list(map(int, input().split()))
-n, lineN = list(map(int, input().split()))
-
-graph = [[] for _ in range(n+1)]
+nodeN, lineN = list(map(int, input().split()))
+graph = [[] for _ in range(nodeN+1)]
 
 for i in range(lineN):
   n1, n2 = list(map(int, input().split()))
   graph[n1].append(n2)
   graph[n2].append(n1)
 
-visited = [False]*(n+1)
-visited[start] = True
-
 queue = deque()
 queue.append([start, 0])
+vistied = [False]*(nodeN+1)
+vistied[start] = True
+
 answer = -1
 
 while len(queue):
@@ -24,9 +23,8 @@ while len(queue):
     break
 
   for nextNode in graph[node]:
-    if visited[nextNode]:
-      continue
-    queue.append([nextNode, cnt+1])
-    visited[nextNode] = True
+    if not vistied[nextNode]:
+      queue.append([nextNode, cnt + 1])
+      vistied[nextNode] = True
 
 print(answer)
