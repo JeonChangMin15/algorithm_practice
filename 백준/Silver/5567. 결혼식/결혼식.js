d2e2797830f1815d178e82d35570c40ba0be756b
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-03-20 08:28:30 +0900

    [Silver II] Title: 결혼식, Time: 180 ms, Memory: 14688 KB -BaekjoonHub

--- 백준/Silver/5567. 결혼식/결혼식.js
+++ 백준/Silver/5567. 결혼식/결혼식.js
@@ -4,43 +4,44 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
+// 첫번째줄에 동기수 두번째줄에 관계수가 주어진다
+// 2번째줄부터 관계가 주어진다. 친구와 친구의 친구까지 초대를 한다
+// 초대할 사람의 수를 구해라
+// bfs로 depth가 2이하면 추가를하면 된다
 const solution = (input) => {
-  const n = Number(input[0]);
-  const graph = {};
+  const peopleN = Number(input[0]);
+  const lineN = Number(input[1]);
+  const graph = Array(peopleN + 1)
+    .fill(0)
+    .map((v) => []);
 
-  for (let i = 1; i <= n; i++) {
-    graph[i] = [];
-  }
-
-  for (let i = 2; i < input.length; i++) {
+  for (let i = 2; i < 2 + lineN; i++) {
     const [n1, n2] = input[i].split(" ").map((v) => Number(v));
     graph[n1].push(n2);
     graph[n2].push(n1);
   }
 
-  const visited = Array(n + 1).fill(false);
+  const queue = [[1, 0]];
+  const visited = Array(peopleN + 1).fill(false);
   visited[1] = true;
 
-  const queue = [[1, 0]];
-  let friend = 0;
+  let answer = 0;
 
   while (queue.length) {
-    const [node, depth] = queue.shift();
-    if (depth > 2) break;
-    if (depth > 0 && depth < 3) {
-      friend += 1;
+    const [person, depth] = queue.shift();
+
+    if (depth && depth < 3) {
+      answer += 1;
     }
 
-    for (let i = 0; i < graph[node].length; i++) {
-      const nextNode = graph[node][i];
-      if (visited[nextNode]) continue;
-
-      queue.push([nextNode, depth + 1]);
-      visited[nextNode] = true;
+    for (const nextPerson of graph[person]) {
+      if (visited[nextPerson]) continue;
+      queue.push([nextPerson, depth + 1]);
+      visited[nextPerson] = true;
     }
   }
 
-  console.log(friend);
+  console.log(answer);
 };
 
 solution(input);
