84e6a75d93dc2b8226c7cbafa6acae4fe465a935
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-03-21 08:12:41 +0900

    [Silver IV] Title: 2+1 세일, Time: 236 ms, Memory: 27144 KB -BaekjoonHub

--- 백준/Silver/11508. 2＋1 세일/2＋1 세일.js
+++ 백준/Silver/11508. 2＋1 세일/2＋1 세일.js
@@ -1,34 +1,31 @@
 const input = require("fs")
-  .readFileSync("/dev/stdin", "utf8")
+  .readFileSync(0, "utf8")
   .trim()
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// 첫번째줄에는 제품의 수
-// 두번째 줄부터 제품의 가격들이 출력
-// 3묶음으로 구매하면 가장 싼 가격의 제품은 할인해준다
-// 가장 싼 가격으로 살 수 있는 총 가격
-// 10 4 5 6 6 8
-// 내림차순으로 정렬하고나서 3으로 나눈 나머지가 2이면 값에서 포함시키지 않는다
+// 3개를 구매하면 가장싼 제품 하나는 공짜이다
+// 최소비용으로 구매해야된다
+// 첫번째줄에 유제품의 수 두번째줄에 유제품 가격이 한줄씩 있다
+// 가격 배열을 내림차순으로 정렬한후 해당 인덱스가 3의배수면 제외하면된다
 const solution = (input) => {
   const n = Number(input[0]);
-  const cost = [];
+  const price = [];
 
-  for (let i = 1; i < input.length; i++) {
-    cost.push(Number(input[i]));
+  for (let i = 1; i <= n; i++) {
+    price.push(Number(input[i]));
   }
+  price.sort((a, b) => b - a);
 
-  cost.sort((a, b) => b - a);
+  let answer = 0;
 
-  let total = 0;
-
-  for (let i = 0; i < cost.length; i++) {
+  for (let i = 0; i < n; i++) {
     if (i % 3 !== 2) {
-      total += cost[i];
+      answer += price[i];
     }
   }
 
-  console.log(total);
+  console.log(answer);
 };
 
 solution(input);
