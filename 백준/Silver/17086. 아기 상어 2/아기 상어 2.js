b7c1bb88e4449494e42ae8c562ded3aa1c449fcf
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-03-23 14:08:51 +0900

    [Silver II] Title: 아기 상어 2, Time: 176 ms, Memory: 12676 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.js
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.js
@@ -1,40 +1,46 @@
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
-  .split("\n")
-  .map((line) => line.replace(/\r/, ""));
+  .split("\n");
 
 const solution = (input) => {
   const [rowN, colN] = input[0].split(" ").map((v) => Number(v));
   const grid = [];
 
-  for (let i = 1; i < input.length; i++) {
+  for (let i = 1; i <= rowN; i++) {
     grid.push(input[i].split(" ").map((v) => Number(v)));
   }
+  const dist = Array(rowN)
+    .fill(0)
+    .map((v) => Array(colN).fill(Infinity));
+
   const queue = [];
+
   for (let i = 0; i < rowN; i++) {
     for (let j = 0; j < colN; j++) {
       if (grid[i][j] === 1) {
-        queue.push([i, j, 1]);
+        queue.push([i, j, 0]);
+        dist[i][j] = 0;
       }
     }
   }
-
   const dirs = [
-    [-1, 0],
     [1, 0],
+    [-1, 0],
     [0, 1],
     [0, -1],
-    [-1, -1],
-    [-1, 1],
-    [1, -1],
     [1, 1],
+    [1, -1],
+    [-1, 1],
+    [-1, -1],
   ];
+  let answer = 0;
 
   while (queue.length) {
-    const [x, y, len] = queue.shift();
+    const [x, y, d] = queue.shift();
+    answer = Math.max(answer, d);
 
-    for (let [dx, dy] of dirs) {
+    for (const [dx, dy] of dirs) {
       const nextX = x + dx;
       const nextY = y + dy;
       const isValid =
@@ -42,20 +48,12 @@ const solution = (input) => {
         nextX < rowN &&
         nextY >= 0 &&
         nextY < colN &&
-        grid[nextX][nextY] === 0;
+        d + 1 < dist[nextX][nextY];
 
       if (isValid) {
-        queue.push([nextX, nextY, len + 1]);
-        grid[nextX][nextY] = len + 1;
+        queue.push([nextX, nextY, d + 1]);
+        dist[nextX][nextY] = d + 1;
       }
-    }
-  }
-
-  let answer = 0;
-
-  for (let i = 0; i < rowN; i++) {
-    for (let j = 0; j < colN; j++) {
-      answer = Math.max(grid[i][j] - 1, answer);
     }
   }
 
