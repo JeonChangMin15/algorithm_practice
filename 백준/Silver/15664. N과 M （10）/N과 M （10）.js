44be4971deb3434dca2513802520bddaac28cab8
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-03-25 08:07:30 +0900

    [Silver II] Title: N과 M (10), Time: 96 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.js
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.js
@@ -4,22 +4,27 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// arr, start가 들어가는 dfs 조합
-// 중복되는지 확인해야된다
+// 첫번째줄에 숫자의 수와 조합수가 주어진다
+// 두번째줄에 숫자들이 주어진다
+// 오름차순으로 조합을 만들어야하는데 수열에 중복된 인덱스를 넣으면 안되고
+// 그리고 중복된 조합이 있으면 안된다
 const solution = (input) => {
-  const [n, m] = input[0].split(" ").map((v) => Number(v));
+  const [n, combN] = input[0].split(" ").map((v) => Number(v));
   const nums = input[1]
     .split(" ")
     .map((v) => Number(v))
     .sort((a, b) => a - b);
 
-  const combination = [];
+  const answer = [];
+  const set = new Set();
 
   const dfs = (arr, start) => {
-    if (arr.length === m) {
+    if (arr.length === combN) {
       const val = arr.map((v) => nums[v]).join(" ");
-
-      if (!combination.includes(val)) combination.push(val);
+      if (!set.has(val)) {
+        set.add(val);
+        answer.push(val);
+      }
 
       return;
     }
@@ -33,12 +38,7 @@ const solution = (input) => {
 
   dfs([], 0);
 
-  let answer = combination[0];
-
-  for (let i = 1; i < combination.length; i++) {
-    answer += "\n" + combination[i];
-  }
-  console.log(answer);
+  console.log(answer.join("\n"));
 };
 
 solution(input);
