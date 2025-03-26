9fdf4f454aa88d8b2c8bf14725aae690ca03904f
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-03-27 08:05:05 +0900

    [Silver II] Title: 섬의 개수, Time: 148 ms, Memory: 241716 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,27 +1,30 @@
+import sys
+sys.setrecursionlimit(10**5)
+
+dirs = [[-1, 0], [1, 0], [0, -1], [0, 1], [-1, -1], [-1, 1], [1, -1], [1, 1]]
 while True:
   colN, rowN = list(map(int, input().split()))
-  if colN == 0 and rowN == 0:
+  
+  if rowN == 0 and colN ==0:
     break
-
   grid = []
+
   for i in range(rowN):
     grid.append(list(map(int, input().split())))
-  
-  dirs = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
-  answer = 0
 
   def dfs(x, y):
-    if x < 0 or x >= rowN or y < 0 or y >= colN or grid[x][y] ==0:
+    if x < 0 or x >= rowN or y < 0 or y >= colN or grid[x][y] ==0 :
       return
+
     grid[x][y] = 0
-
     for dx, dy in dirs:
-      dfs(x+dx, y + dy)
+      dfs(x +dx, y + dy)
 
+  answer = 0
   for i in range(rowN):
     for j in range(colN):
-      if grid[i][j] == 1:
-        answer += 1
+      if grid[i][j]:
         dfs(i, j)
+        answer += 1
 
   print(answer)
