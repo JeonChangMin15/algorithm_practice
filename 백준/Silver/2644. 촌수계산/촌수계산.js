9c53b9b69402f07e70e46779d401d13321bd7cd3
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-04-01 08:13:09 +0900

    [Silver II] Title: 촌수계산, Time: 96 ms, Memory: 9388 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.js
+++ 백준/Silver/2644. 촌수계산/촌수계산.js
@@ -1,46 +1,47 @@
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
-  .split("\n");
+  .split("\n")
+  .map((line) => line.replace(/\r/, ""));
 
-
-// 첫번째줄에는 총 인원수 두번째줄에는 촌수를 계산해야하는 두개의 사람숫자
-// 3번째줄은 관계수 4번째줄부터 관계
-// dfs로 노드 탐색해야하는데 부모 자식 관계이니깐 양방향 그래프
-// 만약 촌수가 없으면 -1 출력
+// 첫번째줄에 인원수가 주어진다 1부터 N
+// 두번째줄에는 촌수 계산해야하는 서로다른 두사람 번호
+// 셋째줄에는 부모 자식간의 관계 개수가 주어진다
+// 네번째줄부터는 부모 자식간의 관계를 부모 자식이 주어진다
+// 촌수를 구하고 만약 관계가 없으면 -1을 출력해라
+// 먼저 그래프로 각 관계를 다 넣고 dfs로 하나씩 탐색하면 된다
 const solution = (input) => {
-  const n = Number(input[0]);
-  const [p1, p2] = input[1].split(" ").map((v) => Number(v));
-  const graph = {};
+  const peopleN = Number(input[0]);
+  const [start, end] = input[1].split(" ").map((v) => Number(v));
+  const lineN = Number(input[2]);
+  const graph = Array(peopleN + 1)
+    .fill(0)
+    .map((v) => []);
 
-  for (let i = 1; i <= n; i++) {
-    graph[i] = [];
-  }
-
-  for (let i = 3; i < input.length; i++) {
+  for (let i = 3; i < 3 + lineN; i++) {
     const [n1, n2] = input[i].split(" ").map((v) => Number(v));
     graph[n1].push(n2);
     graph[n2].push(n1);
   }
 
-  const visited = Array(n + 1).fill(false);
+  const visited = Array(peopleN + 1).fill(false);
   let answer = -1;
 
-  const dfs = (node, depth) => {
+  const dfs = (node, relation) => {
     if (visited[node]) return;
-    if (node === p2) {
-      answer = depth;
-      return;
-    }
     visited[node] = true;
 
-    for (let i = 0; i < graph[node].length; i++) {
-      const nextNode = graph[node][i];
-      dfs(nextNode, depth + 1);
+    if (node === end) {
+      answer = relation;
+      return;
+    }
+
+    for (const nextNode of graph[node]) {
+      dfs(nextNode, relation + 1);
     }
   };
 
-  dfs(p1, 0);
+  dfs(start, 0);
 
   console.log(answer);
 };
