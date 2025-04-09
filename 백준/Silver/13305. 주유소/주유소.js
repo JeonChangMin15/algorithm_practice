c0986ac6128504df9e958d364d7005352df38411
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-04-10 08:12:35 +0900

    [Silver III] Title: 주유소, Time: 184 ms, Memory: 20080 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -0,0 +1,32 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin", "utf8")
+  .trim()
+  .split("\n")
+  .map((line) => line.replace(/\r/, ""));
+
+// 첫번째줄에 도시의 갯수가 주어지고
+// 두번째줄에는 도로의 길이가 주어진다
+// 세번째줄에는 각 도시마다 리터당 가격이다
+// 도로의 길이만큼 for문 돌리면서 주유소의 가격의 최소인걸 갱신해주면된다
+const solution = (input) => {
+  const cityN = Number(input[0]);
+  const cityLen = input[1].split(" ").map((v) => Number(v));
+  const cost = input[2].split(" ").map((v) => Number(v));
+
+  let minCost = cost[0];
+  let totalCost = 0;
+
+  for (let i = 0; i < cityLen.length; i++) {
+    const curCost = cost[i];
+
+    if (curCost < minCost) {
+      minCost = curCost;
+    }
+
+    totalCost += cityLen[i] * minCost;
+  }
+
+  console.log(totalCost);
+};
+
+solution(input);
