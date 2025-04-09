379d13fc337580316c93f7bd8ef997cc970009a9
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-04-10 08:15:38 +0900

    [Silver III] Title: 주유소, Time: 272 ms, Memory: 44732 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -9,24 +9,22 @@ const input = require("fs")
 // 세번째줄에는 각 도시마다 리터당 가격이다
 // 도로의 길이만큼 for문 돌리면서 주유소의 가격의 최소인걸 갱신해주면된다
 const solution = (input) => {
-  const cityN = Number(input[0]);
-  const cityLen = input[1].split(" ").map((v) => Number(v));
-  const cost = input[2].split(" ").map((v) => Number(v));
+  const cityN = BigInt(input[0]); // cityN은 사실 계산에 안 쓰이지만, BigInt 처리
+  const cityLen = input[1].split(" ").map((v) => BigInt(v));
+  const cost = input[2].split(" ").map((v) => BigInt(v));
 
   let minCost = cost[0];
-  let totalCost = 0;
+  let totalCost = 0n;
 
   for (let i = 0; i < cityLen.length; i++) {
     const curCost = cost[i];
-
     if (curCost < minCost) {
       minCost = curCost;
     }
-
     totalCost += cityLen[i] * minCost;
   }
 
-  console.log(totalCost);
+  console.log(totalCost.toString()); // BigInt는 문자열로 출력해야 함
 };
 
 solution(input);
