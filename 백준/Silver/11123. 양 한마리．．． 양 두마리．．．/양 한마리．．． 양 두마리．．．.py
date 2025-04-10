32edd9714f1d3a00add471dd1aa4026ed04f6619
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-04-11 08:11:39 +0900

    [Silver II] Title: 양 한마리... 양 두마리..., Time: 228 ms, Memory: 250980 KB -BaekjoonHub

--- 백준/Silver/11123. 양 한마리．．． 양 두마리．．．/양 한마리．．． 양 두마리．．．.py
+++ 백준/Silver/11123. 양 한마리．．． 양 두마리．．．/양 한마리．．． 양 두마리．．．.py
@@ -1,35 +1,35 @@
 import sys
 sys.setrecursionlimit(10**5)
 
-n = int(input())
+testN = int(input())
 
-def solution():
-  rowN, colN = list(map(int, input().split()))
+def dfs(grid, x, y, rowN, colN):
+  if x < 0 or x >= rowN or y < 0 or y >= colN or grid[x][y] == '.':
+    return
+  grid[x][y] = '.'
+
+  dfs(grid, x-1, y, rowN, colN)
+  dfs(grid, x+1, y, rowN, colN)
+  dfs(grid, x, y-1, rowN, colN)
+  dfs(grid, x, y+1, rowN, colN)
+
+def searchShip():
+  rowN, colN = list(map(int,input().split()))
   grid = []
-
-  for i in range(rowN):
-    grid.append(list(input()))
-
-  def dfs(x, y):
-    if x < 0 or x >= rowN or y < 0 or y >= colN or grid[x][y] == '.':
-      return
-
-    grid[x][y] = '.'
-    dfs(x - 1, y)
-    dfs(x + 1, y)
-    dfs(x, y -1)
-    dfs(x, y + 1)
-
   answer = 0
 
   for i in range(rowN):
+    grid.append(list(map(str, input())))
+
+  for i in range(rowN):
     for j in range(colN):
-      if grid[i][j] =='#':
+      if grid[i][j] == '#':
+        dfs(grid, i, j, rowN, colN)
         answer += 1
-        dfs(i, j)
 
   print(answer)
+
   
 
-for i in range(n):
-  solution()
+for i in range(testN):
+  searchShip()
