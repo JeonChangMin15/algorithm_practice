793cd4e8146e23661002fe87afe7a4eb84b6660e
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-04-13 12:24:14 +0900

    [Silver I] Title: 숨바꼭질, Time: 3172 ms, Memory: 18628 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -3,43 +3,36 @@ const input = require("fs")
   .trim()
   .split("\n");
 
-// const input = require("fs")
-//   .readFileSync("example.txt", "utf8")
-//   .trim()
-//   .split("\n");
-
-// N에 있고 동생은 K에 있다 걷거나 순간이동 가능하다
-// 1초후에 X+1, x-1 순가이동하는 경우 2*X 1초후에
-// 수빈이와 동생의 위치가 정해져였을때 가장 빠른시간을 구해라
-// 첫째줄에 수빈이가 있는 위치 동생 위치
-// bfs로 방문한 지점은 체크하고 하면된다
+// 첫번째줄에 수빈이 위치와 동생 위치가 주어짐
+// 1초후에 앞뒤 한칸 or 2배 위치로 이동 가능
+// 동생 위치로 갈 수 있는 최소 시간을 구해라
+// bfs로 각 조건마다 visited 검사하면서 가면된다
 const solution = (input) => {
-  const [start, target] = input[0].split(" ").map((v) => Number(v));
-  const visited = Array(100001).fill(false);
-
+  const [start, end] = input[0].split(" ").map((v) => Number(v));
   const queue = [[start, 0]];
+  const visited = Array(100001).fill(false);
   visited[start] = true;
 
   while (queue.length) {
-    const [position, time] = queue.shift();
-    if (position === target) {
+    const [pos, time] = queue.shift();
+    if (pos === end) {
       console.log(time);
-      return;
+      break;
     }
 
-    if (position * 2 <= 100000 && !visited[position * 2] && position < target) {
-      queue.push([position * 2, time + 1]);
-      visited[position * 2] = true;
+    if (pos * 2 <= 100000 && !visited[pos * 2]) {
+      queue.push([pos * 2, time + 1]);
+      visited[pos * 2] = true;
     }
 
-    if (!visited[position + 1] && position < target) {
-      queue.push([position + 1, time + 1]);
-      visited[position + 1] = true;
+    if (pos - 1 >= 0 && !visited[pos - 1]) {
+      queue.push([pos - 1, time + 1]);
+      visited[pos - 1] = true;
     }
 
-    if (position >= 0 && !visited[position - 1]) {
-      queue.push([position - 1, time + 1]);
-      visited[position - 1] = true;
+    if (pos + 1 <= 100000 && !visited[pos + 1]) {
+      queue.push([pos + 1, time + 1]);
+      visited[pos + 1] = true;
     }
   }
 };
