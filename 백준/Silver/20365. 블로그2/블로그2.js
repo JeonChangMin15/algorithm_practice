6e08d6903c99e74c2e55deeda63e47d2f25e4fc9
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-04-16 08:26:43 +0900

    [Silver III] Title: 블로그2, Time: 156 ms, Memory: 10576 KB -BaekjoonHub

--- 백준/Silver/20365. 블로그2/블로그2.js
+++ 백준/Silver/20365. 블로그2/블로그2.js
@@ -1,30 +1,33 @@
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
-  .split("\n");
+  .split("\n")
+  .map((line) => line.replace(/\r/, ""));
 
-
-// 첫번째줄에는 문제의 수 두번째줄에는 RB가 공백없이 주어진다
-// 덩어리의 개수를 각각 구해야되나?
+// 첫번째줄에 문제의 수, 두번째줄에 N개의 문자가 공백없이 주어진다
+// 연속된건 하나의 색깔로 생각하면된다
+//
 const solution = (input) => {
   const n = Number(input[0]);
-  const arr = input[1].split("");
+  const colorWork = input[1];
+  let prevColor = "";
+  let blueN = 0;
+  let redN = 0;
 
-  let blue = 0;
-  let red = 0;
-  let prev = "";
+  for (let i = 0; i < n; i++) {
+    const color = colorWork[i];
+    if (color === "R" && color !== prevColor) {
+      redN += 1;
+      prevColor = "R";
+    }
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== prev) {
-      prev = arr[i];
-      if (arr[i] === "R") red += 1;
-      else blue += 1;
+    if (color === "B" && color !== prevColor) {
+      blueN += 1;
+      prevColor = "B";
     }
   }
 
-  const min = Math.min(blue, red);
-
-  console.log(min + 1);
+  console.log(Math.min(blueN, redN) + 1);
 };
 
 solution(input);
