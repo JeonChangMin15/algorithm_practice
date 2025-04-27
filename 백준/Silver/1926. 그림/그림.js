865fb5864c319107a7318c4ef8061c1a2e96ff71
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-04-27 13:32:47 +0900

    [Silver I] Title: 그림, Time: 660 ms, Memory: 41084 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.js
+++ 백준/Silver/1926. 그림/그림.js
@@ -1,51 +1,41 @@
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
-  .split("\n")
-  .map((line) => line.replace(/\r/, ""));
+  .split("\n");
 
-// 영역의 개수와 가장큰 영역을 각각 출력해라
-// dfs로 영역 탐색을하면된다.
-// 첫째줄에 rowN, colN
-// 두번째줄부터 그리드가 주어진다
 const solution = (input) => {
   const [rowN, colN] = input[0].split(" ").map((v) => Number(v));
   const grid = [];
-  for (let i = 1; i < input.length; i++) {
+
+  for (let i = 1; i <= rowN; i++) {
     grid.push(input[i].split(" ").map((v) => Number(v)));
   }
 
-  const dfs = (row, col) => {
-    if (
-      row < 0 ||
-      row >= rowN ||
-      col < 0 ||
-      col >= colN ||
-      grid[row][col] === 0
-    )
-      return 0;
+  let areaCnt = 0;
+  let maxArea = 0;
 
-    grid[row][col] = 0;
+  const dfs = (x, y) => {
+    if (x < 0 || x >= rowN || y < 0 || y >= colN || grid[x][y] === 0) return 0;
+    grid[x][y] = 0;
 
-    const up = dfs(row - 1, col);
-    const down = dfs(row + 1, col);
-    const left = dfs(row, col - 1);
-    const right = dfs(row, col + 1);
+    const up = dfs(x - 1, y);
+    const down = dfs(x + 1, y);
+    const left = dfs(x, y - 1);
+    const right = dfs(x, y + 1);
 
     return 1 + up + down + left + right;
   };
-  const answer = [];
+
   for (let i = 0; i < rowN; i++) {
     for (let j = 0; j < colN; j++) {
       if (grid[i][j] === 0) continue;
-      const area = dfs(i, j);
-      answer.push(area);
+      areaCnt += 1;
+      maxArea = Math.max(maxArea, dfs(i, j));
     }
   }
-  answer.sort((a, b) => b - a);
-  console.log(answer.length);
-  const max = answer.length === 0 ? 0 : answer[0];
-  console.log(max);
+
+  console.log(areaCnt);
+  console.log(maxArea);
 };
 
 solution(input);
