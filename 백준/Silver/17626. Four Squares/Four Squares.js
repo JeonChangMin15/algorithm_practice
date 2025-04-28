d121a955a156d5ecadfd09867af3d2d0ae700572
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-04-29 08:36:13 +0900

    [Silver III] Title: Four Squares, Time: 228 ms, Memory: 10520 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.js
+++ 백준/Silver/17626. Four Squares/Four Squares.js
@@ -1,72 +1,51 @@
-// const input = require("fs")
-//   .readFileSync("example.txt", "utf8")
-//   .trim()
-//   .split("\n")
-//   .map((line) => line.replace(/\r/, ""));
-
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// 모든 자연수는 4개 이하의 제곱수의 합으로 표현이 가능하다
-// 제곱수들의 최소 개수를 출력한다.
-// 먼저 해당 숫자의 제곱근보다 작거나 같은 숫자를 찾는다
-// 백트래킹으로는 시간초과가 나온다
-
+// 모든 자연수는 넷혹은 그 이하의 제곱수의 합으로 표현이 가능하다
+// n을 최소개수 제곱수의 합으로 표현해라
+// 1부터 Math.sqrt(n)까지 전부다 for문으로 돌려봐야 알 수 있나
 const solution = (input) => {
   const n = Number(input[0]);
-  const sqrt = Math.floor(Math.sqrt(n));
+  const sqrtN = Math.floor(Math.sqrt(n));
+  let answer = 5;
 
-  const nums = [];
-  for (let i = 1; i <= sqrt; i++) {
-    nums.push(i);
-  }
-
-  for (let i = 0; i < nums.length; i++) {
-    const sum = nums[i] ** 2;
-    if (sum === n) {
-      console.log(1);
-      return;
-    }
-  }
-
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length; j++) {
-      const sum = nums[i] ** 2 + nums[j] ** 2;
-      if (sum === n) {
-        console.log(2);
-        return;
+  for (let i = 1; i <= sqrtN; i++) {
+    for (let j = 1; j <= sqrtN; j++) {
+      for (let k = 1; k <= sqrtN; k++) {
+        const res = n - i * i - j * j - k * k;
+        if (Math.sqrt(res) === Math.floor(Math.sqrt(res))) answer = 4;
       }
     }
   }
 
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length; j++) {
-      for (let k = 0; k < nums.length; k++) {
-        const sum = nums[i] ** 2 + nums[j] ** 2 + nums[k] ** 2;
-        if (sum === n) {
-          console.log(3);
-          return;
+  for (let i = 1; i <= sqrtN; i++) {
+    for (let j = 1; j <= sqrtN; j++) {
+      for (let k = 1; k <= sqrtN; k++) {
+        if (i * i + j * j + k * k === n) {
+          answer = 3;
         }
       }
     }
   }
 
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length; j++) {
-      for (let k = 0; k < nums.length; k++) {
-        for (let l = 0; l < nums.length; l++) {
-          const sum = nums[i] ** 2 + nums[j] ** 2 + nums[k] ** 2 + nums[l] ** 2;
-          if (sum === n) {
-            console.log(4);
-            return;
-          }
-        }
+  for (let i = 1; i <= sqrtN; i++) {
+    for (let j = 1; j <= sqrtN; j++) {
+      if (i * i + j * j === n) {
+        answer = 2;
       }
     }
   }
+
+  for (let i = 1; i <= sqrtN; i++) {
+    if (i * i === n) {
+      answer = 1;
+    }
+  }
+
+  console.log(answer);
 };
 
 solution(input);
