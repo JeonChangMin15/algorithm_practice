17cf85343ab8362ecd075b54db0b0cf9d27b4393
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-04-30 08:12:21 +0900

    [Silver II] Title: 차이를 최대로, Time: 192 ms, Memory: 12964 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.js
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.js
@@ -4,40 +4,37 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// 첫째줄에 N이 주어진다. 둘째줄에는 배열 A에 들어가있는 정수가 주어진다
-// Math.abs(a[0]-a[1]) + ... Math.abs(a[n-2] - a[n-1])
-// 최대값을 출력해야된다
-// 모든 경우의 조합을 따져봐야한다
-// 그래서 len === n일때 값을 계산하면될거같다
-
+// 첫번째줄에 N이 주어진다
+// 두번째줄에 숫자들이 주어진다
+// 각 위치에서 뒤에 있는 숫자와 차이의 합 총합 최대값을 구해라
+// 백트래킹으로 숫자들의 조합을 구한뒤 하나씩 계산하면된다
 const solution = (input) => {
   const n = Number(input[0]);
   const nums = input[1].split(" ").map((v) => Number(v));
-
   let answer = 0;
 
-  const dfs = (arr) => {
+  const backTrack = (arr) => {
     if (arr.length === n) {
-      let total = 0;
+      const realValues = arr.map((v) => nums[v]);
+      let sum = 0;
+
       for (let i = 0; i < n - 1; i++) {
-        const idx = arr[i];
-        const nextIdx = arr[i + 1];
-        total += Math.abs(nums[idx] - nums[nextIdx]);
+        sum += Math.abs(realValues[i] - realValues[i + 1]);
       }
 
-      answer = Math.max(total, answer);
+      answer = Math.max(answer, sum);
       return;
     }
 
     for (let i = 0; i < n; i++) {
       if (arr.includes(i)) continue;
       arr.push(i);
-      dfs(arr);
+      backTrack(arr);
       arr.pop();
     }
   };
 
-  dfs([]);
+  backTrack([]);
 
   console.log(answer);
 };
