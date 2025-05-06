eacc2103d1c24cf37cad0ec746f78817970628f8
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-05-07 08:41:55 +0900

    [Silver I] Title: 현명한 나이트, Time: 196 ms, Memory: 122048 KB -BaekjoonHub

--- 백준/Silver/18404. 현명한 나이트/현명한 나이트.py
+++ 백준/Silver/18404. 현명한 나이트/현명한 나이트.py
@@ -1,32 +1,36 @@
 from collections import deque
 
-dirs = [[-2,-1],[-2,1],[-1,-2],[-1,2],[1,-2],[1,2],[2,-1],[2,1]]
-n, targetN = list(map(int, input().split()))
-startX, startY = list(map(int, input().split()))
-target = []
+n,m = list(map(int,input().split()))
+startX, startY = list(map(int,input().split()))
+positions = []
 
-for i in range(targetN):
-  x, y = list(map(int, input().split()))
-  target.append([x-1, y-1])
-
-grid = [[float('inf')]*n for _ in range(n)]
-queue = deque()
-queue.append([startX-1, startY-1, 0])
-grid[startX-1][startY-1] = 0
-
-while len(queue):
-  x,y,dist = queue.popleft()
-
-  for dx, dy in dirs:
-    nextX = x +dx
-    nextY = y +dy
-    isValid = 0<=nextX<n and 0<=nextY<n and dist+1 < grid[nextX][nextY]
-    if isValid:
-      queue.append([nextX,nextY, dist+1])
-      grid[nextX][nextY] = dist + 1
+for i in range(m):
+  x, y = list(map(int,input().split()))
+  positions.append([x-1, y-1])
 
 answer = []
-for x,y in target:
-  answer.append(grid[x][y])
+dirs = [[-2, -1],[-2, 1],[-1, -2],[-1, 2],[1, -2],[1, 2],[2, -1],[2, 1]]
+visited = [[float('inf')]*n for _ in range(n)]
+
+def bfs():
+  queue = deque()
+  queue.append([startX-1, startY-1, 0])
+
+  while(len(queue)):
+    x,y,cnt = queue.popleft()
+
+    for dx, dy in dirs:
+      nextX = x + dx
+      nextY = y + dy
+      isValid = 0<=nextX<n and 0<=nextY<n and cnt+1 < visited[nextX][nextY]
+
+      if isValid:
+        queue.append([nextX, nextY, cnt+1])
+        visited[nextX][nextY] = cnt +1
+
+bfs()
+
+for x,y in positions:
+  answer.append(visited[x][y])
 
 print(" ".join(list(map(str, answer))))
