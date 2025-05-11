c307bd1c89300febca9bc86109d829531cf9b510
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-05-12 08:13:35 +0900

    [Silver V] Title: 돌 게임, Time: 148 ms, Memory: 9388 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.js
+++ 백준/Silver/9655. 돌 게임/돌 게임.js
@@ -4,19 +4,25 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// 탁자위에 돌 N개 상근이와 창여이가 턴을 번갈아가면서 돌을 가져간다
-// 1개 또는 3개 마지막돌을 가져가는 사람이이긴다
-// 두 사람이 완벽하게 게임을 했을때
-// 상근이가 이기면 SK, 창영이가 이기면 CY
-
+// 탁자위에 돌 N개가 있다. 상근이와 창영이는 턴을 번갈아가며 돌을 가져가며
+// 1개 또는 3개를 가져갈 수 있다. 마지막돌을 가져가는 사람이 이긴다
+// 두사람이 완변하게 게임을 할때 이기는 사람을 구해라. 게임은 상근이가 시작
+// 첫번째줄에 돌의 갯수가 주어진다. 상근이가 이기면 SK, 창영이가 이기면 CY
+// Array(N+1)을 생성한후 dp[1] =1 dp[2] =2 dp[3] = 1
+// 그리고 dp[i] = Math.min(dp[i-1]+1, dp[i-3]+1)을 한후 dp[i] %2가 1이면 상근
+// 0이면 CY
 const solution = (input) => {
   const n = Number(input[0]);
+  const dp = Array(n + 1).fill(0);
 
-  if (n % 2 === 1) {
-    console.log("SK");
-  } else {
-    console.log("CY");
+  dp[1] = 1;
+  dp[2] = 2;
+  dp[3] = 1;
+
+  for (let i = 4; i <= n; i++) {
+    dp[i] = Math.min(dp[i - 1] + 1, dp[i - 3] + 1);
   }
+  console.log(dp[n] % 2 === 1 ? "SK" : "CY");
 };
 
 solution(input);
