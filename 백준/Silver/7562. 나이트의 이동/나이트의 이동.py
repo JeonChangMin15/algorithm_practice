1ddd0dc2f60f14da9d094471fcd7297f31223333
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-05-23 08:23:00 +0900

    [Silver I] Title: 나이트의 이동, Time: 484 ms, Memory: 141904 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -1,32 +1,32 @@
 from collections import deque
 
-def bfs(n, startx, starty, endx, endy):
-  dist = [[float('inf')]*n for _ in range(n)]
-  dirs = [[-1, -2],[-2, -1],[-2, 1],[-1, 2],[1,2],[2,1],[2,-1],[1,-2]]
-  dist[startx][starty] = 0
+n = int(input())
+dirs = [[-1,-2],[-2,-1],[-2,1],[-1,2],[1,-2],[2,-1],[2,1],[1,2]]
 
+def bfs(size, startx, starty, endx, endy):
+  grid = [[float('inf')]*size for _ in range(size)]
+  grid[startx][starty] = 0
   queue = deque()
   queue.append([startx, starty, 0])
 
   while len(queue):
-    curx, cury, curdist = queue.popleft()
-    if curx == endx and cury == endy:
-      print(curdist)
+    x, y, cnt = queue.popleft()
+    if x == endx and y == endy:
+      print(cnt)
       break
-    
+
     for dx, dy in dirs:
-      nextX = curx + dx
-      nextY = cury + dy
-      isValid = 0<=nextX<n and 0<=nextY<n and curdist +1 < dist[nextX][nextY]
+      nextX = x + dx
+      nextY = y + dy
+      isValid = 0<=nextX<size and 0<=nextY<size and cnt +1 < grid[nextX][nextY]
       if isValid:
-        queue.append([nextX, nextY, curdist+1])
-        dist[nextX][nextY] = curdist + 1
+        queue.append([nextX, nextY, cnt +1])
+        grid[nextX][nextY] = cnt + 1
 
-
-n = int(input())
 
 for i in range(n):
-  sizeN = int(input())
-  sx, sy = list(map(int, input().split()))
-  ex, ey = list(map(int, input().split()))
-  bfs(sizeN, sx, sy, ex, ey)
+  s = int(input())
+  sx, sy = map(int, input().split())
+  ex, ey = map(int, input().split())
+  bfs(s, sx, sy, ex, ey)
+  
