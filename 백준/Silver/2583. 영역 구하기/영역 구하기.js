84b84f4b8f8b9b07ebf7a319458798849ca88b26
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-05-26 08:13:03 +0900

    [Silver I] Title: 영역 구하기, Time: 156 ms, Memory: 11180 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.js
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.js
@@ -4,47 +4,48 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
+// 첫번째줄에 rown, coln, 직사각형 갯수
+// 두번재줄부터 직사각형 좌표가 x1,y1,x2,y2
+// 직사각형 영역을 제외한 나머지 영역들의 넓이를 오름차순
+// 먼저 그리드로 1로 만들고 직사각형 영역을 다 0으로 마킹
+// 그 다음 dfs로
 const solution = (input) => {
   const [rowN, colN, squareN] = input[0].split(" ").map((v) => Number(v));
   const grid = Array(rowN)
     .fill(0)
-    .map((v) => Array(colN).fill(false));
+    .map((v) => Array(colN).fill(1));
 
-  for (let i = 1; i < input.length; i++) {
+  for (let i = 1; i <= squareN; i++) {
     const [y1, x1, y2, x2] = input[i].split(" ").map((v) => Number(v));
 
-    for (let j = x1; j < x2; j++) {
-      for (let k = y1; k < y2; k++) {
-        grid[j][k] = true;
+    for (let i = x1; i < x2; i++) {
+      for (let j = y1; j < y2; j++) {
+        grid[i][j] = 0;
       }
     }
   }
 
-  const dfs = (row, col) => {
-    if (row < 0 || row >= rowN || col < 0 || col >= colN || grid[row][col])
-      return 0;
-
-    grid[row][col] = true;
-
-    const down = dfs(row + 1, col);
-    const up = dfs(row - 1, col);
-    const right = dfs(row, col + 1);
-    const left = dfs(row, col - 1);
-
-    return 1 + up + down + right + left;
-  };
   const answer = [];
+
+  const dfs = (x, y) => {
+    if (x < 0 || x >= rowN || y < 0 || y >= colN || grid[x][y] === 0) return 0;
+    grid[x][y] = 0;
+    const up = dfs(x - 1, y);
+    const down = dfs(x + 1, y);
+    const left = dfs(x, y - 1);
+    const right = dfs(x, y + 1);
+
+    return 1 + up + down + left + right;
+  };
+
   for (let i = 0; i < rowN; i++) {
     for (let j = 0; j < colN; j++) {
-      if (grid[i][j]) continue;
-      const area = dfs(i, j);
-      answer.push(area);
+      if (grid[i][j] === 0) continue;
+      answer.push(dfs(i, j));
     }
   }
-
-  answer.sort((a, b) => a - b);
   console.log(answer.length);
-  console.log(answer.join(" "));
+  console.log(answer.sort((a, b) => a - b).join(" "));
 };
 
 solution(input);
