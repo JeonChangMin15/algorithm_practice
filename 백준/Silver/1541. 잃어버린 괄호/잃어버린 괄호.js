9b68a9928dcd34cb11e39a95c8e4474d70b8b0a7
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-05-28 08:26:18 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 92 ms, Memory: 9348 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
@@ -1,40 +1,26 @@
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
-  .split("\n");
+  .split("\n")
+  .map((line) => line.replace(/\r/, ""));
 
-// 가장 처음과 마지막은 무조건 숫자
-// - + 기호가 주어지고 괄호를 잘 사용해서 최솟값을 구한다
-// 일단 숫자 기호를 분리해서 배열에다가 넣는다
-// 10 - 5 + 20 - 9 + 19 +10
-// 기호가 한번이라도 마이너스가 나오면 그때부터는 계속 빼주면된다
-
-
+// 가장 처음과 마지막 문자는 숫자. 연속해서 두개 이사으이 연산자는 없다
+// 먼저 -로 스플릿한다음에
+// 해당 문자열을 하나씩 +로 스플릿한다
+// 첫번째만 더하고 나머지는 싹다 빼주면된다
 const solution = (input) => {
-  const example = input[0];
-  let val = "";
-  const arr = [];
+  const sample = input[0];
+  const minusSplit = sample.split("-");
 
-  for (let i = 0; i < example.length; i++) {
-    if (example[i] === "-" || example[i] === "+") {
-      arr.push(Number(val));
-      arr.push(example[i]);
-      val = "";
-    } else {
-      val += String(example[i]);
-    }
-  }
-
-  arr.push(Number(val));
-
-  let isMinus = false;
   let answer = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === "+") continue;
-    if (arr[i] === "-") isMinus = true;
-    if (arr[i] !== "+" && arr[i] !== "-") {
-      if (isMinus) answer -= arr[i];
-      else answer += arr[i];
+
+  for (let i = 0; i < minusSplit.length; i++) {
+    const plusSplit = minusSplit[i].split("+").map((v) => Number(v));
+    const total = plusSplit.reduce((prev, cur) => prev + cur, 0);
+    if (i === 0) {
+      answer += total;
+    } else {
+      answer -= total;
     }
   }
 
