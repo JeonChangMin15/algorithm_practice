4d9d8325609b19961ea3fa9995cc6447aea952f5
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-06-02 09:27:27 +0900

    [Silver I] Title: 현수막, Time: 264 ms, Memory: 24596 KB -BaekjoonHub

--- 백준/Silver/14716. 현수막/현수막.js
+++ 백준/Silver/14716. 현수막/현수막.js
@@ -4,49 +4,50 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// 섬이 몇개인지 구하는 문제로 풀면된다
-// dfs로 푸는데 상하좌우 대각선까지 연결된걸로 생각한다
-// 1만 탐색하고 방문한 지점은 0으로 체킹하면 된다
-// 첫째줄에 rowN, colN이 주어지고 그 다음줄 부터는 그리드
+// 첫번째줄에 rowN, colN 두번째줄부터 그리드가 주어진다
+// 1로 연결된 영역의 수를 구하는데 상하좌우 대각선이 연결이다
+// dfs로 탐색하면서 지나간곳은 0으로 마킹하고 이중 for문으로 1인곳만 탐색해서
+// 카운팅을 증가
 const solution = (input) => {
   const [rowN, colN] = input[0].split(" ").map((v) => Number(v));
   const grid = [];
 
-  for (let i = 1; i < input.length; i++) {
+  for (let i = 1; i <= rowN; i++) {
     grid.push(input[i].split(" ").map((v) => Number(v)));
   }
 
   const dirs = [
     [-1, 0],
     [1, 0],
-    [0, 1],
     [0, -1],
-    [1, 1],
-    [1, -1],
+    [0, 1],
     [-1, 1],
     [-1, -1],
+    [1, 1],
+    [1, -1],
   ];
 
   const dfs = (x, y) => {
     if (x < 0 || x >= rowN || y < 0 || y >= colN || grid[x][y] === 0) return;
     grid[x][y] = 0;
 
-    for (let [dx, dy] of dirs) {
+    for (const [dx, dy] of dirs) {
       dfs(x + dx, y + dy);
     }
   };
 
-  let area = 0;
+  let answer = 0;
 
   for (let i = 0; i < rowN; i++) {
     for (let j = 0; j < colN; j++) {
-      if (grid[i][j] === 0) continue;
-      dfs(i, j);
-      area += 1;
+      if (grid[i][j] === 1) {
+        dfs(i, j);
+        answer += 1;
+      }
     }
   }
 
-  console.log(area);
+  console.log(answer);
 };
 
 solution(input);
