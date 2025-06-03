4a4e35889c7320ced03e32f5fffee1cce7e6ea47
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-06-04 08:55:26 +0900

    [Silver III] Title: 계단 오르기, Time: 88 ms, Memory: 9368 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.js
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.js
@@ -4,34 +4,27 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// 첫번째줄에 계단의 수 그다음줄부터는 점수
-// 계단은 한번에 한개 or 두개만 오를 수 있다
-// 연속된 세 개의 계단을 모두 밟으면 안된다
-// 123안됨 마지막 도착계단은 반드시 밟아야한다
-// 최대값을 구해야된다\
-// 거꾸로 위에서 아래로 내려간다고 생각해보자
-// [전전칸, 전칸]의 누적합을 넣어줘야한다
+// 첫번째줄에 계단의 수가 주어지고 각 계단의 점수가 주어진다
+// 한번에 계단을 한 계단, 두 계단씩 가능 연속 세개는 안된다
+// 마지막 계단은 반드시 밟아야한다
+// i번째는 dp[i] = arr[i]+arr[i-1] + dp[i-3], arr[i]+dp[i-2]
 const solution = (input) => {
   const n = Number(input[0]);
-  const score = [];
-  for (let i = 1; i < input.length; i++) {
-    score.push(Number(input[i]));
+  const arr = [0];
+
+  for (let i = 1; i <= n; i++) {
+    arr.push(Number(input[i]));
   }
 
-  const dp = Array(n)
-    .fill(0)
-    .map((v) => [0, 0]);
-  dp[0] = [score[0], score[0]];
-  dp[1] = [score[1], score[0] + score[1]];
+  const dp = Array(n + 1).fill(0);
+  dp[1] = arr[1];
+  dp[2] = arr[1] + arr[2];
 
-  for (let i = 2; i < n; i++) {
-    dp[i] = [
-      Math.max(dp[i - 2][1] + score[i], dp[i - 2][0] + score[i]),
-      dp[i - 1][0] + score[i],
-    ];
+  for (let i = 3; i <= n; i++) {
+    dp[i] = Math.max(arr[i] + arr[i - 1] + dp[i - 3], arr[i] + dp[i - 2]);
   }
 
-  console.log(Math.max(dp[n - 1][0], dp[n - 1][1]));
+  console.log(dp[n]);
 };
 
 solution(input);
