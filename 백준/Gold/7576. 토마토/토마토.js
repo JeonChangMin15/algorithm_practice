bdbe2328bbe35078422a90c4ac19b0f0b1e3371e
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-06-30 13:17:15 +0900

    [Gold V] Title: 토마토, Time: 812 ms, Memory: 180744 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.js
+++ 백준/Gold/7576. 토마토/토마토.js
@@ -1,56 +1,36 @@
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
-  .split("\n");
+  .split("\n")
+  .map((line) => line.replace(/\r/, ""));
 
-// 첫째줄에는 M가로칸수(Col), N(세로칸수(Row)
-// 둘째줄부터 토마토의 정보가 주어진다
-// 1은 익음, 0은 안익음, -1은 토마토가 없다
-// 토마토가 다 익을때 까지 최소 날짜를 출력
-// 토마토가 하나라도 익지 못한다면 -1을 출력 처음부터 다 익어있으면 0
-// 일단 이중for문으로 익은 토마토가 있는 위치를 다 넣고 시작읋 한다
-// bfs로 상하좌우 탐색을 해서 0일때만 큐에다가 넣고 day +1
-// 다 끝나면 하나라도 0이 있으면 -1 출력하면된다
-
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
-}
-
-// 큐 클래스
 class Queue {
   constructor() {
-    this.head = null; // 제일 앞 노드
-    this.rear = null; // 제일 뒤 노드
-    this.length = 0; // 노드의 길이
+    this._arr = [];
+    this._head = 0;
   }
 
-  enqueue(data) {
-    // 노드 추가.
-    const node = new Node(data); // data를 가진 node를 만들어준다.
-    if (!this.head) {
-      // 헤드가 없을 경우 head를 해당 노드로
-      this.head = node;
-    } else {
-      this.rear.next = node; // 아닐 경우 마지막의 다음 노드로
-    }
-    this.rear = node; // 마지막을 해당 노드로 한다.
-    this.length++;
+  // 큐 뒤에 삽입
+  enqueue(value) {
+    this._arr.push(value);
   }
 
+  // 큐에서 꺼내기
   dequeue() {
-    // 노드 삭제.
-    if (!this.head) {
-      // 헤드가 없으면 한 개도 없는 것이므로 false를 반환.
-      return false;
+    if (this.isEmpty()) return undefined;
+    const val = this._arr[this._head];
+    this._head++;
+    // 메모리 방치 방지: 헤드가 배열 절반을 넘으면 앞쪽 요소들 잘라내기
+    if (this._head * 2 >= this._arr.length) {
+      this._arr = this._arr.slice(this._head);
+      this._head = 0;
     }
-    const data = this.head.data; // head를 head의 다음 것으로 바꿔주고 뺀 data를 return
-    this.head = this.head.next;
-    this.length--;
+    return val;
+  }
 
-    return data;
+  // 비었는지 검사
+  isEmpty() {
+    return this._head >= this._arr.length;
   }
 }
 
@@ -58,18 +38,11 @@ const solution = (input) => {
   const [colN, rowN] = input[0].split(" ").map((v) => Number(v));
   const grid = [];
 
-  for (let i = 1; i < input.length; i++) {
+  for (let i = 1; i <= rowN; i++) {
     grid.push(input[i].split(" ").map((v) => Number(v)));
   }
 
   const queue = new Queue();
-  const dirs = [
-    [-1, 0],
-    [1, 0],
-    [0, 1],
-    [0, -1],
-  ];
-  let day = 0;
 
   for (let i = 0; i < rowN; i++) {
     for (let j = 0; j < colN; j++) {
@@ -79,14 +52,22 @@ const solution = (input) => {
     }
   }
 
-  while (queue.length) {
-    const [x, y, n] = queue.dequeue();
-    day = Math.max(day, n);
+  let day = 0;
 
-    for (let [dx, dy] of dirs) {
+  const dirs = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+  ];
+
+  while (!queue.isEmpty()) {
+    const [x, y, d] = queue.dequeue();
+    day = Math.max(day, d);
+
+    for (const [dx, dy] of dirs) {
       const nextX = x + dx;
       const nextY = y + dy;
-
       const isValid =
         nextX >= 0 &&
         nextX < rowN &&
@@ -95,22 +76,21 @@ const solution = (input) => {
         grid[nextX][nextY] === 0;
 
       if (isValid) {
-        queue.enqueue([nextX, nextY, n + 1]);
+        queue.enqueue([nextX, nextY, d + 1]);
         grid[nextX][nextY] = 1;
       }
     }
   }
 
+  let isValid = true;
+
   for (let i = 0; i < rowN; i++) {
     for (let j = 0; j < colN; j++) {
-      if (grid[i][j] === 0) {
-        console.log(-1);
-        return;
-      }
+      if (grid[i][j] === 0) isValid = false;
     }
   }
 
-  console.log(day);
+  console.log(isValid ? day : -1);
 };
 
 solution(input);
