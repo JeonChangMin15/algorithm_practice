01540e7fc510c49cf3c5c72413e987e78325590e
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-07-01 08:43:24 +0900

    [Silver IV] Title: 카드 놓기, Time: 156 ms, Memory: 11324 KB -BaekjoonHub

--- 백준/Silver/5568. 카드 놓기/카드 놓기.js
+++ 백준/Silver/5568. 카드 놓기/카드 놓기.js
@@ -4,39 +4,37 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// 첫째줄에 카드 N장 둘째줄에는 K장을 선택
-// 셋째줄부터 카드숫자가 주어짐
-// 만들 수 있는 정수는 몇가지인가?
-// dfs 조합으로 set으로 카운팅하면될거같다
-
+// 첫번째줄에 숫자의 개수 두번째줄에 숫자 고르는 개수
+// 3번째줄부터 주어진다
+// 숫자를 조합해서 만들 수 있는 정수의 개수를 구해라
+// 중복없는 모든 조합을 구해서 set에 넣어서 구하면된다
 const solution = (input) => {
   const n = Number(input[0]);
-  const k = Number(input[1]);
-  const cards = [];
+  const combN = Number(input[1]);
+  const nums = [];
 
-  for (let i = 2; i < input.length; i++) {
-    cards.push(Number(input[i]));
+  for (let i = 2; i < n + 2; i++) {
+    nums.push(input[i]);
   }
 
   const set = new Set();
 
-  const dfs = (arr) => {
-    if (arr.length === k) {
-      const num = arr.map((v) => cards[v]).join("");
-      set.add(num);
+  const backTrack = (arr) => {
+    if (arr.length === combN) {
+      const combNum = arr.reduce((prev, cur) => prev + nums[cur], "");
+      set.add(combNum);
       return;
     }
 
-    for (let i = 0; i < cards.length; i++) {
-      if (!arr.includes(i)) {
-        arr.push(i);
-        dfs(arr);
-        arr.pop();
-      }
+    for (let i = 0; i < n; i++) {
+      if (arr.includes(i)) continue;
+      arr.push(i);
+      backTrack(arr);
+      arr.pop();
     }
   };
 
-  dfs([]);
+  backTrack([]);
 
   console.log(set.size);
 };
