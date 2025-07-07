4fc7f7e41b37f15328f3ea1d877ecfe9fd36e418
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-07-07 09:45:23 +0900

    [Silver II] Title: 꽃길, Time: 4636 ms, Memory: 23076 KB -BaekjoonHub

--- 백준/Silver/14620. 꽃길/꽃길.js
+++ 백준/Silver/14620. 꽃길/꽃길.js
@@ -1,94 +1,62 @@
-
-
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// 3개의 씨앗을 뿌리고 중앙 상하좌우 5칸을 차지한다
-// 3개의 꽃이 겹치지 않도록, 밖으로 나가지않도록 뿌리면서 가장 낮은 가격으로
-// 임대할 수 있도록  해야된다.
-// 첫째줄에는 길이 둘째줄에는 각 지역마다 가격이 찍힌 그리드가 주어짐
-// 3중 for문으로 0-> n*n전까지 탐색하면서 몫을 row 나머지를 col로한다
-// 근데 셋중 하나라도 몫이 0, 나머지가 나머지가0, 몫이 n-1 나머지가 n-1이면 스킵
-// 상하좌우의 위치가 총 15개면 price 계산
+// 첫번째줄에 격자 크기, 두번째줄부터 그리드가 주어진다
+// 서로다른 씨앗을 3개를 심어서 하나도 안겹치게해서 최소비용을 구한다
+// 삼중 for문으로 set을 넣고 15개면 계산하는 방식
 const solution = (input) => {
   const n = Number(input[0]);
-  const prices = [];
-  for (let i = 1; i < input.length; i++) {
-    prices.push(input[i].split(" ").map((v) => Number(v)));
+  const grid = [];
+  const dirs = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+    [0, 0],
+  ];
+
+  for (let i = 1; i <= n; i++) {
+    grid.push(input[i].split(" ").map((v) => Number(v)));
   }
 
-  let cost = Infinity;
+  let answer = Infinity;
 
-  for (let i = 0; i < n * n - 1; i++) {
-    for (let j = 0; j < n * n - 1; j++) {
-      for (let k = 0; k < n * n - 1; k++) {
-        if (
-          parseInt(i / n) === 0 ||
-          i % n === 0 ||
-          parseInt(i / n) === n - 1 ||
-          i % n === n - 1 ||
-          parseInt(j / n) === 0 ||
-          j % n === 0 ||
-          parseInt(j / n) === n - 1 ||
-          j % n === n - 1 ||
-          parseInt(k / n) === 0 ||
-          k % n === 0 ||
-          parseInt(k / n) === n - 1 ||
-          k % n === n - 1
-        )
-          continue;
-
-        const firstRow = parseInt(i / n);
-        const firstCol = i % n;
-        const secondRow = parseInt(j / n);
-        const secondCol = j % n;
-        const thirdRow = parseInt(k / n);
-        const thirdCol = k % n;
-
-        const position = [
-          [firstRow, firstCol],
-          [secondRow, secondCol],
-          [thirdRow, thirdCol],
-        ];
-
-        const dirs = [
-          [-1, 0],
-          [1, 0],
-          [0, -1],
-          [0, 1],
-        ];
-
+  for (let i = 0; i < n * n; i++) {
+    for (let j = 0; j < n * n; j++) {
+      for (let k = 0; k < n * n; k++) {
         const set = new Set();
-        set.add(`${firstRow}${firstCol}`);
-        set.add(`${secondRow}${secondCol}`);
-        set.add(`${thirdRow}${thirdCol}`);
+        const coors = [
+          [Math.floor(i / n), i % n],
+          [Math.floor(j / n), j % n],
+          [Math.floor(k / n), k % n],
+        ];
 
-        let sideCost = 0;
-
-        for (let [row, col] of position) {
-          for (let [dx, dy] of dirs) {
-            sideCost += prices[row + dx][col + dy];
-            set.add(`${row + dx}${col + dy}`);
+        for (const [x, y] of coors) {
+          for (const [dx, dy] of dirs) {
+            if (x + dx >= 0 && x + dx < n && y + dy >= 0 && y + dy < n) {
+              set.add(`${x + dx} ${y + dy}`);
+            }
           }
         }
 
         if (set.size === 15) {
-          cost = Math.min(
-            cost,
-            sideCost +
-              prices[firstRow][firstCol] +
-              prices[secondRow][secondCol] +
-              prices[thirdRow][thirdCol]
-          );
+          let cost = 0;
+          for (const [x, y] of coors) {
+            for (const [dx, dy] of dirs) {
+              cost += grid[x + dx][y + dy];
+            }
+          }
+
+          answer = Math.min(cost, answer);
         }
       }
     }
   }
 
-  console.log(cost);
+  console.log(answer);
 };
 
 solution(input);
