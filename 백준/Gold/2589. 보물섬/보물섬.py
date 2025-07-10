f4f3baec3c33ab320335ed42c77760e3c5369885
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-07-10 20:11:29 +0900

    [Gold V] Title: 보물섬, Time: 708 ms, Memory: 121448 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.py
+++ 백준/Gold/2589. 보물섬/보물섬.py
@@ -1,40 +1,38 @@
 from collections import deque
 
-rowN, colN = list(map(int, input().split()))
+rowN, colN = list(map(int,input().split()))
 grid = []
 
 for i in range(rowN):
-  grid.append(list(input()))
+  grid.append(list(map(str, input())))
 
 dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-
 def bfs(startX, startY):
   queue = deque()
   queue.append([startX, startY, 0])
-  visited = [[False]*colN for _ in range(rowN)]
-  maxDist = 0
-  visited[startX][startY] = True
+  timeGrid = [[float('inf')]*colN for _ in range(rowN)]
+  timeGrid[startX][startY] = 0
+  maxTime = 0
 
   while len(queue):
-    x, y, dist = queue.popleft()
-    maxDist = max(dist, maxDist)
+    x, y, t = queue.popleft()
+    maxTime = max(maxTime, t)
 
     for dx, dy in dirs:
       nextX = x + dx
       nextY = y + dy
-      isValid = 0<=nextX<rowN and 0<=nextY<colN and grid[nextX][nextY]=='L' and not visited[nextX][nextY]
+      isValid = 0<=nextX<rowN and 0<=nextY<colN and t+1<timeGrid[nextX][nextY] and grid[nextX][nextY] == 'L'
       if isValid:
-        queue.append([nextX, nextY, dist + 1])
-        visited[nextX][nextY] = True
+        queue.append([nextX, nextY, t+1])
+        timeGrid[nextX][nextY] = t + 1
 
-  return maxDist
+  return maxTime
 
 answer = 0
 
 for i in range(rowN):
   for j in range(colN):
     if grid[i][j] == 'L':
-      dist = bfs(i, j)
-      answer = max(dist, answer)
+      answer = max(answer, bfs(i, j))
 
 print(answer)
