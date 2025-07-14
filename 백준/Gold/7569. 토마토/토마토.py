13fc08bbc2037c073376bdfed5a9161f3775b274
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-07-14 13:31:19 +0900

    [Gold V] Title: 토마토, Time: 544 ms, Memory: 205212 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,42 +1,37 @@
 from collections import deque
 
-colN, rowN, heightN = list(map(int, input().split()))
-grid = [[] for _ in range(heightN)]
-
+colN, rowN, height = list(map(int, input().split()))
+grid = []
 queue = deque()
 
-for i in range(heightN):
+for i in range(height):
+  arr = []
   for j in range(rowN):
-    grid[i].append(list(map(int, input().split())))
+    arr.append(list(map(int, input().split())))
+  grid.append(arr)
 
-for z in  range(heightN):
+for z in range(height):
   for x in range(rowN):
     for y in range(colN):
       if grid[z][x][y] == 1:
         queue.append([z,x,y,0])
 
 dirs = [[-1,0,0],[1,0,0],[0,-1,0],[0,1,0],[0,0,-1],[0,0,1]]
-
 answer = 0
-
 while len(queue):
-  z,x,y,day = queue.popleft()
-  answer = max(answer, day)
+  z,x,y, time = queue.popleft()
+  answer = max(answer, time)
 
-  for dz, dx, dy in dirs:
+  for dz, dx,dy in dirs:
     nextZ = z + dz
     nextX = x + dx
     nextY = y + dy
-    isValid = (0<=nextZ<heightN and 
-               0<=nextX<rowN and 
-               0<=nextY<colN and
-               grid[nextZ][nextX][nextY] == 0)
-
+    isValid = 0<=nextZ<height and 0<=nextX<rowN and 0<=nextY<colN and grid[nextZ][nextX][nextY] == 0
     if isValid:
-      queue.append([nextZ, nextX, nextY, day + 1])
+      queue.append([nextZ, nextX, nextY, time + 1])
       grid[nextZ][nextX][nextY] = 1
 
-for z in range(heightN):
+for z in range(height):
   for x in range(rowN):
     for y in range(colN):
       if grid[z][x][y] == 0:
