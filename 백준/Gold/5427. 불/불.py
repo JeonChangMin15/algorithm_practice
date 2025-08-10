1ec94180741bc104b6c6af3290afffa67ba2e660
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-08-10 15:35:12 +0900

    [Gold IV] Title: 불, Time: 628 ms, Memory: 234756 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.py
+++ 백준/Gold/5427. 불/불.py
@@ -1,57 +1,56 @@
 from collections import deque
 
-n = int(input())
+testN = int(input())
 
 def bfs():
   global answer
 
   colN, rowN = list(map(int, input().split()))
   grid = []
-
-  for i in range(rowN):
-    grid.append(list(input()))
-
   queue = deque()
 
-  for i in range(rowN):
-    for j in range(colN):
-      if grid[i][j] == '@':
-        queue.append([i, j, 0])
+  for _ in range(rowN):
+    grid.append(list(input()))
 
   for i in range(rowN):
     for j in range(colN):
       if grid[i][j] == '*':
-        queue.append([i, j, 0]) 
+        queue.append([i, j, '*', 0])
 
-  answer = 0
-  dirs = [[-1, 0],[1, 0],[0,-1],[0,1]]
+  for i in range(rowN):
+    for j in range(colN):
+      if grid[i][j] == '@':
+        queue.append([i, j, '@', 0])
+
+  dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+  answer = 'IMPOSSIBLE'
 
   while len(queue):
-    x, y, time = queue.popleft()
-    if grid[x][y] =='@' and (x == 0 or y == 0 or x ==rowN-1 or y == colN-1):
-      answer = time + 1
+    x, y, prevType, time = queue.popleft()
+
+    if prevType == '@' and (x == 0 or x == rowN-1 or y == 0 or y == colN-1):
+      answer = time+1
       break
-
-    if grid[x][y] == '@':
+    
+    if prevType == '*':
       for dx, dy in dirs:
         nextX = x + dx
         nextY = y + dy
-        isValid = 0<=nextX<rowN and 0<=nextY<colN and grid[nextX][nextY] == '.'
-        if isValid:
-          queue.append([nextX, nextY, time + 1])
-          grid[nextX][nextY] = '@'
-
-    if grid[x][y] == '*':
-      for dx, dy in dirs:
-        nextX = x + dx
-        nextY = y + dy
-        isValid = 0<=nextX<rowN and 0<=nextY<colN and (grid[nextX][nextY] =='.' or grid[nextX][nextY]=='@')
-        if isValid:
-          queue.append([nextX, nextY, time + 1])
+        isNextFire = 0<=nextX<rowN and 0<=nextY<colN and (grid[nextX][nextY] == '.' or grid[nextX][nextY] == '@')
+        if isNextFire:
+          queue.append([nextX, nextY, prevType, time+1])
           grid[nextX][nextY] = '*'
 
-  print(answer if answer != 0 else 'IMPOSSIBLE')
+    if prevType == '@':
+      for dx, dy in dirs:
+        nextX = x + dx
+        nextY = y + dy
+        isNextMove = 0<=nextX<rowN and 0<=nextY<colN and grid[nextX][nextY] == '.'
+        if isNextMove:
+          queue.append([nextX, nextY,prevType, time+1])
+          grid[nextX][nextY] = '@'
 
+  print(answer)
 
-for i in range(n):
+for _ in range(testN):
   bfs()
