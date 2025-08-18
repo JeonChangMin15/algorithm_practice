3225599e4db0ede105299a7f303828672bcf059a
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-08-18 10:21:20 +0900

    [Gold IV] Title: 빙산, Time: 2060 ms, Memory: 56308 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.js
+++ 백준/Gold/2573. 빙산/빙산.js
@@ -1,16 +1,17 @@
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
-  .split("\n");
+  .split("\n")
+  .map((line) => line.replace(/\r/, ""));
 
-// 첫번째줄에 rowN, colN이 주어진다
+// 첫번째 줄에 rowN, colN이 주어진다
 // 두번재줄부터 그리드가 주어진다
-// 빙산의 높이가 해당 칸에 동서남북 네 방향으로 붙어있는 0의 개수만큼 줄어든다
-// 0보다 더 줄어들지는 않는다 두 덩어리 이상으로 분리되는 최초의 시간을 구한다
-// 만약 전부 녹을때까지 분리안되면 0을 출력
-// 먼저 이중 for문으로 해당 그리드가 0이면 상하좌우 탐색해서 0보다 큰 지점이 있으면
-// 해당 좌표를 저장한다 그리고나서 해당 좌표들을 -1씩해준다
-// 그리고나서 시간 +1을 해주고 dfs로 영역의 수를 탐색하는 방식으로 하면된다
+// 각 영역에서 상하좌우 인접한 0 갯수만큼 높이가 줄어든다
+// 줄어든 후 영역의 수가 두개 이상되는 최소한의 년수를 구해라
+// 먼저 grid를 하나 만들고 영역수가 1개 이상일때 계속 돌린다
+// for문으로 전부 다 각 영역마다 인접한 0의 수를 구한다음에
+// Math.max(cur-zero, 0)으로 갱신한 후 day +1
+// 그리고나서 영역의 수 구하고 두개 이상이면 answer = 갱신
 const solution = (input) => {
   const [rowN, colN] = input[0].split(" ").map((v) => Number(v));
   const grid = [];
@@ -19,28 +20,8 @@ const solution = (input) => {
     grid.push(input[i].split(" ").map((v) => Number(v)));
   }
 
-  let day = 0;
-  let isValid = false;
-  let isContinue = true;
-
-  const dfs = (x, y, visited) => {
-    if (
-      x < 0 ||
-      x >= rowN ||
-      y < 0 ||
-      y >= colN ||
-      visited[x][y] ||
-      grid[x][y] === 0
-    )
-      return;
-
-    visited[x][y] = true;
-
-    dfs(x - 1, y, visited);
-    dfs(x + 1, y, visited);
-    dfs(x, y - 1, visited);
-    dfs(x, y + 1, visited);
-  };
+  let yearAnswer = 0;
+  let answer = 0;
 
   const dirs = [
     [-1, 0],
@@ -49,61 +30,78 @@ const solution = (input) => {
     [0, 1],
   ];
 
-  while (isContinue) {
+  const dfs = (x, y, vistiedGrid) => {
+    if (
+      x < 0 ||
+      x >= rowN ||
+      y < 0 ||
+      y >= colN ||
+      vistiedGrid[x][y] ||
+      grid[x][y] === 0
+    )
+      return;
+    vistiedGrid[x][y] = true;
+
+    dfs(x - 1, y, vistiedGrid);
+    dfs(x + 1, y, vistiedGrid);
+    dfs(x, y - 1, vistiedGrid);
+    dfs(x, y + 1, vistiedGrid);
+  };
+
+  while (true) {
+    const zeroCountGrid = Array(rowN)
+      .fill(0)
+      .map((v) => Array(colN).fill(0));
     const visited = Array(rowN)
       .fill(0)
       .map((v) => Array(colN).fill(false));
 
-    let area = 0;
-    const coordinates = [];
-
-    for (let i = 0; i < rowN; i++) {
-      for (let j = 0; j < colN; j++) {
-        if (grid[i][j] === 0 || visited[i][j]) continue;
-        dfs(i, j, visited);
-        area += 1;
-      }
-    }
-
-    if (area > 1) {
-      isValid = true;
-      break;
-    }
-    if (area === 0) {
-      isValid = false;
-      break;
-    }
-
-    for (let i = 0; i < rowN; i++) {
-      for (let j = 0; j < colN; j++) {
-        if (grid[i][j] > 0) continue;
+    for (let x = 0; x < rowN; x++) {
+      for (let y = 0; y < colN; y++) {
         for (const [dx, dy] of dirs) {
-          const nextX = i + dx;
-          const nextY = j + dy;
-          const isValid =
+          const nextX = x + dx;
+          const nextY = y + dy;
+          const isCnt =
             nextX >= 0 &&
             nextX < rowN &&
             nextY >= 0 &&
             nextY < colN &&
-            grid[nextX][nextY] > 0;
-
-          if (isValid) {
-            coordinates.push([nextX, nextY]);
+            grid[nextX][nextY] === 0;
+          if (isCnt) {
+            zeroCountGrid[x][y] += 1;
           }
         }
       }
     }
 
-    for (const [x, y] of coordinates) {
-      if (grid[x][y] > 0) {
-        grid[x][y] -= 1;
+    for (let i = 0; i < rowN; i++) {
+      for (let j = 0; j < colN; j++) {
+        grid[i][j] = Math.max(grid[i][j] - zeroCountGrid[i][j], 0);
       }
     }
 
-    day += 1;
+    let areaCnt = 0;
+
+    for (let i = 0; i < rowN; i++) {
+      for (let j = 0; j < colN; j++) {
+        if (visited[i][j] || grid[i][j] === 0) continue;
+        dfs(i, j, visited);
+        areaCnt += 1;
+      }
+    }
+
+    yearAnswer += 1;
+
+    if (areaCnt > 1) {
+      answer = yearAnswer;
+      break;
+    }
+    if (areaCnt === 0) {
+      break;
+    }
   }
 
-  console.log(isValid ? day : 0);
+  console.log(answer);
 };
 
 solution(input);
