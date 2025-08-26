21f8ac7af887dc7844bced88b0d391504a3b91d3
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-08-26 09:09:14 +0900

    [Gold IV] Title: 탈출, Time: 104 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.js
+++ 백준/Gold/3055. 탈출/탈출.js
@@ -1,29 +1,25 @@
-// const input = require("fs")
-//   .readFileSync("example.txt", "utf8")
-//   .trim()
-//   .split("\n")
-//   .map((line) => line.replace(/\r/, ""));
-
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// 첫째줄에 rowN, colN이 주어진다
-// 그다음줄부터  .은 비어있는곳, *은 물, X는 돌이 있다
-// 시작점은 S, 비버의 굴은 D로 각각 하나씩만 있다
-// 굴로 이동하기 위해 필요한 최소 시간을 구해야된다
-// 매분 상하좌우로 이동할 수 있다. 물도 똑같이 확장이된다
-// 물은 비어있는곳만 침투가능하다. 물이 찰 예정인 칸으로 이동이 안된다
-// bfs로 탐색을하는데 먼저 물부터 탐색해서 큐에넣고 그다음에 시작점을 넣는다
-// 물을 상하좌우로 탐색해서 전염시키고 그당므에 비어있는곳이면 S로 만든다
-// 만약 도착안되면 KAKTUS를 출력한다
+// 첫번째줄에 rowN, colN이 주어진다
+// 두번째줄부터 그리드가 주어지고 D(비버), S(고슴 도치)
+// .(비이었음), *(물 지역), X(돌) 이루어져있다
+// 분마다 고슴도치는 상하좌우, 물도 가능. 돌은 통과 못한다
+// 물은 비버의 소굴은 이동 못한다.
+// 고슴 도치가 비버의 굴로 이동하기 위한 최소시간을 구해라
+// 이동 못하면 KAKTUS 출력
+// 큐에 먼저 물 좌표를 넣어주고 [x,y,0, 'water']
+// 고슴도치를 마지막에 넣어준다. 그리고나서 물은 다음지역이 ., S면 간다
+// 고슴도치는 다음지역 빈 공간이거나, D면 간다
+// 만약 현재 위차가 D면 time으로 바꾸고 break
 const solution = (input) => {
   const [rowN, colN] = input[0].split(" ").map((v) => Number(v));
   const grid = [];
 
-  for (let i = 1; i < input.length; i++) {
+  for (let i = 1; i <= rowN; i++) {
     grid.push(input[i].split(""));
   }
 
@@ -31,71 +27,74 @@ const solution = (input) => {
   const visited = Array(rowN)
     .fill(0)
     .map((v) => Array(colN).fill(false));
-  let endX;
-  let endY;
-
-  for (let i = 0; i < rowN; i++) {
-    for (let j = 0; j < colN; j++) {
-      if (grid[i][j] === "S") {
-        queue.push([i, j, 0]);
-        visited[i][j] = true;
-      }
-      if (grid[i][j] === "D") {
-        endX = i;
-        endY = j;
-      }
-    }
-  }
 
   for (let i = 0; i < rowN; i++) {
     for (let j = 0; j < colN; j++) {
       if (grid[i][j] === "*") {
-        queue.push([i, j, 0]);
+        queue.push([i, j, 0, "water"]);
+      }
+    }
+  }
+
+  for (let i = 0; i < rowN; i++) {
+    for (let j = 0; j < colN; j++) {
+      if (grid[i][j] === "S") {
+        queue.push([i, j, 0, "고슴도치"]);
         visited[i][j] = true;
       }
     }
   }
 
-  let answer = "KAKTUS";
-  let isEnd = false;
   const dirs = [
     [-1, 0],
     [1, 0],
-    [0, 1],
     [0, -1],
+    [0, 1],
   ];
 
-  while (queue.length > 0 && !isEnd) {
-    const [x, y, cnt] = queue.shift();
+  let answer = "KAKTUS";
 
-    if (x === endX && y === endY) {
-      answer = cnt;
+  while (queue.length) {
+    const [x, y, time, type] = queue.shift();
+
+    if (grid[x][y] === "D" && type === "고슴도치") {
+      answer = time;
       break;
     }
 
-    for (const [dx, dy] of dirs) {
-      const nextX = x + dx;
-      const nextY = y + dy;
-      const isValid = nextX >= 0 && nextX < rowN && nextY >= 0 && nextY < colN;
-      if (!isValid) continue;
+    if (type === "water") {
+      for (const [dx, dy] of dirs) {
+        const nextX = x + dx;
+        const nextY = y + dy;
+        const isValid =
+          nextX >= 0 &&
+          nextX < rowN &&
+          nextY >= 0 &&
+          nextY < colN &&
+          (grid[nextX][nextY] === "." || grid[nextX][nextY] === "S");
 
-      if (grid[x][y] === "S") {
-        if (grid[nextX][nextY] === ".") {
-          queue.push([nextX, nextY, cnt + 1]);
-          grid[nextX][nextY] = "S";
-        }
-
-        if (grid[nextX][nextY] === "D") {
-          queue.push([nextX, nextY, cnt + 1]);
-          answer = cnt + 1;
-          isEnd = true;
+        if (isValid) {
+          queue.push([nextX, nextY, time + 1, "water"]);
+          grid[nextX][nextY] = "*";
         }
       }
+    }
 
-      if (grid[x][y] === "*") {
-        if (grid[nextX][nextY] === "." || grid[nextX][nextY] === "S") {
-          queue.push([nextX, nextY, cnt + 1]);
-          grid[nextX][nextY] = "*";
+    if (type === "고슴도치") {
+      for (const [dx, dy] of dirs) {
+        const nextX = x + dx;
+        const nextY = y + dy;
+        const isValid =
+          nextX >= 0 &&
+          nextX < rowN &&
+          nextY >= 0 &&
+          nextY < colN &&
+          !visited[nextX][nextY] &&
+          (grid[nextX][nextY] === "." || grid[nextX][nextY] === "D");
+
+        if (isValid) {
+          queue.push([nextX, nextY, time + 1, "고슴도치"]);
+          visited[nextX][nextY] = true;
         }
       }
     }
