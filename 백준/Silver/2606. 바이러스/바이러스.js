cdd1f44f7b7d8087d7f7add45f7df19b2db14ad2
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-09-02 20:50:00 +0900

    [Silver III] Title: 바이러스, Time: 92 ms, Memory: 9452 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.js
+++ 백준/Silver/2606. 바이러스/바이러스.js
@@ -3,17 +3,21 @@ const input = require("fs")
   .trim()
   .split("\n");
 
+// 첫번째줄에 컴퓨터수 두번째줄에 간선수
+// 그다움줄부터 컴퓨터 쌍이 주어진다
+// 1번 감염시켯을때 감염된 컴퓨터의 수를 구해라
+// dfs로 visitid 마킹하면된다
 const solution = (input) => {
   const computerN = Number(input[0]);
   const lineN = Number(input[1]);
-  const grid = Array(computerN + 1)
+  const graph = Array(computerN + 1)
     .fill(0)
     .map((v) => []);
 
   for (let i = 2; i < 2 + lineN; i++) {
     const [n1, n2] = input[i].split(" ").map((v) => Number(v));
-    grid[n1].push(n2);
-    grid[n2].push(n1);
+    graph[n1].push(n2);
+    graph[n2].push(n1);
   }
 
   const visited = Array(computerN + 1).fill(false);
@@ -21,10 +25,9 @@ const solution = (input) => {
   const dfs = (curNode) => {
     visited[curNode] = true;
 
-    for (const nextNode of grid[curNode]) {
-      if (!visited[nextNode]) {
-        dfs(nextNode);
-      }
+    for (const nextNode of graph[curNode]) {
+      if (visited[nextNode]) continue;
+      dfs(nextNode);
     }
   };
 
