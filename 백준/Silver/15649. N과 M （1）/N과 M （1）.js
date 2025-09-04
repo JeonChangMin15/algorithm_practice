375ab2482411ed00d23676ab0911c94553f379b3
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-09-04 20:32:22 +0900

    [Silver III] Title: N과 M (1), Time: 1624 ms, Memory: 13792 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.js
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -1,26 +1,27 @@
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
-  .split("\n")
-  .map((line) => line.replace(/\r/, ""));
+  .split("\n");
 
+// 백트레킹을 한다 arr, start로 돌리면서 length에 도달하면 출력
 const solution = (input) => {
   const [n, sizeN] = input[0].split(" ").map((v) => Number(v));
 
-  const dfs = (arr) => {
+  const backTrack = (arr) => {
     if (arr.length === sizeN) {
       console.log(arr.join(" "));
+      return;
     }
 
     for (let i = 1; i <= n; i++) {
       if (arr.includes(i)) continue;
       arr.push(i);
-      dfs(arr);
+      backTrack(arr, i + 1);
       arr.pop();
     }
   };
 
-  dfs([]);
+  backTrack([]);
 };
 
 solution(input);
