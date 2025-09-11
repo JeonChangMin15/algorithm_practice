67f6929fc2dbbe061e61a7f385129d8b4f23cb17
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-09-11 13:35:11 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 560 ms, Memory: 100220 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.js
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.js
@@ -1,47 +1,43 @@
-// const input = require("fs")
-//   .readFileSync("example.txt", "utf8")
-//   .trim()
-//   .split("\n");
-
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
-  .split("\n");
+  .split("\n")
+  .map((line) => line.replace(/\r/, ""));
 
-// grid를 만들고 bfs로 [curNode, parent]로 넣어서 돌리면된다
-// visited로 방문한곳 마킹하면된다
-// 첫째줄에 노드 수 두번째줄부터 정점이 주어진다
-// 루트노드느 1로 고정된다
-// 각 노드들의 부모를 출력하면된다
+// 트리의 루트를 1이라고 정한다
+// 각 노드의 부모를 구한다
+// 첫번째줄에 노드의 갯수가 주어진다. 두번째줄부터 두 정점이 주어진다
+// 2번노드부터 각 노드의 부모 노드번호를 출력한다
+// dfs로 visited로 마킹하면서 1번부터 돌린다
+// dfs(prev, cur)로 하면서 parent[cur] = prev로 하면된다
 const solution = (input) => {
   const n = Number(input[0]);
-  const grid = Array(n + 1)
+  const graph = Array(n + 1)
     .fill(0)
     .map((v) => []);
 
   for (let i = 1; i < n; i++) {
     const [n1, n2] = input[i].split(" ").map((v) => Number(v));
-    grid[n1].push(n2);
-    grid[n2].push(n1);
+    graph[n1].push(n2);
+    graph[n2].push(n1);
   }
 
-  const visited = Array(n + 1).fill(false);
-  const queue = [1];
-  const answer = Array(n + 1).fill(0);
+  const vistied = Array(n + 1).fill(false);
+  const parentArr = Array(n + 1).fill(0);
 
-  while (queue.length) {
-    const curNode = queue.shift();
+  const dfs = (prev, cur) => {
+    vistied[cur] = true;
+    parentArr[cur] = prev;
 
-    for (const nextNode of grid[curNode]) {
-      if (!visited[nextNode]) {
-        visited[nextNode] = true;
-        answer[nextNode] = curNode;
-        queue.push(nextNode);
-      }
+    for (const nextNode of graph[cur]) {
+      if (vistied[nextNode]) continue;
+      dfs(cur, nextNode);
     }
-  }
+  };
 
-  console.log(answer.slice(2).join("\n"));
+  dfs(0, 1);
+
+  console.log(parentArr.slice(2).join("\n"));
 };
 
 solution(input);
