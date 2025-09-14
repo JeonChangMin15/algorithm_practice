9676ca4b6d447e18b798a71df3eb8cfe9bb7e2f0
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-09-15 08:43:25 +0900

    [Silver III] Title: N과 M (3), Time: 732 ms, Memory: 152072 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.js
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.js
@@ -1,30 +1,37 @@
+// const input = require("fs")
+//   .readFileSync("example.txt", "utf8")
+//   .trim()
+//   .split("\n")
+//   .map((line) => line.replace(/\r/, ""));
+
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
+// 첫번째줄에 n,m이 주어지고 1부터 n까지 m개 골라서
+// 중복순열을 전부 출력한다
 const solution = (input) => {
-  const [n, maxLen] = input[0].split(" ").map((v) => Number(v));
+  const [n, lengthN] = input[0].split(" ").map((v) => Number(v));
+  const anwser = [];
 
-  const answer = [];
-
-  const dfs = (arr) => {
-    if (arr.length === maxLen) {
-      answer.push(arr.join(" "));
+  const backTrack = (arr) => {
+    if (arr.length === lengthN) {
+      anwser.push(arr.join(" "));
       return;
     }
 
     for (let i = 1; i <= n; i++) {
       arr.push(i);
-      dfs(arr);
+      backTrack(arr);
       arr.pop();
     }
   };
 
-  dfs([]);
+  backTrack([]);
 
-  console.log(answer.join("\n"));
+  console.log(anwser.join("\n"));
 };
 
 solution(input);
