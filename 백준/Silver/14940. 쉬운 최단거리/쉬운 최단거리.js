fb4211001b3281988604fdf1b70fa150b6b0df7b
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-09-23 16:04:41 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 328 ms, Memory: 31852 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.js
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.js
@@ -1,12 +1,16 @@
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
-  .split("\n");
+  .split("\n")
+  .map((line) => line.replace(/\r/, ""));
 
-// 첫번째줄에 rowN, colN이 주어진다
-// 0은 갈 수없고 1은 갈 수있고 2는 목표 지지점
-// 각 지점에서 목표지점까지 거리를 출력 원래 갈 수 있는데 못가면 -1 출력
-// bfs로 큐에 넣고 상하좌우로 그리드가 1이고 방문안한 지점이면 간다
+// 첫번째줄에 rown, colN 주어진다
+// 두번째줄에 그리드가 주어지고 2는 목표지점, 1은 갈 수 있는 땅 0은 갈 수 없다
+// 각 지점에서 목표지점까지의 거리를 출력. 원래 갈 수 없는 땅은 0, 갈 수 있는데
+// 못간곳은 -1을 출력한다
+// 먼저 그리드를 만들고 0은 지점을 큐에다가 넣는다
+// 그리고나서 거리 좌표를 비교하면서 지금 dist보다 +1 보다 작으면 간다
+// 다 돌고나서 마지막으로 0인데 못간곳은 -1로 마킹을 해준다
 const solution = (input) => {
   const [rowN, colN] = input[0].split(" ").map((v) => Number(v));
   const grid = [];
@@ -16,19 +20,15 @@ const solution = (input) => {
   }
 
   const queue = [];
-  const visited = Array(rowN)
-    .fill(0)
-    .map((v) => Array(colN).fill(false));
-
   const dist = Array(rowN)
     .fill(0)
-    .map((v) => Array(colN).fill(0));
+    .map((v) => Array(colN).fill(Infinity));
 
   for (let i = 0; i < rowN; i++) {
     for (let j = 0; j < colN; j++) {
       if (grid[i][j] === 2) {
         queue.push([i, j, 0]);
-        visited[i][j] = true;
+        dist[i][j] = 0;
       }
     }
   }
@@ -51,26 +51,30 @@ const solution = (input) => {
         nextX < rowN &&
         nextY >= 0 &&
         nextY < colN &&
-        !visited[nextX][nextY] &&
+        d + 1 < dist[nextX][nextY] &&
         grid[nextX][nextY] === 1;
 
       if (isValid) {
         queue.push([nextX, nextY, d + 1]);
         dist[nextX][nextY] = d + 1;
-        visited[nextX][nextY] = true;
       }
     }
   }
 
   for (let i = 0; i < rowN; i++) {
     for (let j = 0; j < colN; j++) {
-      if (grid[i][j] === 1 && dist[i][j] === 0) {
+      if (grid[i][j] === 0) {
+        dist[i][j] = 0;
+      }
+      if (grid[i][j] === 1 && dist[i][j] === Infinity) {
         dist[i][j] = -1;
       }
     }
   }
-  const answer = dist.map((v) => v.join(" ")).join("\n");
-  console.log(answer);
+
+  for (let i = 0; i < rowN; i++) {
+    console.log(dist[i].join(" "));
+  }
 };
 
 solution(input);
