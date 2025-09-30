84a9bd19a02c0220fc439a564ddbfe99b14ade31
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-09-30 09:58:48 +0900

    [Silver I] Title: 단지번호붙이기, Time: 92 ms, Memory: 9404 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
@@ -4,6 +4,10 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
+// 첫번째줄 N 그다음줄부터 그리드가 주어진다
+// 지역의 갯수와 지역의 크기를 오름차순으로 한줄씩 출력한다
+// dfs로 1인곳은 전부 확인해서 상하좌우로 체킹하면된다
+// 방문한곳은 0으로 만들면 된다
 const solution = (input) => {
   const n = Number(input[0]);
   const grid = [];
@@ -14,7 +18,6 @@ const solution = (input) => {
 
   const dfs = (x, y) => {
     if (x < 0 || x >= n || y < 0 || y >= n || grid[x][y] === 0) return 0;
-
     grid[x][y] = 0;
 
     const up = dfs(x - 1, y);
@@ -26,12 +29,11 @@ const solution = (input) => {
   };
 
   const answer = [];
-
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
-      if (grid[i][j] === 1) {
-        answer.push(dfs(i, j));
-      }
+      if (grid[i][j] === 0) continue;
+      const area = dfs(i, j);
+      answer.push(area);
     }
   }
 
