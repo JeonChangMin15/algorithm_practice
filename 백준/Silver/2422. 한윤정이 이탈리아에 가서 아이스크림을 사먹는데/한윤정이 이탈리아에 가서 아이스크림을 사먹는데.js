4bc35f41c567f6ba97c30a95b51f3b21a3a05e81
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-10-17 09:23:35 +0900

    [Silver IV] Title: 한윤정이 이탈리아에 가서 아이스크림을 사먹는데, Time: 2656 ms, Memory: 167156 KB -BaekjoonHub

--- 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.js
+++ 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.js
@@ -1,39 +1,53 @@
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
-  .split("\n");
+  .split("\n")
+  .map((line) => line.replace(/\r/, ""));
 
+// 아이스크림을 3가지를 선택해서 먹으려고 한다
+// 같이 먹으면 안되는 조합을 피해서 먹는다
+// 가능한 조합을 출력한다
+// 삼중 for문으로 체크한다
+// 해당 배열에 모든 피하는 조합을 체크한 후 패스하면 +1
+// 전체 조합에서 안되는 조합의 갯수를 빼는 방식으로 구해야된다
 
-// 첫번째줄에는 아이스크림 종류수, 조합개수
-// 아이스크림을 3가지를 고르는데 조합을 피해서 고를 수 있는 경우의 수를 출력한다
-// 1부터 N까지 3가지 고를 수 있는 조합을 만들고
-// 해당 조합이 맛없는 조합이랑 되는지 for문에서 확인한다
 const solution = (input) => {
-  const [n, m] = input[0].split(" ").map((v) => Number(v));
-  const notComb = Array(n + 1)
-    .fill(0)
-    .map(() => Array(n + 1).fill(false));
+  const [n, checkN] = input[0].split(" ").map((v) => Number(v));
+  const checkComb = [];
 
-  for (let i = 1; i < input.length; i++) {
-    const [n1, n2] = input[i].split(" ").map((v) => Number(v));
-
-    notComb[n1][n2] = true;
-    notComb[n2][n1] = true;
+  for (let i = 1; i < checkN + 1; i++) {
+    const arr = input[i].split(" ").map((v) => Number(v));
+    checkComb.push(arr);
   }
 
-  let cnt = 0;
+  let total = 0;
+  const notCombSet = new Set();
 
-  for (let i = 1; i <= n - 2; i++) {
-    for (let j = i + 1; j <= n - 1; j++) {
-      if (notComb[i][j]) continue;
-      for (let k = j + 1; k <= n; k++) {
-        if (notComb[i][k] || notComb[j][k]) continue;
-        cnt += 1;
+  const backTracking = (arr, start) => {
+    if (arr.length === 3) {
+      total += 1;
+      return;
+    }
+
+    for (let i = start; i <= n; i++) {
+      arr.push(i);
+      backTracking(arr, i + 1);
+      arr.pop();
+    }
+  };
+
+  backTracking([], 1);
+
+  for (const [n1, n2] of checkComb) {
+    for (let i = 1; i <= n; i++) {
+      if (i !== n1 && i !== n2) {
+        const comb = [n1, n2, i].sort((a, b) => a - b).join(" ");
+        notCombSet.add(comb);
       }
     }
   }
 
-  console.log(cnt);
+  console.log(total - notCombSet.size);
 };
 
 solution(input);
