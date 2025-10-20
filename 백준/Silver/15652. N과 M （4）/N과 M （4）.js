41b1e58e839a694e8602fb8a3e14ee5a726d09ad
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-10-20 20:01:31 +0900

    [Silver III] Title: N과 M (4), Time: 152 ms, Memory: 11372 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.js
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.js
@@ -1,32 +1,31 @@
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
-  .split("\n")
-  .map((line) => line.replace(/\r/, ""));
+  .split("\n");
 
-// 첫번째줄에 N과 수열 길이가 주어진다
-// 1부터 시작해서 여러번 골라도 되는 오름차순 수열을 하나씩 출력해라
-// for문을 돌리면서 가장 맨뒤에 있는 숫자보다 크거나 같으면 넣으면된다
+// 무조건 앞에 숫자보다 같거나 커야한다
+// 첫번째줄에 n, combN이 주어진다
+// 백트래킹으로 조합을 만든다
+// 인자에 arr, prev 값을 넣으면 되나
 const solution = (input) => {
   const [n, combN] = input[0].split(" ").map((v) => Number(v));
   const answer = [];
 
-  const backTracking = (arr, lastNum) => {
+  const backTracking = (arr, prev) => {
     if (arr.length === combN) {
       answer.push(arr.join(" "));
       return;
     }
 
     for (let i = 1; i <= n; i++) {
-      if (i >= lastNum) {
-        arr.push(i);
-        backTracking(arr, i);
-        arr.pop();
-      }
+      if (i < prev) continue;
+      arr.push(i);
+      backTracking(arr, i);
+      arr.pop();
     }
   };
 
-  backTracking([], 0);
+  backTracking([], -1);
 
   console.log(answer.join("\n"));
 };
