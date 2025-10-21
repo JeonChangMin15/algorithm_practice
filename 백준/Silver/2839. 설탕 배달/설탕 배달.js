b984ef1ccef2c23a79b2401a4af54a77bd297430
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-10-21 13:12:29 +0900

    [Silver IV] Title: 설탕 배달, Time: 100 ms, Memory: 9568 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.js
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.js
@@ -4,17 +4,22 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
+// 3키로, 5키로 봉지가 있다
+// 더 적은 개수의 봉지를 배달
+// dp[n] = Math.min(dp[n-3], dp[n-5]) +1
+// 이걸로 풀이하면된다
+// dp[3] = 1, dp[5] = 1
 const solution = (input) => {
-  const weight = Number(input[0]);
-  const dp = Array(weight + 1).fill(Infinity);
+  const n = Number(input[0]);
+  const dp = Array(n + 1).fill(Infinity);
   dp[3] = 1;
   dp[5] = 1;
 
-  for (let i = 6; i <= weight; i++) {
-    dp[i] = Math.min(dp[i - 3] + 1, dp[i - 5] + 1);
+  for (let i = 6; i <= n; i++) {
+    dp[i] = Math.min(dp[i - 3], dp[i - 5]) + 1;
   }
 
-  console.log(dp[weight] !== Infinity ? dp[weight] : -1);
+  console.log(dp[n] !== Infinity ? dp[n] : -1);
 };
 
 solution(input);
