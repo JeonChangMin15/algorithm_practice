bb82912e62c39d3a95134ab7cecad656e1ef3839
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-10-23 12:58:57 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1564 ms, Memory: 170316 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,29 +1,30 @@
 from collections import deque
 
-cityN, lineN, targetDist, startCity = list(map(int,input().split()))
-graph = [[] for _ in range(cityN + 1)]
-
-for i in range(lineN):
-  n1, n2 = map(int,input().split())
-  graph[n1].append(n2)
-
+cityN, lineN, targetLength, startCity = list(map(int,input().split()))
 dist = [float('inf')]*(cityN+1)
 dist[startCity] = 0
+
+graph = [[] for _ in range(cityN+1)]
+
+for _ in range(lineN):
+  n1, n2 = list(map(int, input().split()))
+  graph[n1].append(n2)
+
 queue = deque()
 queue.append([startCity, 0])
 
 while len(queue):
-  curCity, curDist = queue.popleft()
+  curCity, d = queue.popleft()
 
   for nextCity in graph[curCity]:
-    if curDist + 1 < dist[nextCity]:
-      dist[nextCity] = curDist + 1
-      queue.append([nextCity, curDist + 1])
+    if d+1 < dist[nextCity]:
+      queue.append([nextCity, d+1])
+      dist[nextCity] = d + 1
 
 answer = []
 
 for i in range(1, cityN+1):
-  if dist[i] == targetDist:
-    answer.append(i)
+  if dist[i] == targetLength:
+    answer.append(str(i))
 
-print("\n".join(list(map(str, answer))) if len(answer) else -1)
+print("\n".join(answer) if len(answer) > 0 else -1)
