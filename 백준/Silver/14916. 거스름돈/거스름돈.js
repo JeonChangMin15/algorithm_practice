2f2244adc6269d68defac65399bcc5c2d7dbdbd6
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-10-31 09:33:13 +0900

    [Silver V] Title: 거스름돈, Time: 92 ms, Memory: 9600 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.js
+++ 백준/Silver/14916. 거스름돈/거스름돈.js
@@ -4,21 +4,21 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// 2원, 5원으로 거스름돈을 준다. 동전의 개수는 무한대고 동전의 갯수가 최소가 되도록 거슬러 준다
-// 첫번째줄에 거스름돈 N이 주어지고 만약 안되면 -1을 출력한다
-// 먼저 5원짜리를 최대 갯수를 구하고 해당 액수를 제외한 금액에서 2원으로 가능하면 된다
+// 2원 5원으로 거스름돈을 준다
+// 동전의 개수가 최소가 되도록 거슬러 주어야한다
+// 동전의 최소 개수를 출력하고 만약 거슬러 줄 수 없으면 -1을 출력한다
+// 먼저 해당 금액을 5로 나눈 몫부터 0까지 차례대로 경우의 수를 계산을 한다
+// 그래서 2로 나눈 나머지가 0이되면 break하고 총 개수를 갱신해준다
 const solution = (input) => {
-  const money = Number(input[0]);
+  const totalMoney = Number(input[0]);
+  const fiveWonCoinMax = Math.floor(totalMoney / 5);
   let answer = -1;
-  let fiveWonCoin = Math.floor(money / 5);
+  for (let i = fiveWonCoinMax; i >= 0; i--) {
+    const resMoney = totalMoney - 5 * i;
 
-  while (fiveWonCoin >= 0) {
-    const res = money - fiveWonCoin * 5;
-    if (res % 2 === 0) {
-      answer = fiveWonCoin + res / 2;
+    if (resMoney % 2 === 0) {
+      answer = i + resMoney / 2;
       break;
-    } else {
-      fiveWonCoin -= 1;
     }
   }
 
