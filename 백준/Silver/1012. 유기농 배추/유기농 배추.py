1b2a0e2e28801f3f6e23394ba93f76d3fa1e569d
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-11-03 09:38:55 +0900

    [Silver II] Title: 유기농 배추, Time: 132 ms, Memory: 111836 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,28 +1,34 @@
 testN = int(input())
-dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
-for i in range(testN):
-  rowN, colN, coorN = list(map(int, input().split()))
+dirs =[[-1, 0], [1, 0], [0,- 1], [0, 1]]
+
+def dfs(x, y, grid, rowN, colN):
+  if x < 0 or x >= rowN or y < 0 or y >= colN or grid[x][y] == 0:
+    return
+  
+  grid[x][y] = 0
+
+  for dx, dy in dirs:
+    dfs(x + dx, y+dy, grid, rowN, colN)
+
+def findArea():
+  colN, rowN, areaN = list(map(int, input().split()))
   grid = [[0]*colN for _ in range(rowN)]
-
-  for _ in range(coorN):
-    x, y = list(map(int, input().split()))
-    grid[x][y] = 1
-
   answer = 0
 
-  def dfs(x, y):
-    if x < 0 or x >= rowN or y < 0 or y >= colN or grid[x][y] == 0:
-      return
-    grid[x][y] = 0
-    
-    for dx, dy in dirs:
-      dfs(x+dx, y + dy)
+  for _ in range(areaN):
+    y, x = list(map(int, input().split()))
+    grid[x][y] = 1
 
-  for x in range(rowN):
-    for y in range(colN):
-      if grid[x][y] == 1:
-        dfs(x, y)
+  for i in range(rowN):
+    for j in range(colN):
+      if grid[i][j] == 1:
+        dfs(i, j, grid, rowN, colN)
         answer += 1
 
   print(answer)
+
+
+
+for _ in range(testN):
+  findArea()
