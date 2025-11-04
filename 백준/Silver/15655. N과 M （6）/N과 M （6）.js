773171511b8c09504b062879d3809682fa28ed1e
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-11-04 09:08:48 +0900

    [Silver III] Title: N과 M (6), Time: 92 ms, Memory: 9600 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.js
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.js
@@ -4,31 +4,32 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
+// 백트래킹으로 순열을 만드느데 중복없이 만들어야하낟
+// 인자 arr, prev로 이전값보다 큰 값만 넣어서
+// 첫번째줄에 숫자의 개수와 조합 길이가 주어진다
+// 숫자를 오름차순 정렬을 한 뒤에 백트래킹을 실행
 const solution = (input) => {
-  const [n, combN] = input[0].split(" ").map((v) => Number(v));
-  const nums = input[1]
-    .split(" ")
-    .map((v) => Number(v))
-    .sort((a, b) => a - b);
+  const [n, comLen] = input[0].split(" ").map((v) => Number(v));
+  const nums = input[1].split(" ").map((v) => Number(v));
+  nums.sort((a, b) => a - b);
 
   const answer = [];
 
-  const backTrack = (arr, prev) => {
-    if (arr.length === combN) {
+  const backTracking = (arr, start) => {
+    if (arr.length === comLen) {
       answer.push(arr.join(" "));
       return;
     }
 
-    for (let i = 0; i < n; i++) {
-      if (!arr.includes(nums[i]) && nums[i] > prev) {
-        arr.push(nums[i]);
-        backTrack(arr, nums[i]);
-        arr.pop();
-      }
+    for (let i = start; i < n; i++) {
+      arr.push(nums[i]);
+      backTracking(arr, i + 1);
+      arr.pop();
     }
   };
 
-  backTrack([], 0);
+  backTracking([], 0);
+
   console.log(answer.join("\n"));
 };
 
