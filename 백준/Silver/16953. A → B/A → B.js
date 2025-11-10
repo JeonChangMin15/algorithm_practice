24a5a3475907386b12cc154e7634f1679f47b4c8
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-11-10 19:59:09 +0900

    [Silver II] Title: A → B, Time: 5032 ms, Memory: 31700 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.js
+++ 백준/Silver/16953. A → B/A → B.js
@@ -3,30 +3,29 @@ const input = require("fs")
   .trim()
   .split("\n");
 
+// 현재값에서 2를 곱하거나 10을 곱한 후 +1을 할 수 있다
+// A -> B로 바꾸는데 필요한 연산의 최소값
+// 만들 수 없으면 -1을 출력
+// 현재값이 타겟보다 크면 continue
+// 큐에 [value, cnt]
 const solution = (input) => {
-  const [start, target] = input[0].split(" ").map((v) => Number(v));
-  const nums = new Set();
+  const [start, end] = input[0].split(" ").map((v) => Number(v));
   const queue = [[start, 1]];
-
   let answer = -1;
-  const limit = 10 ** 9;
+
   while (queue.length) {
     const [val, cnt] = queue.shift();
+    if (val > end) {
+      continue;
+    }
 
-    if (val === target) {
+    if (val === end) {
       answer = cnt;
       break;
     }
 
-    if (val * 2 <= limit && !nums.has(val * 2)) {
-      queue.push([val * 2, cnt + 1]);
-      nums.add(val * 2);
-    }
-
-    if (val * 10 + 1 <= limit && !nums.has(val * 10 + 1)) {
-      queue.push([val * 10 + 1, cnt + 1]);
-      nums.add(val * 10 + 1);
-    }
+    queue.push([val * 2, cnt + 1]);
+    queue.push([val * 10 + 1, cnt + 1]);
   }
 
   console.log(answer);
