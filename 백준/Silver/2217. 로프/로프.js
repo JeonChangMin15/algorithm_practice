a78efcb182dd218156380ed40c5697713bbd590f
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-11-13 13:19:06 +0900

    [Silver IV] Title: 로프, Time: 236 ms, Memory: 26788 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.js
+++ 백준/Silver/2217. 로프/로프.js
@@ -4,24 +4,26 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// k개의 로프를 사용하여 중량이 w인 물체를 올리면 고르게 w/k만큼 중량이 걸린다
-// 각 로프의 정보가 주어질때 들어올릴 수 있는 최대 중량을 구해내라
-// 모든 로프를 사용할 필요는 없고 임의로 몇개의 로프를 골라서 사용해도 된다
-// 첫번째줄에 N이 주어지고 N개의 줄에는 각 로프가 버틸 수 있는 최대 중량이 주어진다
-// 로프는 최대 100000
-// 결국 무게를 오름차순으로 한 후에 해당 무게*(가방갯수 - 인덱스)
+// 첫번째줄에 로프의 개수, 그다음줄부터 로프가 견딜 수 있는 무게
+// 로프를 활용해서 최대로 견딜 수 있는 무게를 구해라
+// 모든 로프를 사용할 필요는 없고 필요한 로프만 사용하면된다
+// 로프를 오름차순으로 정렬한후 본인무게 *(총갯수-i)를 해서 최대값 비교
 const solution = (input) => {
-  const n = Number(input[0]);
-  const arr = [];
-  for (let i = 1; i <= n; i++) {
-    arr.push(Number(input[i]));
+  const totalN = Number(input[0]);
+  const lopeWeight = [];
+
+  for (let i = 1; i <= totalN; i++) {
+    lopeWeight.push(Number(input[i]));
   }
-  arr.sort((a, b) => a - b);
+  lopeWeight.sort((a, b) => a - b);
 
   let answer = 0;
-  for (let i = 0; i < n; i++) {
-    answer = Math.max(answer, arr[i] * (n - i));
+
+  for (let i = 0; i < totalN; i++) {
+    const w = lopeWeight[i];
+    answer = Math.max(answer, w * (totalN - i));
   }
+
   console.log(answer);
 };
 
