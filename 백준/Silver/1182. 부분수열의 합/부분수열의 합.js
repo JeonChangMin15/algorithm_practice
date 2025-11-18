d1f93b794abdd6985e92bef22da641f56ea7ea63
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-11-18 19:22:20 +0900

    [Silver II] Title: 부분수열의 합, Time: 388 ms, Memory: 12816 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.js
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.js
@@ -1,39 +1,32 @@
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
-  .split("\n")
-  .map((line) => line.replace(/\r/, ""));
+  .split("\n");
 
-// 첫번째 줄에 숫자의 수와 목표값이 주어진다
-// 두번째줄에 숫자들이 주어진다
-// 부분수열의 원소값을 다 더한게 목표값이 되는 경우의 수를 구해라
-// 백트레킹으로 인덱스 값들을 중복안되게 넣어서 모든 경우의 수를 체크하면된다
+// 첫번째줄에 정수의 개수와 target이 주어진다
+// 다 더한 값이 target되는 경우의 수를 구해라
+// index를 기준으로 순열을 만들어서 length > 0 보다 크면 무조건 합산해서 검사한다
 const solution = (input) => {
-  const [n, sumVal] = input[0].split(" ").map((v) => Number(v));
+  const [n, target] = input[0].split(" ").map((v) => Number(v));
   const nums = input[1].split(" ").map((v) => Number(v));
   let answer = 0;
 
-  const backTrack = (arr, lastIndex) => {
-    if (lastIndex >= 0) {
-      const total = arr
-        .map((v) => nums[v])
-        .reduce((prev, cur) => prev + cur, 0);
-
-      if (total === sumVal) {
-        answer += 1;
-      }
+  const backTrack = (arr, start) => {
+    if (arr.length) {
+      const vals = arr.map((v) => nums[v]);
+      const total = vals.reduce((prev, cur) => prev + cur, 0);
+      if (total === target) answer += 1;
     }
 
-    for (let i = 0; i < n; i++) {
-      if (i > lastIndex) {
-        arr.push(i);
-        backTrack(arr, i);
-        arr.pop();
-      }
+    for (let i = start; i < n; i++) {
+      arr.push(i);
+      backTrack(arr, i + 1);
+      arr.pop();
     }
   };
 
-  backTrack([], -1);
+  backTrack([], 0);
+
   console.log(answer);
 };
 
