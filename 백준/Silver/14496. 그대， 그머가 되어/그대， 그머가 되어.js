63ad5701925ae970e196c2cd6df60073edc9969c
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-11-25 13:18:07 +0900

    [Silver II] Title: 그대, 그머가 되어, Time: 172 ms, Memory: 14540 KB -BaekjoonHub

--- 백준/Silver/14496. 그대， 그머가 되어/그대， 그머가 되어.js
+++ 백준/Silver/14496. 그대， 그머가 되어/그대， 그머가 되어.js
@@ -4,18 +4,16 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// 첫째줄에 시작점 끝점이 주어지고
-// 두번째줄에는 N(문자의 수), 노드쌍의 수가 주어진다
-// 이후 노드 쌍수만큼 주어진다. 최소 치환 횟수를 출력한다.
-// 불가능하면 -1을 출력
+// 첫째줄에 start, end 가주어진다
+// 두번째줄에 nodeN, lineN이 주어진다
+// 최소한의 횟수와 안되면 -1을 출력한ㄷ
+// graph로 저장을 하고 bfs로 탐색을하면된다
 const solution = (input) => {
-  const [startNode, endNode] = input[0].split(" ").map((v) => Number(v));
+  const [start, end] = input[0].split(" ").map((v) => Number(v));
   const [nodeN, lineN] = input[1].split(" ").map((v) => Number(v));
-  const graph = {};
-
-  for (let i = 1; i <= nodeN; i++) {
-    graph[i] = [];
-  }
+  const graph = Array(nodeN + 1)
+    .fill(0)
+    .map((v) => []);
 
   for (let i = 2; i < 2 + lineN; i++) {
     const [n1, n2] = input[i].split(" ").map((v) => Number(v));
@@ -23,15 +21,15 @@ const solution = (input) => {
     graph[n2].push(n1);
   }
 
-  const queue = [[startNode, 0]];
   const visited = Array(nodeN + 1).fill(false);
-  visited[startNode] = true;
+  visited[start] = true;
 
+  const queue = [[start, 0]];
   let answer = -1;
 
   while (queue.length) {
     const [curNode, cnt] = queue.shift();
-    if (curNode === endNode) {
+    if (curNode === end) {
       answer = cnt;
       break;
     }
