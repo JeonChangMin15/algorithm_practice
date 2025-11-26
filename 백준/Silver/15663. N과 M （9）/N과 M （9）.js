8da49ec6ffcb7d923cb343522425037da10a04f3
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-11-26 09:00:39 +0900

    [Silver II] Title: N과 M (9), Time: 2596 ms, Memory: 16148 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.js
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.js
@@ -4,22 +4,21 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
+// 오름차순으로 정렬한 후 인덱스로 중복없이 넣는다
+// 그 다음에 해당 조합이 있는지 확인한 후 추가하면 된다
 const solution = (input) => {
   const [n, combN] = input[0].split(" ").map((v) => Number(v));
-  const nums = input[1]
-    .split(" ")
-    .map((v) => Number(v))
-    .sort((a, b) => a - b);
+  const nums = input[1].split(" ").map((v) => Number(v));
+  nums.sort((a, b) => a - b);
 
   const answer = [];
-  const set = new Set();
 
   const backTrack = (arr) => {
     if (arr.length === combN) {
-      const str = arr.map((v) => nums[v]).join(" ");
-      if (!set.has(str)) {
-        answer.push(str);
-        set.add(str);
+      const vals = arr.map((v) => nums[v]);
+      const combStr = vals.join(" ");
+      if (!answer.includes(combStr)) {
+        answer.push(combStr);
       }
       return;
     }
@@ -27,7 +26,7 @@ const solution = (input) => {
     for (let i = 0; i < n; i++) {
       if (arr.includes(i)) continue;
       arr.push(i);
-      backTrack(arr, i);
+      backTrack(arr);
       arr.pop();
     }
   };
