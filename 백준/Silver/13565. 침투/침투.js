d915d2933c6c370402e9768e974cd9d5144382c7
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-11-27 16:28:23 +0900

    [Silver II] Title: 침투, Time: 316 ms, Memory: 80332 KB -BaekjoonHub

--- 백준/Silver/13565. 침투/침투.js
+++ 백준/Silver/13565. 침투/침투.js
@@ -4,10 +4,10 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// 첫번째줄에 rowN, colN 주어지고 그 다음줄부터 간격없는 그리드가 주어진다
-// 0이면 지나가고 1은 못지나간다
-// for문으로 [0][colN]을 탐색하면서 visit를 찍으면서 만약 지점의 x가 rowN-1 이면 YES 바꾼다
-
+// 첫번쩨줄에 rowN, colN이 주어진다
+// 두번째줄에 그리드가 주어지고 0이 지나가고, 1은 안된다
+// 0,0, -> 0,colN-1까지 0인곳이면 dfs를 시작한다
+// 상하좌우로 탐색하면 x가 rowN-1이면 'YES'로 바꾸면 된다
 const solution = (input) => {
   const [rowN, colN] = input[0].split(" ").map((v) => Number(v));
   const grid = [];
@@ -15,22 +15,22 @@ const solution = (input) => {
   for (let i = 1; i <= rowN; i++) {
     grid.push(input[i].split("").map((v) => Number(v)));
   }
-  let answer = "NO";
+
   const dirs = [
     [-1, 0],
     [1, 0],
     [0, -1],
     [0, 1],
   ];
-
+  let answer = "NO";
   const dfs = (x, y) => {
     if (x < 0 || x >= rowN || y < 0 || y >= colN || grid[x][y] === 1) return;
+    grid[x][y] = 1;
 
     if (x === rowN - 1) {
       answer = "YES";
       return;
     }
-    grid[x][y] = 1;
 
     for (const [dx, dy] of dirs) {
       dfs(x + dx, y + dy);
