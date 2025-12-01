5b7b2bf032a675c9e1a7569f1416e0bc2730fef8
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-12-01 12:46:59 +0900

    [Silver II] Title: N과 M (11), Time: 700 ms, Memory: 93472 KB -BaekjoonHub

--- 백준/Silver/15665. N과 M （11）/N과 M （11）.js
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.js
@@ -4,25 +4,19 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// 첫번째줄에 숫자갯수 조합수가 주어진다
-// 두번째줄에 숫자들이 주어진다
-// 하나의 숫자를 여러번 선택이 가능하고 중복되는 수열은 안된다
-// 그렇다면 먼저 숫자들을 오름차순으로 정렬한 후 백트레킹으로 조합하는데
-// 대신 조합을 set에 넣고 중복을 확인해야된다
+// 백트레킹으로 숫자를 탐색을 한다
+// 배열에다가 넣고 해당 숫자 조합이 없으면 추가
+// 숫자는 오름차순으로 먼저 정렬을 해준다
 const solution = (input) => {
   const [n, combN] = input[0].split(" ").map((v) => Number(v));
-  const nums = input[1]
-    .split(" ")
-    .map((v) => Number(v))
-    .sort((a, b) => a - b);
-
-  const set = new Set();
+  const nums = input[1].split(" ").map((v) => Number(v));
+  nums.sort((a, b) => a - b);
   const answer = [];
+  const set = new Set();
 
-  const backTrack = (arr) => {
+  const backTracking = (arr) => {
     if (arr.length === combN) {
       const val = arr.join(" ");
-
       if (!set.has(val)) {
         answer.push(val);
         set.add(val);
@@ -33,12 +27,13 @@ const solution = (input) => {
 
     for (let i = 0; i < n; i++) {
       arr.push(nums[i]);
-      backTrack(arr);
+      backTracking(arr);
       arr.pop();
     }
   };
 
-  backTrack([]);
+  backTracking([]);
+
   console.log(answer.join("\n"));
 };
 
