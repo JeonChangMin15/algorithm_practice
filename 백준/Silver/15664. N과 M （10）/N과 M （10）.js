44b62669db1adc6364d3c1e6a1cc7296c4be68bd
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-12-17 08:21:29 +0900

    [Silver II] Title: N과 M (10), Time: 92 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.js
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.js
@@ -4,39 +4,34 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// 첫번째줄에 숫자의 수와 조합수가 주어진다
-// 두번째줄에 숫자들이 주어진다
-// 오름차순으로 조합을 만들어야하는데 수열에 중복된 인덱스를 넣으면 안되고
-// 그리고 중복된 조합이 있으면 안된다
+
 const solution = (input) => {
   const [n, combN] = input[0].split(" ").map((v) => Number(v));
-  const nums = input[1]
-    .split(" ")
-    .map((v) => Number(v))
-    .sort((a, b) => a - b);
+  const nums = input[1].split(" ").map((v) => Number(v));
+  nums.sort((a, b) => a - b);
 
   const answer = [];
   const set = new Set();
 
-  const dfs = (arr, start) => {
+  const backTracking = (arr, start) => {
     if (arr.length === combN) {
       const val = arr.map((v) => nums[v]).join(" ");
       if (!set.has(val)) {
-        set.add(val);
         answer.push(val);
+        set.add(val);
       }
-
       return;
     }
 
     for (let i = start; i < n; i++) {
+      if (arr.includes(i)) continue;
       arr.push(i);
-      dfs(arr, i + 1);
+      backTracking(arr, i + 1);
       arr.pop();
     }
   };
 
-  dfs([], 0);
+  backTracking([], 0);
 
   console.log(answer.join("\n"));
 };
