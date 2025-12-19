93341c86010aed8edb602972629645e7e43a1068
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-12-19 15:03:52 +0900

    [Silver IV] Title: 2+1 세일, Time: 240 ms, Memory: 26780 KB -BaekjoonHub

--- 백준/Silver/11508. 2＋1 세일/2＋1 세일.js
+++ 백준/Silver/11508. 2＋1 세일/2＋1 세일.js
@@ -4,24 +4,22 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// 3개를 구매하면 가장싼 제품 하나는 공짜이다
-// 최소비용으로 구매해야된다
-// 첫번째줄에 유제품의 수 두번째줄에 유제품 가격이 한줄씩 있다
-// 가격 배열을 내림차순으로 정렬한후 해당 인덱스가 3의배수면 제외하면된다
+// 내림차순으로 정렬한후 해당 인덱스 i%3 === 2인건 스킵하고 더하면된다
 const solution = (input) => {
   const n = Number(input[0]);
-  const price = [];
+  const arr = [];
 
   for (let i = 1; i <= n; i++) {
-    price.push(Number(input[i]));
+    arr.push(Number(input[i]));
   }
-  price.sort((a, b) => b - a);
+
+  arr.sort((a, b) => b - a);
 
   let answer = 0;
 
   for (let i = 0; i < n; i++) {
     if (i % 3 !== 2) {
-      answer += price[i];
+      answer += arr[i];
     }
   }
 
