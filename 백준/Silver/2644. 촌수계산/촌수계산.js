2fde863897c971863f8c17b2e421be4a2bf40ef1
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-12-21 14:57:16 +0900

    [Silver II] Title: 촌수계산, Time: 92 ms, Memory: 9384 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.js
+++ 백준/Silver/2644. 촌수계산/촌수계산.js
@@ -1,47 +1,45 @@
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
-  .split("\n")
-  .map((line) => line.replace(/\r/, ""));
+  .split("\n");
 
-// 첫번째줄에 인원수가 주어진다 1부터 N
-// 두번째줄에는 촌수 계산해야하는 서로다른 두사람 번호
-// 셋째줄에는 부모 자식간의 관계 개수가 주어진다
-// 네번째줄부터는 부모 자식간의 관계를 부모 자식이 주어진다
-// 촌수를 구하고 만약 관계가 없으면 -1을 출력해라
-// 먼저 그래프로 각 관계를 다 넣고 dfs로 하나씩 탐색하면 된다
+// 촌수를 계산해야된다
+// 첫번째줄에 사람의 인원수 두번째줄에 관계 촌수를 구해야할 사람 번호
+// 세번째줄에 관계수 그다음부터 부모-자식 번호가 주어진다
+// bfs로 visited로 마킹하면서 몇번째 촌수인지 하면된다
+// 만약 친척 관계가 아니면 -1 출력
 const solution = (input) => {
   const peopleN = Number(input[0]);
   const [start, end] = input[1].split(" ").map((v) => Number(v));
   const lineN = Number(input[2]);
-  const graph = Array(peopleN + 1)
+  const grid = Array(peopleN + 1)
     .fill(0)
     .map((v) => []);
 
   for (let i = 3; i < 3 + lineN; i++) {
     const [n1, n2] = input[i].split(" ").map((v) => Number(v));
-    graph[n1].push(n2);
-    graph[n2].push(n1);
+    grid[n1].push(n2);
+    grid[n2].push(n1);
   }
 
-  const visited = Array(peopleN + 1).fill(false);
+  const visit = Array(peopleN + 1).fill(false);
+  visit[start] = true;
+  const queue = [[start, 0]];
   let answer = -1;
 
-  const dfs = (node, relation) => {
-    if (visited[node]) return;
-    visited[node] = true;
-
-    if (node === end) {
-      answer = relation;
-      return;
+  while (queue.length) {
+    const [curPerson, dist] = queue.shift();
+    if (end === curPerson) {
+      answer = dist;
+      break;
     }
 
-    for (const nextNode of graph[node]) {
-      dfs(nextNode, relation + 1);
+    for (const nextPerson of grid[curPerson]) {
+      if (visit[nextPerson]) continue;
+      visit[nextPerson] = true;
+      queue.push([nextPerson, dist + 1]);
     }
-  };
-
-  dfs(start, 0);
+  }
 
   console.log(answer);
 };
