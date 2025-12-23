2411e4effb6fef5b2e58dc8b6d6d7027a29393ad
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2025-12-23 13:48:49 +0900

    [Silver II] Title: 점프 점프, Time: 180 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Silver/14248. 점프 점프/점프 점프.js
+++ 백준/Silver/14248. 점프 점프/점프 점프.js
@@ -1,41 +1,34 @@
-// const input = require("fs")
-//   .readFileSync("example.txt", "utf8")
-//   .trim()
-//   .split("\n");
-
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
-  .split("\n");
+  .split("\n")
+  .map((line) => line.replace(/\r/, ""));
 
-// 첫번째줄에 돌다리의 수(1부터 시작)
-// 두번째줄에 돌의 각 위치에서 점프 할 수 있는 거리
-// 다음 줄에는 출발점 s가 주어진다
-// 방문 가능한 돌들의 갯수를 구해라
-// bfs로 visited를 마킹하면서 좌우로 0,n-1 범위 확인하면서 가면된다
-// vistied가 true 갯수 반환
+// 첫번재줄에 돌의 갯수 두번째줄에 돌이 주어진다
+// 해당 위치에 적힌 숫자만큼 왼, 오른쪽을 점프 가능하다
+// 세번재줄에는 출발점이 주어진다
+// bfs로 visited로 마킹하면서 탐색한 후 true인 갯수를 출력
 const solution = (input) => {
-  const n = Number(input[0]);
+  const rockN = Number(input[0]);
   const arr = input[1].split(" ").map((v) => Number(v));
   const start = Number(input[2]) - 1;
 
+  const visited = Array(rockN + 1).fill(false);
+  visited[start] = true;
   const queue = [start];
-  const visited = Array(n).fill(false);
-  visited[queue] = true;
 
   while (queue.length) {
-    const curPos = queue.shift();
-    const nextLeftPos = curPos - arr[curPos];
-    const nextRightPos = curPos + arr[curPos];
+    const position = queue.shift();
+    const nextStep = arr[position];
 
-    if (nextLeftPos >= 0 && !visited[nextLeftPos]) {
-      queue.push(nextLeftPos);
-      visited[nextLeftPos] = true;
+    if (position - nextStep >= 0 && !visited[position - nextStep]) {
+      queue.push(position - nextStep);
+      visited[position - nextStep] = true;
     }
 
-    if (nextRightPos < n && !visited[nextRightPos]) {
-      queue.push(nextRightPos);
-      visited[nextRightPos] = true;
+    if (position + nextStep < rockN && !visited[position + nextStep]) {
+      queue.push(position + nextStep);
+      visited[position + nextStep] = true;
     }
   }
 
