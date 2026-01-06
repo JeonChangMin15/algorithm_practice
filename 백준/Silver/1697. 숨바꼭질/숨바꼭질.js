0dc1065bb6da78caf6bbf882781233e2bd5affc5
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2026-01-06 12:58:23 +0900

    [Silver I] Title: 숨바꼭질, Time: 3616 ms, Memory: 22792 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -1,38 +1,41 @@
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
-  .split("\n");
+  .split("\n")
+  .map((line) => line.replace(/\r/, ""));
 
-// 첫번째줄에 수빈이 위치와 동생 위치가 주어짐
-// 1초후에 앞뒤 한칸 or 2배 위치로 이동 가능
-// 동생 위치로 갈 수 있는 최소 시간을 구해라
-// bfs로 각 조건마다 visited 검사하면서 가면된다
+// -1, +1, *2로 이동이 가능하다
+// 0<x<100000
+// 1초씩 걸리고 가장빠른 초
+// [위치, 시간]으로 bfs로 탐색하면서 set에 넣고 중복 체크한다
 const solution = (input) => {
   const [start, end] = input[0].split(" ").map((v) => Number(v));
+  const set = new Set();
+  set.add(start);
+
   const queue = [[start, 0]];
-  const visited = Array(100001).fill(false);
-  visited[start] = true;
 
   while (queue.length) {
     const [pos, time] = queue.shift();
+
     if (pos === end) {
       console.log(time);
       break;
     }
 
-    if (pos * 2 <= 100000 && !visited[pos * 2]) {
-      queue.push([pos * 2, time + 1]);
-      visited[pos * 2] = true;
-    }
-
-    if (pos - 1 >= 0 && !visited[pos - 1]) {
+    if (pos - 1 >= 0 && !set.has(pos - 1)) {
       queue.push([pos - 1, time + 1]);
-      visited[pos - 1] = true;
+      set.add(pos - 1);
     }
 
-    if (pos + 1 <= 100000 && !visited[pos + 1]) {
+    if (pos + 1 <= 100000 && !set.has(pos + 1)) {
       queue.push([pos + 1, time + 1]);
-      visited[pos + 1] = true;
+      set.add(pos + 1);
+    }
+
+    if (pos * 2 <= 100000 && !set.has(pos * 2)) {
+      queue.push([pos * 2, time + 1]);
+      set.add(pos * 2);
     }
   }
 };
