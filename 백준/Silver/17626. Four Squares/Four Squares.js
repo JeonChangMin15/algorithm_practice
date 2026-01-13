656cd194de6e7cb4f7680d3a3c1ac51902b0d259
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2026-01-13 09:09:32 +0900

    [Silver III] Title: Four Squares, Time: 200 ms, Memory: 11748 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.js
+++ 백준/Silver/17626. Four Squares/Four Squares.js
@@ -4,45 +4,44 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// 모든 자연수는 넷혹은 그 이하의 제곱수의 합으로 표현이 가능하다
-// n을 최소개수 제곱수의 합으로 표현해라
-// 1부터 Math.sqrt(n)까지 전부다 for문으로 돌려봐야 알 수 있나
+// 제곱근의 합으로 나타내는 최소 갯수를 구해라
+// Math.sqrt로 해서 각 케이스마다 다 계산을 해서
+// 4 3 2 1로 체킹을 한다
 const solution = (input) => {
   const n = Number(input[0]);
-  const sqrtN = Math.floor(Math.sqrt(n));
-  let answer = 5;
+  const sqartN = Math.floor(Math.sqrt(n));
 
-  for (let i = 1; i <= sqrtN; i++) {
-    for (let j = 1; j <= sqrtN; j++) {
-      for (let k = 1; k <= sqrtN; k++) {
+  let answer = Infinity;
+
+  for (let i = 1; i <= sqartN; i++) {
+    for (let j = 1; j <= sqartN; j++) {
+      for (let k = 1; k <= sqartN; k++) {
         const res = n - i * i - j * j - k * k;
-        if (Math.sqrt(res) === Math.floor(Math.sqrt(res))) answer = 4;
-      }
-    }
-  }
-
-  for (let i = 1; i <= sqrtN; i++) {
-    for (let j = 1; j <= sqrtN; j++) {
-      for (let k = 1; k <= sqrtN; k++) {
-        if (i * i + j * j + k * k === n) {
-          answer = 3;
+        if (res >= 0 && Math.sqrt(res) === Math.floor(Math.sqrt(res))) {
+          answer = Math.min(answer, 4);
         }
       }
     }
   }
 
-  for (let i = 1; i <= sqrtN; i++) {
-    for (let j = 1; j <= sqrtN; j++) {
-      if (i * i + j * j === n) {
-        answer = 2;
+  for (let i = 1; i <= sqartN; i++) {
+    for (let j = 1; j <= sqartN; j++) {
+      const res = n - i * i - j * j;
+      if (res >= 0 && Math.sqrt(res) === Math.floor(Math.sqrt(res))) {
+        answer = Math.min(answer, 3);
       }
     }
   }
 
-  for (let i = 1; i <= sqrtN; i++) {
-    if (i * i === n) {
-      answer = 1;
+  for (let i = 1; i <= sqartN; i++) {
+    const res = n - i * i;
+    if (res >= 0 && Math.sqrt(res) === Math.floor(Math.sqrt(res))) {
+      answer = Math.min(answer, 2);
     }
+  }
+
+  if (n === sqartN * sqartN) {
+    answer = Math.min(answer, 1);
   }
 
   console.log(answer);
