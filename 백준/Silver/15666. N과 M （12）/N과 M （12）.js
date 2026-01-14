2236803dc96e12a7491e924a631a8f82fa6a3b7c
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2026-01-14 10:20:58 +0900

    [Silver II] Title: N과 M (12), Time: 152 ms, Memory: 11348 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.js
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.js
@@ -1,37 +1,38 @@
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
-  .split("\n");
+  .split("\n")
+  .map((line) => line.replace(/\r/, ""));
+
+// 첫번째줄에 숫자수와 조합 길이가 주어진다
+// 두번째줄에 숫자 리스트가 주어지고 오름차순으로 정렬해줘야한다
+// 중복되는 조합은 안되기때문에 set으로 판단해줘야한다
 
 const solution = (input) => {
-  const [n, combN] = input[0].split(" ").map((v) => Number(v));
-  const nums = input[1]
-    .split(" ")
-    .map((v) => Number(v))
-    .sort((a, b) => a - b);
-
+  const [n, comnLen] = input[0].split(" ").map((v) => Number(v));
+  const nums = input[1].split(" ").map((v) => Number(v));
+  nums.sort((a, b) => a - b);
   const set = new Set();
   const answer = [];
 
-  const dfs = (arr, start) => {
-    if (arr.length === combN) {
+  const backTracking = (arr, start) => {
+    if (arr.length === comnLen) {
       const val = arr.join(" ");
       if (!set.has(val)) {
-        answer.push(val);
         set.add(val);
+        answer.push(val);
       }
-
       return;
     }
 
     for (let i = start; i < n; i++) {
       arr.push(nums[i]);
-      dfs(arr, i);
+      backTracking(arr, i);
       arr.pop();
     }
   };
 
-  dfs([], 0);
+  backTracking([], 0);
 
   console.log(answer.join("\n"));
 };
