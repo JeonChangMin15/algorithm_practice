94b745b97045400bd40a734320569589643c747c
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2026-01-19 15:44:56 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 156 ms, Memory: 10052 KB -BaekjoonHub

--- 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.js
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.js
@@ -1,15 +1,13 @@
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
-  .split("\n");
+  .split("\n")
+  .map((line) => line.replace(/\r/, ""));
 
-// N명이 뭉쳐져 있으면 전투력 N*N이다
 // 첫째줄에 colN, rowN이 주어진다
-// 그다음줄에 병사들이 주어지는데 B, W로 주어진다
-// 흰색옷 전투력과 파란색 전투력을 한줄에 출력을 한다
-// dfs로 방문안한곳이면 들어간다, 좌표, 팀을 매개변수로 가지고
-// 상하좌우 탐색하면서 만약 방문했거나 적팀이면 return 0
-// 그렇게해서 종료하면 리턴값 제곱해서 더해주면된다
+// B(파란색), W(흰색) 병사의 위력을 한줄에 출력한다
+// dfs로 상하좌우 탐색하는데 visited로 마킹하면된다
+// 해당 너비의 제곱을 더해주면된다
 const solution = (input) => {
   const [colN, rowN] = input[0].split(" ").map((v) => Number(v));
   const grid = [];
@@ -22,42 +20,45 @@ const solution = (input) => {
     .fill(0)
     .map((v) => Array(colN).fill(false));
 
-  let blueTeam = 0;
-  let whieTeam = 0;
-
   const dfs = (x, y, team) => {
     if (
       x < 0 ||
       x >= rowN ||
       y < 0 ||
       y >= colN ||
-      visited[x][y] ||
-      team !== grid[x][y]
+      grid[x][y] !== team ||
+      visited[x][y]
     )
       return 0;
 
     visited[x][y] = true;
-    const up = dfs(x - 1, y, team);
-    const down = dfs(x + 1, y, team);
+
     const left = dfs(x, y - 1, team);
     const right = dfs(x, y + 1, team);
+    const down = dfs(x + 1, y, team);
+    const up = dfs(x - 1, y, team);
 
-    return 1 + up + down + left + right;
+    return 1 + left + right + down + up;
   };
+
+  let white = 0;
+  let blue = 0;
 
   for (let i = 0; i < rowN; i++) {
     for (let j = 0; j < colN; j++) {
-      if (visited[i][j]) continue;
-      const cnt = dfs(i, j, grid[i][j]);
-      if (grid[i][j] === "W") {
-        whieTeam += cnt * cnt;
-      } else {
-        blueTeam += cnt * cnt;
+      if (!visited[i][j]) {
+        const area = dfs(i, j, grid[i][j]);
+
+        if (grid[i][j] === "W") {
+          white += area * area;
+        } else {
+          blue += area * area;
+        }
       }
     }
   }
 
-  console.log(whieTeam, blueTeam);
+  console.log(white, blue);
 };
 
 solution(input);
