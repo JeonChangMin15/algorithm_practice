1b10ea6df9f174576aad82d8d9e3ae4f67307c9a
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2026-01-20 09:59:51 +0900

    [Silver III] Title: 퇴사, Time: 156 ms, Memory: 10904 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.js
+++ 백준/Silver/14501. 퇴사/퇴사.js
@@ -4,27 +4,31 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
+// 백트레킹으로 모든 경우의 수를 찾아야한다
+// currentDay, money를 인자로 두고 탐색해야된다
 const solution = (input) => {
-  const n = Number(input[0]);
-  const info = [];
+  const dayN = Number(input[0]);
+  const schedule = [[0, 0]];
 
-  for (let i = 1; i <= n; i++) {
-    info.push(input[i].split(" ").map((v) => Number(v)));
+  for (let i = 1; i <= dayN; i++) {
+    schedule.push(input[i].split(" ").map((v) => Number(v)));
   }
 
   let answer = 0;
 
-  const backTrack = (day, cost) => {
-    if (day > n) return;
-    answer = Math.max(cost, answer);
+  const backTracking = (currentDay, totalCost) => {
+    if (currentDay > dayN + 1) return;
+    answer = Math.max(answer, totalCost);
 
-    for (let i = day; i < n; i++) {
-      const [time, money] = info[i];
-      backTrack(i + time, cost + money);
+    for (let i = currentDay; i <= dayN; i++) {
+      const [d, t] = schedule[i];
+
+      backTracking(i + d, totalCost + t);
     }
   };
 
-  backTrack(0, 0);
+  backTracking(1, 0);
+
   console.log(answer);
 };
 
