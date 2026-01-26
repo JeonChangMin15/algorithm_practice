b41a112012030f1c73cdeca116466dfa739d8a24
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2026-01-27 08:34:06 +0900

    [Silver III] Title: 숫자 야구, Time: 320 ms, Memory: 23024 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.js
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.js
@@ -4,62 +4,59 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// 1부터 9까지 서로 다른 숫자 세개로 구성된 조합을 만들고
-// 물어본다 -> 만약 세자리수에 동일한 위치면 스트라이크, 있긴한데 다른위치면 볼
-// 세자리수를 정확히 맞추어 3스트라이크가 되면 게임이 끝난다
-// 아니라면 계속해서 새로운 수를 계속해서 물어본다
-// 첫번째줄에 몇번 질문했는지 나오고 그다음줄부터 세자리수와 스트, 볼 카운트가 한줄에 줄어진다
-// 먼저 세자리 조합수를 전부 다 만들고 질문마다 비교를 한다
-// 모든 질문에 스트, 볼 합이 같다면 카운팅을 해주면된다
-// 만약에 3스트가 있다면 무조건 1
-// 각 자릿수마다 포함되어있고 위치 같은지 확인
+// 1부터 9까지 서로 다른 숫자 세개로 구성된 세자리수를 묻는다
+// 거기서 숫자가 동일한 위치면 스트1 숫자는 존재하지만 위치가 다르면 볼1
+// 질문에 다 맞으면 가능한 숫자로 쳐준다
+// 삼중 for문으로 모든 경우의 케이스를 검사를 시작한다
+// 그리고 각 케이스마다 위치와 존재여부를 확인해서 볼,스트라이크를 검사한다
+// 모든 질문에 볼 스트 갯수가 일치하면 카운팅을 해준다
 const solution = (input) => {
-  const n = Number(input[0]);
-  const questions = [];
+  const caseN = Number(input[0]);
+  const question = [];
 
-  for (let i = 1; i <= n; i++) {
-    const [val, strike, ball] = input[i].split(" ");
-    questions.push([val, Number(strike), Number(ball)]);
+  for (let i = 1; i <= caseN; i++) {
+    question.push(input[i].split(" ").map((v) => Number(v)));
   }
 
-  const combs = [];
+  let answer = 0;
 
   for (let i = 1; i <= 9; i++) {
     for (let j = 1; j <= 9; j++) {
       for (let k = 1; k <= 9; k++) {
         if (i !== j && j !== k && i !== k) {
-          combs.push(`${i}${j}${k}`);
+          const val = [i, j, k];
+          let isValid = true;
+
+          for (const [q, s, b] of question) {
+            let curS = 0;
+            let curB = 0;
+            for (let l = 0; l < 3; l++) {
+              const v = val[l];
+              const qArr = String(q)
+                .split("")
+                .map((v) => Number(v));
+
+              if (qArr.includes(v) && qArr[l] === v) {
+                curS += 1;
+              }
+
+              if (qArr.includes(v) && qArr[l] !== v) {
+                curB += 1;
+              }
+            }
+
+            if (curS !== s || curB !== b) {
+              isValid = false;
+            }
+          }
+
+          if (isValid) answer += 1;
         }
       }
     }
   }
 
-  let answer = 0;
-  let isAllStrike = false;
-
-  for (const comb of combs) {
-    let isValid = true;
-
-    for (const [question, strike, ball] of questions) {
-      let s = 0;
-      let b = 0;
-      for (let i = 0; i < 3; i++) {
-        const s1 = comb[i];
-        if (question.includes(s1) && question[i] === s1) {
-          s += 1;
-        } else if (question.includes(s1) && question[i] !== s1) {
-          b += 1;
-        }
-      }
-
-      if (strike === 3) isAllStrike = true;
-      if (s !== strike || b !== ball) isValid = false;
-    }
-
-    if (isValid) answer += 1;
-  }
-
-  console.log(isAllStrike ? 1 : answer);
+  console.log(answer);
 };
 
 solution(input);
