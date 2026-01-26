151c37e6da85281db8078a587559cdf3491abd69
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2026-01-26 09:28:39 +0900

    [Silver I] Title: 그림, Time: 660 ms, Memory: 41100 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.js
+++ 백준/Silver/1926. 그림/그림.js
@@ -1,8 +1,13 @@
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
-  .split("\n");
+  .split("\n")
+  .map((line) => line.replace(/\r/, ""));
 
+// rowN, colN이 주어지고 그리드가 주어진다
+// 1로 연결된 영역의 개수와 가장큰 영역의 너비를 구한다
+// dfs로 1인 영역을 탐색하면서 상하좌우 넣는다
+// 방문한 지점은 0으로 마킹하고 탐색이 끝나면 최대값 갱신을 해준다
 const solution = (input) => {
   const [rowN, colN] = input[0].split(" ").map((v) => Number(v));
   const grid = [];
@@ -11,11 +16,12 @@ const solution = (input) => {
     grid.push(input[i].split(" ").map((v) => Number(v)));
   }
 
-  let areaCnt = 0;
   let maxArea = 0;
+  let cnt = 0;
 
   const dfs = (x, y) => {
     if (x < 0 || x >= rowN || y < 0 || y >= colN || grid[x][y] === 0) return 0;
+
     grid[x][y] = 0;
 
     const up = dfs(x - 1, y);
@@ -28,13 +34,15 @@ const solution = (input) => {
 
   for (let i = 0; i < rowN; i++) {
     for (let j = 0; j < colN; j++) {
-      if (grid[i][j] === 0) continue;
-      areaCnt += 1;
-      maxArea = Math.max(maxArea, dfs(i, j));
+      if (grid[i][j] === 1) {
+        const cur = dfs(i, j);
+        maxArea = Math.max(maxArea, cur);
+        cnt += 1;
+      }
     }
   }
 
-  console.log(areaCnt);
+  console.log(cnt);
   console.log(maxArea);
 };
 
