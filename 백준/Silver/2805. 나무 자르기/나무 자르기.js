b0dc0c3d5290826a547ec3510c0bca362b1aa20b
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2026-02-02 09:08:07 +0900

    [Silver II] Title: 나무 자르기, Time: 1024 ms, Memory: 135768 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.js
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.js
@@ -4,21 +4,26 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
+// 첫번째줄에 나무의 수와 가져가려고하는 나무의 길이
+// 둘째줄에 나무의 높이가 주어진다
+// 절단기보다 낮은 나무는 안자르고 최소한 같거나 큰 나무만 자른다
+// 자르고 남은 나무들의 길이합이 목표 길이만큼 가능한 절단기의 길이를 최대값
+// 이분탐색으로 하며 된다
 const solution = (input) => {
-  const [n, targetLen] = input[0].split(" ").map((v) => Number(v));
+  const [treeN, targetLen] = input[0].split(" ").map((v) => Number(v));
   const arr = input[1].split(" ").map((v) => Number(v));
 
   let lt = 1;
   let rt = Math.max(...arr);
-
   let answer = 0;
 
   while (lt <= rt) {
-    const mid = Math.floor((lt + rt) / 2);
     let res = 0;
-
-    arr.forEach((v) => {
-      if (v > mid) res += v - mid;
+    const mid = Math.floor((lt + rt) / 2);
+    arr.forEach((len) => {
+      if (len >= mid) {
+        res += len - mid;
+      }
     });
 
     if (res >= targetLen) {
