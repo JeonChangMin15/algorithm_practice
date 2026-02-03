1e75f3d5f2415aaa2b5bcd2ca50be12108f2bdeb
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2026-02-03 13:45:16 +0900

    [Silver I] Title: 현명한 나이트, Time: 252 ms, Memory: 20944 KB -BaekjoonHub

--- 백준/Silver/18404. 현명한 나이트/현명한 나이트.js
+++ 백준/Silver/18404. 현명한 나이트/현명한 나이트.js
@@ -4,31 +4,27 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// bfs로 풀이하면된다
-// 첫째줄에 rowN, colN이 주어지고 둘째줄에 나이트 초기 위치가 주어진다
-// 그다음줄부터 colN만큼 상대편 말 위치를 주어진다
-// 상대말 잡기 위한 최소 이동수를 공백 기준으로 한줄에 줄력한다
-// 위치는 -1씩해서 저장하고 마킹하면서 간다
+// 첫번째줄에 체스판 크기와 말의 수
+// 두번째줄에 시작점
+// 그다음줄부터 말의 위치 정보가 주어진다
+// grid로 무한으로 셋팅한 후 bfs로 탐색하면서 지점을 탐색하면된다
+// 좌표 주어지는 좌표는 -1씩 차감
 const solution = (input) => {
-  const [n, chessN] = input[0].split(" ").map((v) => Number(v));
+  const [sizeN, caseN] = input[0].split(" ").map((v) => Number(v));
   const [startX, startY] = input[1].split(" ").map((v) => Number(v));
-  const positions = [];
+  const grid = Array(sizeN)
+    .fill(0)
+    .map((v) => Array(sizeN).fill(Infinity));
 
-  for (let i = 2; i < 2 + chessN; i++) {
+  const arr = [];
+
+  for (let i = 2; i < 2 + caseN; i++) {
     const [x, y] = input[i].split(" ").map((v) => Number(v));
-    positions.push([x - 1, y - 1]);
+    arr.push([x - 1, y - 1]);
   }
 
-  const visited = Array(n)
-    .fill(0)
-    .map((v) => Array(n).fill(false));
-
-  const dist = Array(n)
-    .fill(0)
-    .map((v) => Array(n).fill(0));
-
+  grid[startX - 1][startY - 1] = 0;
   const queue = [[startX - 1, startY - 1, 0]];
-  visited[startX - 1][startY - 1] = true;
   const dirs = [
     [-2, -1],
     [-2, 1],
@@ -41,31 +37,30 @@ const solution = (input) => {
   ];
 
   while (queue.length) {
-    const [x, y, cnt] = queue.shift();
+    const [x, y, d] = queue.shift();
 
     for (const [dx, dy] of dirs) {
       const nextX = x + dx;
       const nextY = y + dy;
       const isValid =
         nextX >= 0 &&
-        nextX < n &&
+        nextX < sizeN &&
         nextY >= 0 &&
-        nextY < n &&
-        !visited[nextX][nextY];
+        nextY < sizeN &&
+        d + 1 < grid[nextX][nextY];
 
       if (isValid) {
-        queue.push([nextX, nextY, cnt + 1]);
-        dist[nextX][nextY] = cnt + 1;
-        visited[nextX][nextY] = true;
+        queue.push([nextX, nextY, d + 1]);
+        grid[nextX][nextY] = d + 1;
       }
     }
   }
 
   const answer = [];
-
-  for (const [x, y] of positions) {
-    answer.push(dist[x][y]);
+  for (const [x, y] of arr) {
+    answer.push(grid[x][y]);
   }
+
   console.log(answer.join(" "));
 };
 
