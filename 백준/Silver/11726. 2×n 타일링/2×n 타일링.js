7ba25a16e665858ad7e46e4652085957c8a941b2
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2026-02-04 13:44:48 +0900

    [Silver III] Title: 2×n 타일링, Time: 152 ms, Memory: 9648 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.js
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.js
@@ -4,17 +4,16 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
+// 2xN 크기의 직사각형을 채우는 방법의 수를 10007로 나눈 나머지를 출력
+// 1->1 2->2 3->3 4->5
 const solution = (input) => {
   const n = Number(input[0]);
   const dp = Array(n + 1).fill(0);
-
   dp[1] = 1;
   dp[2] = 2;
-
   for (let i = 3; i <= n; i++) {
-    dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+    dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
   }
-
   console.log(dp[n]);
 };
 
