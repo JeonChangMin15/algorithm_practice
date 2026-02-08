875a29d736bc667137161d66225ee168ebd0ffe5
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2026-02-09 08:45:41 +0900

    [Silver I] Title: 작업, Time: 540 ms, Memory: 90272 KB -BaekjoonHub

--- 백준/Silver/21937. 작업/작업.js
+++ 백준/Silver/21937. 작업/작업.js
@@ -4,12 +4,11 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// 첫번째줄에 작업수와 정보의 개수가 주어진다
-// 두번째줄부터 정보의 개수만큼 n1 -> n2간선이 주어지고
-// 마지막줄에 끝내야할 작업이 주어진다
-// 끝내야할 작업을 하기전에 먼저 해야하는일의 개수
-// 간선에서 graph[n2].push(n1)을 넣어준다음에
-// dfs로 탐색을 해주면된다
+// 첫번째줄에 작업개수와 작업 순서정보의 개수 주어진다
+// 두번째줄부터 작업 정보가 주어지고 마지막줄에 끝내야할 작업
+// 끝내야할 작업을 하기위해서 해야할 일의 수를 구해야된다
+// graph에 넣을때 graph[end] = start로 넣고
+// dfs로 방문한 지점의 갯수를 출력하면 될거 같다
 const solution = (input) => {
   const [nodeN, lineN] = input[0].split(" ").map((v) => Number(v));
   const graph = Array(nodeN + 1)
@@ -17,27 +16,25 @@ const solution = (input) => {
     .map((v) => []);
 
   for (let i = 1; i <= lineN; i++) {
-    const [n1, n2] = input[i].split(" ").map((v) => Number(v));
-    graph[n2].push(n1);
+    const [start, end] = input[i].split(" ").map((v) => Number(v));
+    graph[end].push(start);
   }
-  const workNode = Number(input[lineN + 1]);
+
+  const targetNode = Number(input[lineN + 1]);
   const visited = Array(nodeN + 1).fill(false);
 
-  const dfs = (node) => {
-    if (visited[node]) return 0;
-    visited[node] = true;
-    let cnt = 1;
+  const dfs = (curNode) => {
+    if (visited[curNode]) return;
+    visited[curNode] = true;
 
-    for (const nextNode of graph[node]) {
-      cnt += dfs(nextNode);
+    for (const nextNode of graph[curNode]) {
+      dfs(nextNode);
     }
-
-    return cnt;
   };
 
-  const answer = dfs(workNode);
+  dfs(targetNode);
 
-  console.log(answer - 1);
+  console.log(visited.filter((v) => v === true).length - 1);
 };
 
 solution(input);
