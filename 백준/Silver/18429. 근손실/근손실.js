2024616d0d2c178a5694cf1e579160d9d80aaf01
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2026-02-10 09:30:26 +0900

    [Silver III] Title: 근손실, Time: 156 ms, Memory: 10224 KB -BaekjoonHub

--- 백준/Silver/18429. 근손실/근손실.js
+++ 백준/Silver/18429. 근손실/근손실.js
@@ -4,36 +4,31 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// 첫 중량은 500 하루에 k만큼 근손실이 일어난다
-// 첫째줄에 N, k가 주어지고 두번째줄에 각 날짜마다 근육 증가량이 주어진다
-// 모든 기간동안 500 이상 유지되는 루틴의 갯수를 구해라
-// 백트레킹으로 visited로 마킹하고 인자로 day, total로 계산하면서 500보다 작으면 return
-// 해당 날짜에 500 이상이면 +1 return
+// 첫번째 줄에 운동키트 갯수와 하루 근손실이다
+// 중복없는 순열을 만드는데 500kg 밑으로 하루라도 안떨어지는 조합의 수를 구해야된다
+// 인자는 arr, weight를 넣어서 500 미만이면 return
 const solution = (input) => {
-  const [lastDay, loseWeight] = input[0].split(" ").map((v) => Number(v));
-  const weights = input[1].split(" ").map((v) => Number(v));
-
+  const [foodN, lose] = input[0].split(" ").map((v) => Number(v));
+  const weightArr = input[1].split(" ").map((v) => Number(v));
   let answer = 0;
-  const used = Array(lastDay).fill(false);
 
-  const backTrack = (day, total) => {
-    if (total < 500) return;
-    if (lastDay === day) {
-      if (total >= 500) answer += 1;
-
+  const backTracking = (arr, weight) => {
+    if (arr.length > foodN) return;
+    if (weight < 500) return;
+    if (arr.length === foodN) {
+      answer += 1;
       return;
     }
 
-    for (let i = 0; i < lastDay; i++) {
-      if (used[i]) continue;
-      used[i] = true;
-      backTrack(day + 1, total - loseWeight + weights[i]);
-      used[i] = false;
+    for (let i = 0; i < foodN; i++) {
+      if (arr.includes(i)) continue;
+      arr.push(i);
+      backTracking(arr, weight - lose + weightArr[i]);
+      arr.pop();
     }
   };
 
-  backTrack(0, 500);
-
+  backTracking([], 500);
   console.log(answer);
 };
 
