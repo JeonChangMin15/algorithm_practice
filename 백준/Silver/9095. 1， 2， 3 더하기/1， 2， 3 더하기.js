07eed04b17b1ecca34d88ade44f711998c6f439b
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2026-02-11 13:39:58 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 92 ms, Memory: 9372 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
@@ -4,30 +4,29 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// 첫째줄에 테이스 케이스의 개수가 주어진다
-// 그다음줄부터 숫자가 주어지는데 1,2,3의 합으로 나타내는
 // 1
-// 1 + 1, 2
-// 1 + 1 + 1, 1+2, 2+1, 3
-
-// dp[n] = dp[n-3] + dp[n-2] + dp[n-1]
+// 1+1, 2
+// 1+2, 1+1+1, 2+1, 3
+// 첫번재줄에 테스트 수가 주어지고 각 수마다 합의 방법의 수를 나타내라
+// dp[i] = dp[i-3]+dp[i-2]+dp[i-1]
 const solution = (input) => {
   const n = Number(input[0]);
-  const testCase = [];
+  const arr = [];
 
-  for (let i = 1; i < input.length; i++) {
-    testCase.push(Number(input[i]));
-  }
-  const dp = Array(11).fill(0);
-  dp[1] = 1;
-  dp[2] = 2;
-  dp[3] = 4;
-
-  for (let i = 4; i < 11; i++) {
-    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+  for (let i = 1; i <= n; i++) {
+    arr.push(Number(input[i]));
   }
 
-  for (let val of testCase) {
+  for (const val of arr) {
+    const dp = Array(val + 1).fill(0);
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+
+    for (let i = 4; i <= val; i++) {
+      dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+    }
+
     console.log(dp[val]);
   }
 };
