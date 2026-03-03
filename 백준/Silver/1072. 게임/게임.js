83aaee5e3a860f8c44db850ddc61dbeed9e54d0c
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2026-03-03 13:09:48 +0900

    [Silver III] Title: 게임, Time: 96 ms, Memory: 9608 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.js
+++ 백준/Silver/1072. 게임/게임.js
@@ -4,35 +4,28 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// 게임횟수, 이긴 게임, 승률이 주어진다
-// 앞으로 하는 판은 무조건 이긴다는 가정하에 최소 몇번을 더해야 z가 변하는지 구해라
-// z가 절대 안변하면 -1을 출력
-// X,Y가 주어진다. 승률은 소수점은 버린다
-// 그렇다면 (y+a)/(x+a)가 기존z보다 커지면 된다
-// 최소 판은 1판 최대판을 1000000000하고 계산해서 바뀌면 갱신후 rt = mid-1
-// 그대로면 lt = mid +1
+
 const solution = (input) => {
-  const [prevTotal, prevWin] = input[0].split(" ").map((v) => Number(v));
-  const prevWinRate = Math.floor((prevWin * 100) / prevTotal);
+  const [curTotal, curWin] = input[0].split(" ").map((v) => Number(v));
+  const curPercent = Math.floor((100 * curWin) / curTotal);
+
   let lt = 1;
-  let rt = 1000000001;
-  let answer = -1;
+  let rt = curTotal;
+  let answer = Infinity;
 
   while (lt <= rt) {
-    const addWin = Math.floor((lt + rt) / 2);
-    const curWinRate = Math.floor(
-      ((prevWin + addWin) * 100) / (prevTotal + addWin)
-    );
+    const mid = Math.floor((lt + rt) / 2);
+    const nextPercent = Math.floor((100 * (curWin + mid)) / (curTotal + mid));
 
-    if (curWinRate > prevWinRate) {
-      answer = addWin;
-      rt = addWin - 1;
+    if (nextPercent > curPercent) {
+      answer = Math.min(answer, mid);
+      rt = mid - 1;
     } else {
-      lt = addWin + 1;
+      lt = mid + 1;
     }
   }
 
-  console.log(answer);
+  console.log(answer !== Infinity ? answer : -1);
 };
 
 solution(input);
