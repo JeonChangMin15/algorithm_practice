6751a7afd92a1f35e56113a054decd51836e04cd
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2026-03-04 14:36:37 +0900

    [Silver I] Title: 숨바꼭질, Time: 408 ms, Memory: 36096 KB -BaekjoonHub

--- 백준/Silver/6118. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/6118. 숨바꼭질/숨바꼭질.js
@@ -1,13 +1,14 @@
 const input = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
-  .split("\n");
+  .split("\n")
+  .map((line) => line.replace(/\r/, ""));
 
-// 첫번째는 숨어야할 헛간 번호(여러개면 가장 작은 헛간 번호), 두번재 헛간까지 거리, 세번째는 같은 거리를 갖는 헛간의 개수
-// 첫번재줄에는 헛간의 갯수와 간선의 수(양방향)이 주어진다
-// 두번재줄부터 간선이 주어진다
-// 1번에서 시작을 한다 -> bfs로 해당 지점을 grid Infinity로 마킹한 후 +1보다 작으면 가면 된다
-// 가장 먼곳의 거리와 해당 지점을 모으면 된다
+// 첫번째는 숨어야하는 헛간번호(거리가 같으면 가장 짧은 작은 번호)
+// 그다음은 헛간 거리, 세번째는 해당거리까지 있는 헛간의 총 갯수
+// 첫번째줄에 헛간의 수와 라인의 갯수
+// bfs로 각 노드마다 지점을 다 넣고 1번부터 탐색을하면된ㄷ
+// dist 배열에다가 각 노드마다 거리를 Infinity로 초기화 후 짧은곳이면 방문
 const solution = (input) => {
   const [nodeN, lineN] = input[0].split(" ").map((v) => Number(v));
   const graph = Array(nodeN + 1)
@@ -20,34 +21,30 @@ const solution = (input) => {
     graph[n2].push(n1);
   }
 
-  const dist = Array(nodeN + 1).fill(Infinity);
-  dist[1] = 0;
   const queue = [[1, 0]];
-
-  let maxDist = 0;
+  const distArr = Array(nodeN + 1).fill(Infinity);
+  distArr[1] = 0;
+  distArr[0] = 0;
 
   while (queue.length) {
-    const [curNode, curLen] = queue.shift();
-
-    maxDist = Math.max(maxDist, curLen);
+    const [curNode, curDist] = queue.shift();
 
     for (const nextNode of graph[curNode]) {
-      if (dist[nextNode] > curLen + 1) {
-        queue.push([nextNode, curLen + 1]);
-        dist[nextNode] = curLen + 1;
+      if (distArr[nextNode] > curDist + 1) {
+        queue.push([nextNode, curDist + 1]);
+        distArr[nextNode] = curDist + 1;
       }
     }
   }
 
+  const maxDist = Math.max(...distArr);
   const answer = [];
 
   for (let i = 1; i <= nodeN; i++) {
-    if (dist[i] === maxDist) {
+    if (distArr[i] === maxDist) {
       answer.push(i);
     }
   }
-
-  answer.sort((a, b) => a - b);
 
   console.log(answer[0], maxDist, answer.length);
 };
