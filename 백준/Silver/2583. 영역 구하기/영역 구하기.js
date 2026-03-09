23b34b8f4862a3b197c363244db5ccac4b02e4ef
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2026-03-09 13:44:08 +0900

    [Silver I] Title: 영역 구하기, Time: 156 ms, Memory: 11180 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.js
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.js
@@ -4,11 +4,10 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
-// 첫번째줄에 rown, coln, 직사각형 갯수
-// 두번재줄부터 직사각형 좌표가 x1,y1,x2,y2
-// 직사각형 영역을 제외한 나머지 영역들의 넓이를 오름차순
-// 먼저 그리드로 1로 만들고 직사각형 영역을 다 0으로 마킹
-// 그 다음 dfs로
+// 첫번째줄에 rowN, colN, 직사각형 갯수가 주어진다
+// 직사각형에 포함안된 영역의 수와 각각의 너비를 오름차순으로 출력
+// 각 직사각형의 시작점 x1,y1,x2,y2가 주어진다
+// for문을 돌면서 각 지점에 0을 마킹하고 dfs로 영역을 탐색하면된다
 const solution = (input) => {
   const [rowN, colN, squareN] = input[0].split(" ").map((v) => Number(v));
   const grid = Array(rowN)
@@ -17,7 +16,6 @@ const solution = (input) => {
 
   for (let i = 1; i <= squareN; i++) {
     const [y1, x1, y2, x2] = input[i].split(" ").map((v) => Number(v));
-
     for (let i = x1; i < x2; i++) {
       for (let j = y1; j < y2; j++) {
         grid[i][j] = 0;
@@ -30,6 +28,7 @@ const solution = (input) => {
   const dfs = (x, y) => {
     if (x < 0 || x >= rowN || y < 0 || y >= colN || grid[x][y] === 0) return 0;
     grid[x][y] = 0;
+
     const up = dfs(x - 1, y);
     const down = dfs(x + 1, y);
     const left = dfs(x, y - 1);
@@ -44,6 +43,7 @@ const solution = (input) => {
       answer.push(dfs(i, j));
     }
   }
+
   console.log(answer.length);
   console.log(answer.sort((a, b) => a - b).join(" "));
 };
