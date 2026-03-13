aa2fc9c5d1c59e099de847da2decadba8c4394fe
JeonchangMin <89255072+JeonChangMin15@users.noreply.github.com>
2026-03-13 09:33:07 +0900

    [Silver II] Title: 비밀번호, Time: 4672 ms, Memory: 12288 KB -BaekjoonHub

--- 백준/Silver/13908. 비밀번호/비밀번호.js
+++ 백준/Silver/13908. 비밀번호/비밀번호.js
@@ -4,15 +4,28 @@ const input = require("fs")
   .split("\n")
   .map((line) => line.replace(/\r/, ""));
 
+// 첫번째줄에 비밀번호 길이, 알게된 비밀번호에 들어가는 수
+// 두번째줄에 비밀번호에 무조건 포함되는 수
+// 중복포함되는 순열에서 대조해봐야된다
 const solution = (input) => {
-  const [lockN, knowN] = input[0].split(" ").map((v) => Number(v));
-  const knowNums = knowN ? input[1].split(" ").map((v) => Number(v)) : [];
+  const [passwordLen, includeN] = input[0].split(" ").map((v) => Number(v));
+  let includeArr = [];
+  if (includeN > 0) {
+    includeArr = input[1].split(" ").map((v) => Number(v));
+  }
+
   let answer = 0;
 
   const backTrack = (arr) => {
-    if (arr.length === lockN) {
-      const isValid = knowNums.every((v) => arr.includes(v));
-      if (isValid || knowNums.length === 0) {
+    if (arr.length === passwordLen) {
+      const curVal = arr.join("");
+      let isValid = true;
+
+      for (const val of includeArr) {
+        if (!curVal.includes(val)) isValid = false;
+      }
+
+      if (isValid) {
         answer += 1;
       }
 
